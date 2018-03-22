openxla/xla

commit 3304b02843879bdd98b3605979f9b5cfe0d26530

[XLA:python] Plumb hlo_profile flag.
PiperOrigin-RevId: 189377860

xla/client/executable_build_options.cc
@@ -76,4 +76,11 @@ ExecutableBuildOptions::generate_hlo_graph() const {
   return generate_hlo_graph_;
 }
 
+ExecutableBuildOptions& ExecutableBuildOptions::set_hlo_profile(bool enabled) {
+  hlo_profile_ = enabled;
+  return *this;
+}
+
+bool ExecutableBuildOptions::hlo_profile() const { return hlo_profile_; }
+
 }  // namespace xla

xla/client/executable_build_options.h
@@ -57,11 +57,17 @@ class ExecutableBuildOptions {
   ExecutableBuildOptions& set_generate_hlo_graph(string regex);
   const tensorflow::gtl::optional<string>& generate_hlo_graph() const;
 
+  // If set, specifies that we should record an HLO profile during execution and
+  // log it after execution (as in DebugOptions).
+  ExecutableBuildOptions& set_hlo_profile(bool enabled);
+  bool hlo_profile() const;
+
   // Returns a string representation of the build options, suitable for
   // debugging.
   string ToString() const;
 
  private:
+  bool hlo_profile_ = false;
   int device_ordinal_ = -1;
   Shape result_layout_;
   bool result_layout_set_ = false;

xla/service/local_service.cc
@@ -119,6 +119,8 @@ StatusOr<std::unique_ptr<Executable>> LocalService::CompileExecutable(
   }
 
   ExecutionOptions execution_options = CreateDefaultExecutionOptions();
+  execution_options.mutable_debug_options()->set_xla_hlo_profile(
+      build_options.hlo_profile());
   if (build_options.generate_hlo_graph().has_value()) {
     execution_options.mutable_debug_options()->set_xla_generate_hlo_graph(
         build_options.generate_hlo_graph().value());

xla/python/local_computation_builder.i
@@ -833,6 +833,19 @@ tensorflow::ImportNumpy();
     }
     Py_DECREF(o);
 
+    o = PyObject_GetAttrString($input, "hlo_profile");
+    if (o == NULL) {
+      return NULL;
+    }
+    if (o != Py_None) {
+      if (!PyBool_Check(o)) {
+        PyErr_SetString(PyExc_TypeError, "ExecutableBuildOptions.hlo_profile must be a bool or None.");
+        return NULL;
+      }
+      build_options.set_hlo_profile(o == Py_True);
+    }
+    Py_DECREF(o);
+
     o = PyObject_GetAttrString($input, "result_shape");
     if (o == nullptr) {
       return nullptr;

xla/python/xla_client.py
@@ -320,6 +320,7 @@ class CompileOptions(object):
 
   def __init__(self):
     self.generate_hlo_graph = None
+    self.hlo_profile = False
 
 
 def transfer_to_infeed(value, replica_number=None):

commit 07e73fee2e0579beb878e62a0fe6c17dfee64fa7

[XLA] Only overwrite the hlo_profiling flag when it's not enabled by default.
This got broken in 3304b02843879bdd98b3605979f9b5cfe0d26530

PiperOrigin-RevId: 190077360

xla/client/executable_build_options.cc
@@ -103,6 +103,8 @@ ExecutableBuildOptions& ExecutableBuildOptions::set_hlo_profile(bool enabled) {
   return *this;
 }
 
-bool ExecutableBuildOptions::hlo_profile() const { return hlo_profile_; }
+tensorflow::gtl::optional<bool> ExecutableBuildOptions::hlo_profile() const {
+  return hlo_profile_;
+}
 
 }  // namespace xla

xla/client/executable_build_options.h
@@ -70,17 +70,18 @@ class ExecutableBuildOptions {
       tensorflow::StringPiece dirpath);
   const tensorflow::gtl::optional<string>& dump_per_pass_hlo_proto_to() const;
 
-  // If set, specifies that we should record an HLO profile during execution and
-  // log it after execution (as in DebugOptions).
+  // If true, specifies that we should record an HLO profile during execution
+  // and log it after execution (as in DebugOptions). If nullopt the default is
+  // used.
   ExecutableBuildOptions& set_hlo_profile(bool enabled);
-  bool hlo_profile() const;
+  tensorflow::gtl::optional<bool> hlo_profile() const;
 
   // Returns a string representation of the build options, suitable for
   // debugging.
   string ToString() const;
 
  private:
-  bool hlo_profile_ = false;
+  tensorflow::gtl::optional<bool> hlo_profile_;
   int device_ordinal_ = -1;
   Shape result_layout_;
   bool result_layout_set_ = false;

xla/service/local_service.cc
@@ -119,8 +119,10 @@ StatusOr<std::unique_ptr<Executable>> LocalService::CompileExecutable(
   }
 
   ExecutionOptions execution_options = CreateDefaultExecutionOptions();
-  execution_options.mutable_debug_options()->set_xla_hlo_profile(
-      build_options.hlo_profile());
+  if (build_options.hlo_profile().has_value()) {
+    execution_options.mutable_debug_options()->set_xla_hlo_profile(
+        *build_options.hlo_profile());
+  }
   if (build_options.generate_hlo_graph().has_value()) {
     execution_options.mutable_debug_options()->set_xla_generate_hlo_graph(
         build_options.generate_hlo_graph().value());