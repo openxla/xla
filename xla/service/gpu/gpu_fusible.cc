openxla/xla

commit c7e0c2805c70d14163a24c6d7058462f436497b2

GPU fusion code cleanup: Extract GpuInstructionFusion::IsFusible into gpu_fusible.cc
Tested using //tensorflow/compiler/xla/service/gpu/tests:gpu_fusion_test

xla/service/gpu/gpu_fusible.cc
@@ -21,6 +21,7 @@ namespace xla {
 namespace gpu {
 
 namespace {
+
 void AppendParams(const HloInstruction& instr,
                   std::vector<HloInstruction*>* params) {
   if (instr.opcode() == HloOpcode::kFusion) {
@@ -129,6 +130,25 @@ bool ShapesCompatibleForMultiOutputFusion(const HloInstruction& instr1,
   // TODO(tjoerg): Further relax the constraint. The datatype does not matter.
   return ShapeUtil::EqualIgnoringFpPrecision(get_loop_shape(instr_1),
                                              get_loop_shape(instr_2));
+}
+
+bool IsFusible(const HloInstruction& instr) {
+  return (instr.IsElementwise() && instr.operand_count() > 0) ||
+         instr.opcode() == HloOpcode::kBitcast ||
+         instr.opcode() == HloOpcode::kBroadcast ||
+         instr.opcode() == HloOpcode::kConcatenate ||
+         instr.opcode() == HloOpcode::kDynamicSlice ||
+         instr.opcode() == HloOpcode::kDynamicUpdateSlice ||
+         instr.opcode() == HloOpcode::kFusion ||
+         instr.opcode() == HloOpcode::kGather ||
+         instr.opcode() == HloOpcode::kIota || instr.opcode() == HloOpcode::kPad ||
+         instr.opcode() == HloOpcode::kReduce ||
+         instr.opcode() == HloOpcode::kReduceWindow ||
+         instr.opcode() == HloOpcode::kReshape ||
+         instr.opcode() == HloOpcode::kReverse ||
+         instr.opcode() == HloOpcode::kScatter ||
+         instr.opcode() == HloOpcode::kSlice ||
+         instr.opcode() == HloOpcode::kTranspose;
 }
 
 }  // namespace gpu

xla/service/gpu/gpu_fusible.h
@@ -24,6 +24,15 @@ limitations under the License.
 namespace xla {
 namespace gpu {
 
+// Whether 'instr' is a fusible operation
+// Don't fuse get-tuple-element on GPU: We can, but it's slower than not
+// fusing.  We never generate kernels for unfused GTEs.  Instead, if an
+// unfused GTE is an input to a kernel (including a fusion kernel), we
+// compute the address of the GTE at the top of the kernel.  Often we know the
+// address of the GTE result statically, so we can do this without chasing any
+// pointers.
+bool IsFusible(const HloInstruction& instr);
+
 // The code emitted for reduce-rooted input fusions (EmitReductionToVector)
 // suffers from poor data locality if the layouts of input parameters differ. In
 // such situtations it is better not to fuse. Only input params with

xla/service/gpu/instruction_fusion.cc
@@ -28,31 +28,6 @@ namespace gpu {
 
 namespace {
 
-bool IsFusible(const HloInstruction& hlo) {
-  // Don't fuse get-tuple-element on GPU: We can, but it's slower than not
-  // fusing.  We never generate kernels for unfused GTEs.  Instead, if an
-  // unfused GTE is an input to a kernel (including a fusion kernel), we
-  // compute the address of the GTE at the top of the kernel.  Often we know the
-  // address of the GTE result statically, so we can do this without chasing any
-  // pointers.
-  return (hlo.IsElementwise() && hlo.operand_count() > 0) ||
-         hlo.opcode() == HloOpcode::kBitcast ||
-         hlo.opcode() == HloOpcode::kBroadcast ||
-         hlo.opcode() == HloOpcode::kConcatenate ||
-         hlo.opcode() == HloOpcode::kDynamicSlice ||
-         hlo.opcode() == HloOpcode::kDynamicUpdateSlice ||
-         hlo.opcode() == HloOpcode::kFusion ||
-         hlo.opcode() == HloOpcode::kGather ||
-         hlo.opcode() == HloOpcode::kIota || hlo.opcode() == HloOpcode::kPad ||
-         hlo.opcode() == HloOpcode::kReduce ||
-         hlo.opcode() == HloOpcode::kReduceWindow ||
-         hlo.opcode() == HloOpcode::kReshape ||
-         hlo.opcode() == HloOpcode::kReverse ||
-         hlo.opcode() == HloOpcode::kScatter ||
-         hlo.opcode() == HloOpcode::kSlice ||
-         hlo.opcode() == HloOpcode::kTranspose;
-}
-
 bool IsIEEEFloatingPointScalarConstant(const HloInstruction* constant) {
   if (constant->opcode() != HloOpcode::kConstant ||
       !ShapeUtil::IsScalar(constant->shape())) {

commit f765113957718db24bc070242b121653e5225ad3

Allow loop fusion of fusion computations of non-loop type. Fixes bug introduced in commit c7e0c2805c70d14163a24c6d7058462f436497b2

xla/service/gpu/gpu_fusible.cc
@@ -150,8 +150,7 @@ bool IsLoopFusible(const HloInstruction& instr) {
          instr.opcode() == HloOpcode::kConcatenate ||
          instr.opcode() == HloOpcode::kDynamicSlice ||
          instr.opcode() == HloOpcode::kDynamicUpdateSlice ||
-         (instr.opcode() == HloOpcode::kFusion &&
-          instr.fusion_kind() == HloInstruction::FusionKind::kLoop) ||
+         instr.opcode() == HloOpcode::kFusion ||
          instr.opcode() == HloOpcode::kGather ||
          instr.opcode() == HloOpcode::kIota ||
          instr.opcode() == HloOpcode::kPad ||