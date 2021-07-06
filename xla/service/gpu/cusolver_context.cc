tensorflow/tensorflow

Updating tensorflow/compiler/xla/service/gpu/cusolver_context.cc to the rocblas_wrapper, instead of calling rocblas API directly.
My first attempt was to re-use the wrapper in the `rocblas_wrapper.h` file, but that resulted in the following compile errors (when running unit tests)

```
...
...
ERROR: /workspace/tensorflow/compiler/xla/service/gpu/tests/BUILD:613:13: Couldn't build file tensorflow/compiler/xla/service/gpu/tests/hlo_to_llvm_ir: Linking of rule '//tensorflow/compiler/xla/service/gpu/tests:hlo_to_llvm_ir' failed (Ex\
it 1): crosstool_wrapper_driver_is_not_gcc failed: error executing command
  (cd /home/jenkins/workspace/tensorflow/github-prs-rocmfork-develop-upstream/rocm-latest-ubuntu-gpu-single/bazel-ci_build-cache/.cache/bazel/_bazel_jenkins/eab0d61a99b6696edb3d2aff87b585e8/execroot/org_tensorflow && \
  exec env - \
    LD_LIBRARY_PATH='' \
    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin \
    PWD=/proc/self/cwd \
    PYTHON_BIN_PATH=/usr/bin/python3 \
    PYTHON_LIB_PATH=/usr/lib/python3/dist-packages \
    ROCBLAS_TENSILE_LIBPATH=/opt/rocm-4.2.0/lib/library \
    ROCM_PATH=/opt/rocm-4.2.0 \
    TF2_BEHAVIOR=1 \
  external/local_config_rocm/crosstool/clang/bin/crosstool_wrapper_driver_is_not_gcc @bazel-out/k8-opt/bin/tensorflow/compiler/xla/service/gpu/tests/hlo_to_llvm_ir-2.params)
Execution platform: @local_execution_config_platform//:platform
/usr/bin/ld.gold: error: bazel-out/k8-opt/bin/tensorflow/stream_executor/rocm/_objs/rocblas_plugin/rocm_blas.o: multiple definition of 'tensorflow::wrap::DynLoadShim__rocblas_create_handle::kName'
/usr/bin/ld.gold: bazel-out/k8-opt/bin/tensorflow/compiler/xla/service/gpu/_objs/cusolver_context/cusolver_context.o: previous definition here
/usr/bin/ld.gold: error: bazel-out/k8-opt/bin/tensorflow/stream_executor/rocm/_objs/rocblas_plugin/rocm_blas.o: multiple definition of 'tensorflow::wrap::DynLoadShim__rocblas_destroy_handle::kName'
/usr/bin/ld.gold: bazel-out/k8-opt/bin/tenso

tensorflow/compiler/xla/service/gpu/cusolver_context.cc
@@ -56,9 +56,67 @@ using cudaError_t = int;
 using cublasFillMode_t = rocblas_fill;
 using cusolverStatus_t = rocsolver_status;
 
-#define cusolverDnCreate rocblas_create_handle
-#define cusolverDnSetStream rocblas_set_stream
-#define cusolverDnDestroy rocblas_destroy_handle
+namespace rocblas_wrap {
+
+using stream_executor::internal::CachedDsoLoader::GetRocblasDsoHandle;
+using tensorflow::Env;
+
+#ifdef PLATFORM_GOOGLE
+#define ROCBLAS_API_WRAPPER(__name)           \
+  struct WrapperShim__##__name {              \
+    static const char* kName;                 \
+    template <typename... Args>               \
+    rocblas_status operator()(Args... args) { \
+      return ::__name(args...);               \
+    }                                         \
+  } __name;                                   \
+  const char* WrapperShim__##__name::kName = #__name;
+
+#else
+
+#define ROCBLAS_API_WRAPPER(__name)                                        \
+  struct DynLoadShim__##__name {                                           \
+    static const char* kName;                                              \
+    using FuncPtrT = std::add_pointer<decltype(::__name)>::type;           \
+    static void* GetDsoHandle() {                                          \
+      auto s = GetRocblasDsoHandle();                                      \
+      return s.ValueOrDie();                                               \
+    }                                                                      \
+    static FuncPtrT LoadOrDie() {                                          \
+      void* f;                                                             \
+      auto s =                                                             \
+          Env::Default()->GetSymbolFromLibrary(GetDsoHandle(), kName, &f); \
+      CHECK(s.ok()) << "could not find " << kName                          \
+                    << " in rocblas DSO; dlerror: " << s.error_message();  \
+      return reinterpret_cast<FuncPtrT>(f);                                \
+    }                                                                      \
+    static FuncPtrT DynLoad() {                                            \
+      static FuncPtrT f = LoadOrDie();                                     \
+      return f;                                                            \
+    }                                                                      \
+    template <typename... Args>                                            \
+    rocblas_status operator()(Args... args) {                              \
+      return DynLoad()(args...);                                           \
+    }                                                                      \
+  } __name;                                                                \
+  const char* DynLoadShim__##__name::kName = #__name;
+
+#endif
+
+// clang-format off
+#define FOREACH_ROCBLAS_API(__macro)	        \
+  __macro(rocblas_create_handle)		\
+  __macro(rocblas_destroy_handle)		\
+  __macro(rocblas_set_stream)
+// clang-format on
+
+FOREACH_ROCBLAS_API(ROCBLAS_API_WRAPPER)
+
+}  // namespace rocblas_wrap
+
+#define cusolverDnCreate rocblas_wrap::rocblas_create_handle
+#define cusolverDnSetStream rocblas_wrap::rocblas_set_stream
+#define cusolverDnDestroy rocblas_wrap::rocblas_destroy_handle
 
 #define CUBLAS_FILL_MODE_UPPER rocblas_fill_upper
 #define CUBLAS_FILL_MODE_LOWER rocblas_fill_lower

openxla/xla

xla/service/gpu/cusolver_context.cc
@@ -17,6 +17,64 @@ limitations under the License.
 
 #include "tensorflow/compiler/xla/util.h"
 
+namespace rocblas_wrap {
+
+using stream_executor::internal::CachedDsoLoader::GetRocblasDsoHandle;
+using tensorflow::Env;
+
+#ifdef PLATFORM_GOOGLE
+#define ROCBLAS_API_WRAPPER(__name)           \
+  struct WrapperShim__##__name {              \
+    static const char* kName;                 \
+    template <typename... Args>               \
+    rocblas_status operator()(Args... args) { \
+      return ::__name(args...);               \
+    }                                         \
+  } __name;                                   \
+  const char* WrapperShim__##__name::kName = #__name;
+
+#else
+
+#define ROCBLAS_API_WRAPPER(__name)                                        \
+  struct DynLoadShim__##__name {                                           \
+    static const char* kName;                                              \
+    using FuncPtrT = std::add_pointer<decltype(::__name)>::type;           \
+    static void* GetDsoHandle() {                                          \
+      auto s = GetRocblasDsoHandle();                                      \
+      return s.ValueOrDie();                                               \
+    }                                                                      \
+    static FuncPtrT LoadOrDie() {                                          \
+      void* f;                                                             \
+      auto s =                                                             \
+          Env::Default()->GetSymbolFromLibrary(GetDsoHandle(), kName, &f); \
+      CHECK(s.ok()) << "could not find " << kName                          \
+                    << " in rocblas DSO; dlerror: " << s.error_message();  \
+      return reinterpret_cast<FuncPtrT>(f);                                \
+    }                                                                      \
+    static FuncPtrT DynLoad() {                                            \
+      static FuncPtrT f = LoadOrDie();                                     \
+      return f;                                                            \
+    }                                                                      \
+    template <typename... Args>                                            \
+    rocblas_status operator()(Args... args) {                              \
+      return DynLoad()(args...);                                           \
+    }                                                                      \
+  } __name;                                                                \
+  const char* DynLoadShim__##__name::kName = #__name;
+
+#endif
+
+// clang-format off
+#define FOREACH_ROCBLAS_API(__macro)	        \
+  __macro(rocblas_create_handle)		\
+  __macro(rocblas_destroy_handle)		\
+  __macro(rocblas_set_stream)
+// clang-format on
+
+FOREACH_ROCBLAS_API(ROCBLAS_API_WRAPPER)
+
+}  // namespace rocblas_wrap
+
 namespace xla {
 namespace gpu {
 
@@ -48,9 +106,9 @@ struct GpuComplexT<std::complex<double>> {
 
 using gpuStream_t = hipStream_t;
 
-#define GpuSolverCreate rocblas_create_handle
-#define GpuSolverSetStream rocblas_set_stream
-#define GpuSolverDestroy rocblas_destroy_handle
+#define GpuSolverCreate rocblas_wrap::rocblas_create_handle
+#define GpuSolverSetStream rocblas_wrap::rocblas_set_stream
+#define GpuSolverDestroy rocblas_wrap::rocblas_destroy_handle
 
 template <>
 struct GpuComplexT<std::complex<float>> {