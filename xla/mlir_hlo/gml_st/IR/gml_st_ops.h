1a0f4ad6d15e15ede628e3e2b13ed7ffdd11aae5
David Dunleavy <ddunleavy@google.com>
2022-11-03 13:55:25 -0700

    Merge mlir_hlo/**/gml_st into mlir_hlo/gml_st
    We're moving XLA MLIR HLO codebase to a new directory layout, all dialects
    will follow this new convention.
    
    PiperOrigin-RevId: 485965309

--- xla/mlir_hlo/gml_st/IR/gml_st_ops.h
+++ xla/mlir_hlo/gml_st/IR/gml_st_ops.h
@@ -13,25 +13,34 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-// This file defines the operations used in the THLO dialect.
+// This file defines the operations used in the GML ST dialect.
 
-#ifndef MLIR_HLO_DIALECT_THLO_IR_THLO_OPS_H
-#define MLIR_HLO_DIALECT_THLO_IR_THLO_OPS_H
+#ifndef MLIR_HLO_DIALECT_GML_ST_IR_GML_ST_OPS_H
+#define MLIR_HLO_DIALECT_GML_ST_IR_GML_ST_OPS_H
 
-#include "gml_st/interfaces/tiling_interface.h"
-#include "mlir/Dialect/Linalg/IR/LinalgInterfaces.h"
-#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/Dialect/Utils/StructuredOpsUtils.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
-#include "mlir/Interfaces/DestinationStyleOpInterface.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Interfaces/LoopLikeInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/ViewLikeInterface.h"
 
 // Generated dialect declarations.
-#include "thlo/IR/thlo_dialect.h.inc"
+#include "gml_st/IR/gml_st_dialect.h.inc"
+
+// Generated custom type declarations.
+#define GET_TYPEDEF_CLASSES
+#include "gml_st/IR/gml_st_types.h.inc"
+
+// Generated attribute classes.
+#define GET_ATTRDEF_CLASSES
+#include "gml_st/IR/gml_st_attrs.h.inc"
 
 // Generated operation classes.
 #define GET_OP_CLASSES
-#include "thlo/IR/thlo_ops.h.inc"
+#include "gml_st/IR/gml_st_ops.h.inc"
 
-#endif  // MLIR_HLO_DIALECT_THLO_IR_THLO_OPS_H
+#endif  // MLIR_HLO_DIALECT_GML_ST_IR_GML_ST_OPS_H
--- xla/mlir_hlo/gml_st/interfaces/tiling_interface.cc
+++ xla/mlir_hlo/gml_st/interfaces/tiling_interface.cc
@@ -13,25 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-// This file defines the operations used in the THLO dialect.
-
-#ifndef MLIR_HLO_DIALECT_THLO_IR_THLO_OPS_H
-#define MLIR_HLO_DIALECT_THLO_IR_THLO_OPS_H
-
 #include "gml_st/interfaces/tiling_interface.h"
-#include "mlir/Dialect/Linalg/IR/LinalgInterfaces.h"
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Dialect.h"
-#include "mlir/IR/MLIRContext.h"
-#include "mlir/Interfaces/ControlFlowInterfaces.h"
-#include "mlir/Interfaces/DestinationStyleOpInterface.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
 
-// Generated dialect declarations.
-#include "thlo/IR/thlo_dialect.h.inc"
+namespace mlir {
+namespace gml_st {
 
-// Generated operation classes.
-#define GET_OP_CLASSES
-#include "thlo/IR/thlo_ops.h.inc"
+#include "gml_st/interfaces/tiling_interface.cc.inc"
 
-#endif  // MLIR_HLO_DIALECT_THLO_IR_THLO_OPS_H
+}  // namespace gml_st
+}  // namespace mlir
