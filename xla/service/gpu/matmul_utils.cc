c6fa2b7be56fae4c78ca2ef2d504b19d70b8e5df
Nathan Luehr <nluehr@nvidia.com>
2023-01-04 04:28:26 -0800

    PR #58877: [NVIDIA XLA] Fix Xla gemm_rewrite_tests when running on TF32 capable hardware
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/58877
    
    This PR extends the tsl::enable_tensor_float_32_execution API to the cublas and cublaslt integration within XLA. It then uses that API to configure various gemm_rewrite_tests to use float32 rather than TF32 since those tests' tolerances were developed assuming full precision.
    
    Attn: @reedwm @SandSnip3r
    Copybara import of the project:
    
    --
    218160f372a2202d72961ca56953f43f1564aaf0 by Nathan Luehr <nluehr@nvidia.com>:
    
    Make XLA cublasLt matmul respect tsl::tensor_float_32_enabled() API.
    
    --
    cd6f60e8d7669fe24dbb6bcd5ebc4c840cc3edbc by Nathan Luehr <nluehr@nvidia.com>:
    
    Disable TF32 in XLA gemm_rewrite_test cases.
    
    --
    1c21c3d7e47889023bfe5ce901434fca65a1562d by Nathan Luehr <nluehr@nvidia.com>:
    
    Generalize call target in UpcastingF16ToF32.
    
    --
    12a56fa0834e8f4c32cac58c88916e674e0372aa by Nathan Luehr <nluehr@nvidia.com>:
    
    Clang format
    
    Merging this change closes #58877
    
    PiperOrigin-RevId: 499451779

--- xla/service/gpu/matmul_utils.cc
+++ xla/service/gpu/matmul_utils.cc
@@ -38,6 +38,7 @@ limitations under the License.
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/statusor.h"
+#include "tsl/platform/tensor_float_32_utils.h"
 
 #if GOOGLE_CUDA
 #include "xla/stream_executor/cuda/cuda_blas_lt.h"
@@ -411,7 +412,11 @@ StatusOr<se::blas::ComputationType> GetBlasComputationType(
       return se::blas::ComputationType::kF32;
     case F32:  // fall-through
     case C64:
-      return se::blas::ComputationType::kTF32AsF32;
+      if (tsl::tensor_float_32_execution_enabled()) {
+        return se::blas::ComputationType::kTF32AsF32;
+      } else {
+        return se::blas::ComputationType::kF32;
+      }
     case F64:  // fall-through
     case C128:
       return se::blas::ComputationType::kF64;
