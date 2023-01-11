4e4d454f3a1b4b6062b6f98f3424c57a491ee35d
Michael Hudgins <michaelhudgins@google.com>
2023-01-11 09:01:40 -0800

    Reenable the macOS presubmit and add debug commands
    PiperOrigin-RevId: 501296347

--- .kokoro/macos/build.sh
+++ .kokoro/macos/build.sh
@@ -86,6 +86,9 @@ python -m pip install numpy==1.21.4
 TARGET_FILTER="-//xla/hlo/experimental/... -//xla/python_api/... -//xla/python/..."
 TAGS_FILTER="-no_oss,-gpu,-no_mac"
 
+# Print out disk stats
+df -h
+
 bazel test \
     --output_filter="" \
     --nocheck_visibility \
@@ -93,3 +96,5 @@ bazel test \
     --config=nonccl \
     --build_tag_filters=$TAGS_FILTER  --test_tag_filters=$TAGS_FILTER \
     -- //xla/... $TARGET_FILTER
+
+df -h
