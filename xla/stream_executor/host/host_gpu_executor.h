7b9454b99731e2271fa4a2db21b78552072a1550
Chris Jones <cjfj@google.com>
2023-01-19 07:40:26 -0800

    [STREAM_EXEC] Change `ThenDoHostCallback[WithStatus]` to take an `absl::AnyInvocable` callback.
    This removes the need for the callback to copyable.
    
    PiperOrigin-RevId: 503161544

--- xla/stream_executor/host/host_gpu_executor.h
+++ xla/stream_executor/host/host_gpu_executor.h
@@ -21,6 +21,7 @@ limitations under the License.
 
 #include <cstdint>
 
+#include "absl/functional/any_invocable.h"
 #include "xla/stream_executor/blas.h"
 #include "xla/stream_executor/host/host_stream.h"
 #include "xla/stream_executor/host/host_timer.h"
@@ -106,7 +107,7 @@ class HostExecutor : public internal::StreamExecutorInterface {
                                               uint64_t size) override;
 
   bool HostCallback(Stream* stream,
-                    std::function<tsl::Status()> callback) override;
+                    absl::AnyInvocable<tsl::Status() &&> callback) override;
 
   tsl::Status AllocateEvent(Event* event) override;
   tsl::Status DeallocateEvent(Event* event) override;
