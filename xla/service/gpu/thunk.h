97668cc70860cb609911216ced96ca100cb2727f
Rahul Joshi <jurahul@google.com>
2023-02-09 15:33:40 -0800

    Async cp with separate send and recv done prototype
    PiperOrigin-RevId: 508493352

--- xla/service/gpu/thunk.h
+++ xla/service/gpu/thunk.h
@@ -67,6 +67,8 @@ class Thunk {
     kNcclCollectivePermute,
     kNcclCollectivePermuteStart,
     kNcclCollectivePermuteDone,
+    kNcclCollectivePermuteRecvDone,
+    kNcclCollectivePermuteSendDone,
     kNcclReduceScatter,
     kNcclAllToAll,
     kOutfeed,
