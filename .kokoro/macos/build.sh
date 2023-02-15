d32490ac53004dab135e2ca4a38241ba34c19340
Adrian Kuegel <akuegel@google.com>
2023-02-14 23:21:34 -0800

    Filter out log spam in Kokoro builds from --nocheck_visibility
    PiperOrigin-RevId: 509739296

--- .kokoro/macos/build.sh
+++ .kokoro/macos/build.sh
@@ -92,9 +92,10 @@ TARGET_FILTER="-//xla/hlo/experimental/... -//xla/python_api/... -//xla/python/.
 TAGS_FILTER="-no_oss,-gpu,-no_mac,-nomac"
 
 bazel test \
+    --output_filter="" \
     --nocheck_visibility \
     --keep_going \
     --config=nonccl \
     --build_tag_filters=$TAGS_FILTER  --test_tag_filters=$TAGS_FILTER \
     --test_size_filters=small,medium \
-    -- //xla/... $TARGET_FILTER |& grep -v "violates visibility of"
+    -- //xla/... $TARGET_FILTER
