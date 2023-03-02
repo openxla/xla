f34d07b3375c3fe59e0b358af885c3c4072d72ae
A. Unique TensorFlower <gardener@tensorflow.org>
2023-03-02 11:04:54 -0800

    [hlo][sparse] Skip instructions that takes sparse operands in DotDecomposer to avoid crashes.
    PiperOrigin-RevId: 513586592

--- xla/service/dot_decomposer.cc
+++ xla/service/dot_decomposer.cc
@@ -22,7 +22,9 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/layout_util.h"
 #include "xla/permutation_util.h"
+#include "xla/service/sparse_util.h"
 #include "xla/shape_util.h"
 #include "xla/status_macros.h"
 #include "xla/types.h"
@@ -192,6 +194,11 @@ StatusOr<bool> DotDecomposer::Run(
        module->MakeNonfusionComputations(execution_threads)) {
     for (auto* instruction : computation->instructions()) {
       if (instruction->opcode() != HloOpcode::kDot) {
+        continue;
+      }
+      // Skips sparse instruction as DotDecomposer does not know how to handle
+      // sparse input yet.
+      if (SparseUtil::HasSparseInOut(instruction)) {
         continue;
       }
       const DotDimensionNumbers& dnums = instruction->dot_dimension_numbers();
