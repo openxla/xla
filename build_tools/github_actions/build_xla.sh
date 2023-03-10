6c73c6a4e950f8e17b4684a4e050e6806456e1ae
Geoffrey Martin-Noble <gcmn@google.com>
2023-03-09 18:30:09 -0800

    This is just a test of rollback message functionality
    Reverts a19287670a4614090813f22566de98d8766c2778
    
    PiperOrigin-RevId: 515497942

--- build_tools/github_actions/build_xla.sh
+++ build_tools/github_actions/build_xla.sh
@@ -16,11 +16,3 @@
 # ============================================================================
 
 echo "XLA build script is running..."
-
-start_time="$(date +%s)"
-echo "Start Time: ${start_time}"
-bazel build -c opt --nocheck_visibility --keep_going xla/tools:run_hlo_module
-end_time="$(date +%s)"
-echo "End Time: ${end_time}"
-runtime="$((end_time - start_time))"
-echo "Run time is ${runtime} seconds."
