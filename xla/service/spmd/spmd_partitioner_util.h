81180d5f94d4f7b074e94670ee1bb689d2538956
Marcello Maggioni <maggioni@google.com>
2023-03-28 12:21:46 -0700

    [XLA] Avoid emitting a pad when doing masked padwithvalue of iota.
    If its only right padded and we are masking the value we can just emit a bigger iota.
    
    PiperOrigin-RevId: 520100214

--- xla/service/spmd/spmd_partitioner_util.h
+++ xla/service/spmd/spmd_partitioner_util.h
@@ -26,6 +26,7 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/ir/hlo_sharding.h"
 #include "xla/hlo/utils/hlo_sharding_util.h"
