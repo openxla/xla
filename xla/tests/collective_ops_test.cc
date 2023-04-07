2caeede37a219ec69961d363ac590bd4eb473b4a
Rahul Joshi <jurahul@google.com>
2023-04-07 08:47:40 -0700

    [XLA/GPU] Disable async->sync collectives in collective_ops_test - Disable the pass to convert async collectives to sync in the test   as the test intends to be a unit test for exercising collectives.
    PiperOrigin-RevId: 522606446

--- xla/tests/collective_ops_test.cc
+++ xla/tests/collective_ops_test.cc
@@ -21,7 +21,6 @@ limitations under the License.
 #include "absl/types/span.h"
 #include "xla/literal.h"
 #include "xla/primitive_util.h"
-#include "xla/test.h"
 #include "xla/tests/hlo_test_base.h"
 #include "xla/tests/literal_test_util.h"
 #include "xla/tests/test_macros.h"
@@ -57,6 +56,15 @@ class CollectiveOpsTest : public HloTestBase {
   }
 
  protected:
+  DebugOptions GetDebugOptionsForTest() override {
+    DebugOptions debug_options = HloTestBase::GetDebugOptionsForTest();
+    // Disable async->sync collective conversion pass to enable unit testing
+    // of async collectives.
+    debug_options.add_xla_disable_hlo_passes(
+        "convert-async-collectives-to-sync");
+    return debug_options;
+  }
+
   std::unique_ptr<HloModule> MakeCrsModule(
       const Shape& shape, std::vector<std::vector<int64_t>> replica_groups,
       const HloModuleConfig& config, std::string op = "add",
