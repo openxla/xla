776ba802028347a38eaabe51c303601bf89ecf7b
A. Unique TensorFlower <gardener@tensorflow.org>
2023-05-04 16:52:21 -0700

    Split larger TopKs in batches of smaller ones.
    The two TopK implementations we have in XLA GPU both benefit from sharding a
    single large TopK into smaller batches.
    
    The Slice+Sort version uses bitonic sort, which is O(n lg(n)^2) and performs
    lg(n)^2 passes over memory. Therefore, TopK(N, K) into B TopK(N/B, K) is
    beneficial as long as K is small enough that the secondary TopK is negligible
    compared to the first stage.
    
    The small topk cuda kernel only uses a single block per batch dimension, so it
    benefits greatly from this transformation, as long as the resulting slice is
    long enough.
    
    This first pass heuristic is pretty conservative, limiting sharding to K*1MiB
    chunks.
    
    Benchmark Summary:
     * Splitting lead to significant latency decrease for all sizes.
     * For small topk (k<=16) the speedup ranges from 1.7x to 600x.
     * For large topk still using sort+slice, speedups in the 2.5-3.3x range.
     * Optimization triggers for N>1M, benchmarked N in [1M,4M,16M,64M,256M,1G]
    
    PiperOrigin-RevId: 529550607

--- xla/service/gpu/gpu_compiler.cc
+++ xla/service/gpu/gpu_compiler.cc
@@ -125,6 +125,7 @@ limitations under the License.
 #include "xla/service/gpu/runtime_intrinsics.h"
 #include "xla/service/gpu/scatter_slice_simplifier.h"
 #include "xla/service/gpu/topk_specializer.h"
+#include "xla/service/gpu/topk_splitter.h"
 #include "xla/service/gpu/tree_reduction_rewriter.h"
 #include "xla/service/gpu/variadic_op_splitter.h"
 #include "xla/service/hlo_computation_deduplicator.h"
@@ -416,6 +417,7 @@ Status GpuCompiler::OptimizeHloModule(HloModule* hlo_module,
   {
     HloPassPipeline pipeline("optimization");
     AddHloVerifier(&pipeline);
+    pipeline.AddPass<TopKSplitter>();
     pipeline.AddPass<TopkSpecializer>();
     pipeline.AddPass<TopkDecomposer>();
 
