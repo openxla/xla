9eb27a38d2d4a9a382988f977ab47b89ea9f3a64
Justin Lebar <jlebar@google.com>
2023-05-18 18:52:49 -0700

    Roll forward cl/522758420 [XLA] Teach ReshapeMover to handle broadcasts of non-scalars.
    This adds an option to disable reshape(broadcast(x)) where x is
    one-dimensional.  This is beneficial on GPUs, but not on all other platforms.
    
    Reverts 91a42a29131ba30b5c0792eba250c030143b7f8e
    
    PiperOrigin-RevId: 533313853

--- xla/service/gpu/cudnn_simplify_padding_test.cc
+++ xla/service/gpu/cudnn_simplify_padding_test.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include "xla/service/gpu/cudnn_pad_for_convolutions.h"
 #include "xla/service/gpu/cudnn_vectorize_convolutions.h"
 #include "xla/service/hlo_pass_fix.h"
+#include "xla/service/hlo_pass_pipeline.h"
 #include "xla/service/pattern_matcher.h"
 #include "xla/service/pattern_matcher_gmock.h"
 #include "xla/service/reshape_mover.h"
@@ -69,14 +70,14 @@ class CudnnSimplifyPaddingTest : public HloTestBase {
                         RunHloPass(CudnnSimplifyPadding(), module));
     VLOG(1) << "after simplify_padding:\n" << module->ToString();
 
-    TF_RETURN_IF_ERROR(RunHloPass(HloPassFix<ReshapeMover>(), module).status());
-    VLOG(1) << "after reshape mover:\n" << module->ToString();
-
-    TF_RETURN_IF_ERROR(RunHloPass(HloPassFix<AlgebraicSimplifier>(
-                                      AlgebraicSimplifierOptions()),
-                                  module)
-                           .status());
-    VLOG(1) << "after algsimp:\n" << module->ToString();
+    {
+      // reshape-mover expects to be run alongside algsimp.
+      HloPassFix<HloPassPipeline> pipeline("reshape-mover and algsimp");
+      pipeline.AddPass<ReshapeMover>();
+      pipeline.AddPass<AlgebraicSimplifier>(AlgebraicSimplifierOptions());
+      TF_RETURN_IF_ERROR(RunHloPass(pipeline, module).status());
+    }
+    VLOG(1) << "after reshape mover + algsimp:\n" << module->ToString();
 
     return changed;
   }
