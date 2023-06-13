da2c3f20e0e536d4b34fa0b4b8084179bfb4063a
A. Unique TensorFlower <gardener@tensorflow.org>
2023-06-13 15:11:41 -0700

    Add table ID frontend attribute
    PiperOrigin-RevId: 540086872

--- xla/side_effect_util.h
+++ xla/side_effect_util.h
@@ -68,6 +68,9 @@ extern const char kXlaPadValueAttr[];
 extern const char kXlaQuantizationHighValueAttr[];
 extern const char kXlaQuantizationLowValueAttr[];
 extern const char kXlaQuantizationNumBucketsValueAttr[];
+
+// XLA frontend attribute for table id.
+extern const char kXlaTableId[];
 }  // namespace xla
 
 #endif  // XLA_SIDE_EFFECT_UTIL_H_
