389464f73ba4a2e785591944b5045084c0272296
A. Unique TensorFlower <gardener@tensorflow.org>
2023-06-20 11:14:10 -0700

    [XLA/debuggability] Refactor and extend hlo_slicer so that (1) it would be easier to support backward slicing (2) it can take a lambda function which decides where to stop slicing (this feature can be used to define "frontier").
    PiperOrigin-RevId: 541989699

--- xla/tools/hlo_slicer.h
+++ xla/tools/hlo_slicer.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef XLA_TOOLS_HLO_SLICER_H_
 #define XLA_TOOLS_HLO_SLICER_H_
 
+#include <functional>
 #include <vector>
 
 #include "absl/container/flat_hash_map.h"
@@ -26,16 +27,53 @@ limitations under the License.
 
 namespace xla {
 
-// Conduct inter-computation forward program slicing, with  the provided
-// HLO instructions as the starting points and ROOT instruction in the ENTRY
-// computation as the ending point. It will return a map that maps from relevant
-// HLO computation to relevant HLO instructions (excluding the parts of the HLO
-// computations/instructions that are irrelevant).
-absl::flat_hash_map<const HloComputation*,
-                    absl::flat_hash_set<const HloInstruction*>>
-SliceModule(const HloModule* hlo_module,
-            std::vector<const HloInstruction*>& relevant_instructions,
-            bool ignore_control_predecessors = false);
+// Define HloSelector, which is a lambda that, given an HLO
+// instruction, returns true if selected, otherwise return false.
+using HloSelector = std::function<bool(const HloInstruction*)>;
+
+// The data structure capturing the outputs of forward/backward slicing.
+struct SliceOutputStruct {
+  // A map that maps from relevant HLO computation to relevant HLO
+  // instructions (excluding the parts of the HLO computations/instructions that
+  // are irrelevant).
+  absl::flat_hash_map<const HloComputation*,
+                      absl::flat_hash_set<const HloInstruction*>>
+      sliced_insts;
+
+  // Return the total number of the sliced instructions
+  int num_sliced_insts() { return _count_map_of_set(sliced_insts); }
+
+  // A map that maps from the computations to the instructions that form the
+  // slicing frontier.
+  absl::flat_hash_map<const HloComputation*,
+                      absl::flat_hash_set<const HloInstruction*>>
+      frontier_insts;
+
+  // Return the total number of the frontier instructions
+  int num_frontier_insts() { return _count_map_of_set(frontier_insts); }
+
+  int _count_map_of_set(
+      absl::flat_hash_map<const HloComputation*,
+                          absl::flat_hash_set<const HloInstruction*>>&
+          to_count) {
+    int count = 0;
+    for (const auto& [key, set] : to_count) {
+      count += set.size();
+    }
+    return count;
+  }
+};
+
+// Conduct inter-computation forward program slicing (i.e., in the direction
+// that from the specified nodes to the ROOT), with the starting points as the
+// provided HLO instructions (`relevant_instructions`), and with the ending
+// points (frontier) dictated by a lambda function (`hlo_selector`) that users
+// specify. If `ignore_control_dependency` is set as true, control dependency
+// will be ignored during slicing.
+SliceOutputStruct ForwardSliceModule(
+    const HloModule* hlo_module,
+    std::vector<const HloInstruction*>& relevant_instructions,
+    HloSelector hlo_selector, bool ignore_control_dependency = false);
 
 }  // namespace xla
 
