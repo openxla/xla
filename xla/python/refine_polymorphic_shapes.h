cf9457830e50e593a4a2aa6ec75727a9ea8a68a7
George Necula <gcnecula@gmail.com>
2023-07-05 00:06:58 -0700

    Copybara import of the project:
    --
    c82031a74e00d9f85ce3c6de5d1ce7e4a2bc2d6a by George Necula <gcnecula@gmail.com>:
    
    [shape_poly] Reimplement the shape constraint checking using shape assertions.
    
    Most of the functionality is for the JAX native serialization case.
    This relies on newly added functionality to xla_extension.refine_polymorphic_shapes
    that handles custom calls @static_assertion.
    
    As a beneficial side-effect now we get shape constraint checking for jax2tf
    graph serialization when the resulting function is executed in graph mode.
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/jax/pull/16615 from gnecula:poly_check2 c82031a74e00d9f85ce3c6de5d1ce7e4a2bc2d6a
    PiperOrigin-RevId: 545581038

--- xla/python/refine_polymorphic_shapes.h
+++ xla/python/refine_polymorphic_shapes.h
@@ -16,16 +16,27 @@ limitations under the License.
 #ifndef XLA_PYTHON_REFINE_POLYMORPHIC_SHAPES_H_
 #define XLA_PYTHON_REFINE_POLYMORPHIC_SHAPES_H_
 
+#include "absl/status/status.h"
 #include "llvm/Support/raw_ostream.h"
-#include "xla/status.h"
+#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 
 namespace xla {
 
 // Refines the dynamic shapes for a module whose "main" has static shapes
 // and all the intermediate dynamic shapes depend only on the input static
-// shapes. Serializes the refined module to the given `os`.
-xla::Status RefinePolymorphicShapes(llvm::StringRef module_str,
-                                    llvm::raw_ostream &os);
+// shapes. If `enable_shape_assertions` is false, then the shape assertions
+// are removed from the module, otherwise they are removed only if the
+// constraints hold, and result in an error otherwise.
+absl::Status RefinePolymorphicShapes(mlir::ModuleOp module,
+                                     bool enable_shape_assertions);
+
+// Like the above but with serialized input and output modules.
+absl::Status RefinePolymorphicShapes(llvm::StringRef module_str,
+                                     llvm::raw_ostream &os,
+                                     bool enable_shape_assertions);
+
+// Validates that the module has only static shapes.
+absl::Status ValidateStaticShapes(mlir::ModuleOp module);
 
 }  // namespace xla
 
