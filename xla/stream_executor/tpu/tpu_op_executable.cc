fa7a068f65d27fabda00efe6df6ce6899a3f2952
A. Unique TensorFlower <gardener@tensorflow.org>
2023-07-07 15:13:30 -0700

    Implement `TpuOpExecutable::fingerprint()` functionality via existing C APIs
    PiperOrigin-RevId: 546395936

--- xla/stream_executor/tpu/tpu_op_executable.cc
+++ xla/stream_executor/tpu/tpu_op_executable.cc
@@ -15,6 +15,8 @@ limitations under the License.
 
 #include "xla/stream_executor/tpu/tpu_op_executable.h"
 
+#include <memory>
+#include <utility>
 #include <vector>
 
 #include "xla/status.h"
@@ -22,6 +24,7 @@ limitations under the License.
 #include "xla/stream_executor/tpu/proto_helper.h"
 #include "xla/stream_executor/tpu/status_helper.h"
 #include "xla/stream_executor/tpu/tpu_api.h"
+#include "xla/stream_executor/tpu/tpu_ops_c_api.h"
 #include "xla/stream_executor/tpu/tpu_platform.h"
 #include "xla/stream_executor/tpu/tpu_platform_interface.h"
 #include "xla/xla_data.pb.h"
@@ -108,8 +111,8 @@ xla::Status TpuOpExecutable::LoadProgramAndEnqueueToStream(
 }
 
 absl::string_view TpuOpExecutable::fingerprint() const {
-  // TODO(skye): the fingerprint can be plumbed through via core_program_
-  LOG(FATAL) << "TpuOpExecutable::fingerprint() unimplemented";
+  TpuProgramFingerprint fingerprint = TpuProgram_GetFingerprint(core_program_);
+  return absl::string_view(fingerprint.bytes, fingerprint.size);
 }
 
 }  // namespace tensorflow
