ce0f559c24e3ffb303f7b206d756089b0d30d4c7
Reed Wanderman-Milne <reedwm@google.com>
2023-07-14 11:48:46 -0700

    Fix multi-GPU FP8 crash.
    The issue was that each device shared a cublasLtMatmulDesc_t. Normally this is fine since cublasLtMatmulDesc_t mostly just holds general information about the matmul like the compute type but for FP8, it also holds pointers in device memory to the scales. The pointers are set on the cublasLtMatmulDesc_t every time the matmul is run, in cuda_blas_lt.c. Since the pointers are different for each device, a race could occur where a GPU would try to access scale pointers in another GPU's memory.
    
    To fix, now each stream has a different cublasLtMatmulDesc_t.
    
    Fixes https://github.com/openxla/xla/issues/4001
    
    PiperOrigin-RevId: 548182888

--- xla/service/gpu/cublas_lt_matmul_thunk.h
+++ xla/service/gpu/cublas_lt_matmul_thunk.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef XLA_SERVICE_GPU_CUBLAS_LT_MATMUL_THUNK_H_
 #define XLA_SERVICE_GPU_CUBLAS_LT_MATMUL_THUNK_H_
 
+#include <memory>
 #include <optional>
 #include <utility>
 
@@ -24,13 +25,15 @@ limitations under the License.
 #include "xla/service/gpu/thunk.h"
 #include "xla/status.h"
 #include "xla/stream_executor/cuda/cuda_blas_lt.h"
+#include "tsl/platform/statusor.h"
 
 namespace xla {
 namespace gpu {
 
 class CublasLtMatmulThunk : public Thunk {
  public:
-  CublasLtMatmulThunk(ThunkInfo thunk_info, cublas_lt::MatmulPlan plan,
+  CublasLtMatmulThunk(ThunkInfo thunk_info, GemmConfig gemm_config,
+                      se::cuda::BlasLt::Epilogue epilogue,
                       int64_t algorithm_idx, BufferAllocation::Slice a_buffer,
                       BufferAllocation::Slice b_buffer,
                       BufferAllocation::Slice c_buffer,
@@ -46,7 +49,16 @@ class CublasLtMatmulThunk : public Thunk {
   Status ExecuteOnStream(const ExecuteParams& params) override;
 
  private:
-  cublas_lt::MatmulPlan plan_;
+  StatusOr<cublas_lt::MatmulPlan*> GetMatmulPlan(
+      const stream_executor::Stream* stream);
+
+  absl::Mutex matmul_plans_cache_mutex_;
+  absl::flat_hash_map<const stream_executor::Stream*,
+                      std::unique_ptr<cublas_lt::MatmulPlan>>
+      matmul_plans_cache_ ABSL_GUARDED_BY(matmul_plans_cache_mutex_);
+
+  GemmConfig gemm_config_;
+  se::cuda::BlasLt::Epilogue epilogue_;
   int64_t algorithm_idx_;
   BufferAllocation::Slice a_buffer_;
   BufferAllocation::Slice b_buffer_;
