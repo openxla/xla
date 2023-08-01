99f97f02ef6b9222c0db74b5a1376a1490d0d385
A. Unique TensorFlower <gardener@tensorflow.org>
2023-08-01 02:27:52 -0700

    [XLA] NFC: Refactor mlir::StackFrameIndexBuilder.
    The existing class is split into an MLIR-independent part and an MLIR-dependent adapter, and unit tests are added for the independent base class. This is prep work to also include stack frame information from Tensorflow for the legacy non-MLIR bridge - TF can use the new `BaseStackFrameIndexBuilder` directly without any MLIR dependency.
    
    PiperOrigin-RevId: 552739084

--- xla/translate/mhlo_to_hlo/stack_frame_index_builder.cc
+++ xla/translate/mhlo_to_hlo/stack_frame_index_builder.cc
@@ -19,16 +19,18 @@ limitations under the License.
 #include <stack>
 #include <string>
 #include <string_view>
-#include <tuple>
 #include <utility>
 
+#include "mlir/IR/Attributes.h"  // from @llvm-project
 #include "mlir/IR/BuiltinAttributes.h"  // from @llvm-project
 #include "mlir/IR/Location.h"  // from @llvm-project
+#include "mlir/Support/LLVM.h"  // from @llvm-project
 #include "xla/service/hlo.pb.h"
 
 namespace mlir {
 
-int FindId(std::string_view key, std::map<std::string_view, int> &index) {
+namespace {
+int FindId(const std::string_view key, std::map<std::string_view, int>& index) {
   auto entry_iterator = index.find(key);
   if (entry_iterator == index.end()) {
     return 0;
@@ -37,16 +39,13 @@ int FindId(std::string_view key, std::map<std::string_view, int> &index) {
   }
 }
 
-int StackFrameIndexBuilder::AddStackFrameLocation(
-    const mlir::NameLoc &name_location, int parent_frame_id) {
-  mlir::FileLineColLoc file_line_location =
-      cast<mlir::FileLineColLoc>(name_location.getChildLoc());
+bool IsFrameNameLocation(mlir::Location location) {
+  return isa<mlir::NameLoc>(location) &&
+         isa<mlir::FileLineColLoc>(cast<mlir::NameLoc>(location).getChildLoc());
+}
+}  // namespace
 
-  int line = file_line_location.getLine();
-  int column = file_line_location.getColumn();
-  std::string filename = file_line_location.getFilename().str();
-  std::string function_name = name_location.getName().str();
-
+int BaseStackFrameIndexBuilder::FindOrAddFileName(std::string filename) {
   int filename_id = FindId(filename, file_name_to_id_);
   if (filename_id == 0) {
     indexes_.add_file_names(std::move(filename));
@@ -54,6 +53,11 @@ int StackFrameIndexBuilder::AddStackFrameLocation(
     file_name_to_id_[indexes_.file_names(filename_id - 1)] = filename_id;
   }
 
+  return filename_id;
+}
+
+int BaseStackFrameIndexBuilder::FindOrAddFunctionName(
+    std::string function_name) {
   int function_name_id = FindId(function_name, function_name_to_id_);
   if (function_name_id == 0) {
     indexes_.add_function_names(std::move(function_name));
@@ -62,33 +66,38 @@ int StackFrameIndexBuilder::AddStackFrameLocation(
         function_name_id;
   }
 
-  auto location_tuple =
-      std::make_tuple(filename_id, function_name_id, line, column);
-  auto file_location_iterator = file_location_to_id_.find(location_tuple);
+  return function_name_id;
+}
+
+int BaseStackFrameIndexBuilder::FindOrAddFileLocation(
+    const xla::StackFrameIndexProto::FileLocation& file_location) {
+  HashableFileLocation local_loc{file_location.file_name_id(),
+                                 file_location.function_name_id(),
+                                 file_location.line(), file_location.column()};
+
+  auto file_location_iterator = file_location_to_id_.find(local_loc);
   int file_location_id = 0;
   if (file_location_iterator == file_location_to_id_.end()) {
-    auto file_location = indexes_.add_file_locations();
-    file_location->set_file_name_id(filename_id);
-    file_location->set_function_name_id(function_name_id);
-    file_location->set_line(line);
-    file_location->set_column(column);
-
+    *indexes_.add_file_locations() = file_location;
     file_location_id = indexes_.file_locations_size();
-    file_location_to_id_[location_tuple] = file_location_id;
+    file_location_to_id_[local_loc] = file_location_id;
   } else {
     file_location_id = file_location_iterator->second;
   }
 
-  auto frame_tuple = std::make_tuple(file_location_id, parent_frame_id);
-  auto stack_frame_iterator = frame_to_id_.find(frame_tuple);
+  return file_location_id;
+}
+
+int BaseStackFrameIndexBuilder::FindOrAddStackFrame(
+    const xla::StackFrameIndexProto::StackFrame& frame) {
+  HashableStackFrame local_frame{frame.file_location_id(),
+                                 frame.parent_frame_id()};
+  auto stack_frame_iterator = frame_to_id_.find(local_frame);
   int stack_frame_id = 0;
   if (stack_frame_iterator == frame_to_id_.end()) {
-    auto frame = indexes_.add_stack_frames();
-    frame->set_file_location_id(file_location_id);
-    frame->set_parent_frame_id(parent_frame_id);
-
+    *indexes_.add_stack_frames() = frame;
     stack_frame_id = indexes_.stack_frames_size();
-    frame_to_id_[frame_tuple] = stack_frame_id;
+    frame_to_id_[local_frame] = stack_frame_id;
   } else {
     stack_frame_id = stack_frame_iterator->second;
   }
@@ -96,9 +105,31 @@ int StackFrameIndexBuilder::AddStackFrameLocation(
   return stack_frame_id;
 }
 
-bool IsFrameNameLocation(mlir::Location location) {
-  return isa<mlir::NameLoc>(location) &&
-         isa<mlir::FileLineColLoc>(cast<mlir::NameLoc>(location).getChildLoc());
+int BaseStackFrameIndexBuilder::AddStackFrameAndReturnId(
+    std::string file_name, int line_number, std::string function_name,
+    int column_number, int parent_frame_id) {
+  int filename_id = FindOrAddFileName(std::move(file_name));
+  int function_name_id = FindOrAddFunctionName(std::move(function_name));
+
+  xla::StackFrameIndexProto::FileLocation file_location;
+  file_location.set_file_name_id(filename_id);
+  file_location.set_function_name_id(function_name_id);
+  file_location.set_line(line_number);
+  file_location.set_column(column_number);
+
+  int file_location_id = FindOrAddFileLocation(file_location);
+
+  xla::StackFrameIndexProto::StackFrame proto_frame;
+  proto_frame.set_file_location_id(file_location_id);
+  proto_frame.set_parent_frame_id(parent_frame_id);
+
+  int stack_frame_id = FindOrAddStackFrame(proto_frame);
+
+  return stack_frame_id;
+}
+
+xla::StackFrameIndexProto BaseStackFrameIndexBuilder::Build() const {
+  return std::move(indexes_);
 }
 
 int StackFrameIndexBuilder::AddCallStackAndGetFirstFrameId(
@@ -123,17 +154,29 @@ int StackFrameIndexBuilder::AddCallStackAndGetFirstFrameId(
     locations.push(cast<mlir::NameLoc>(root_loc));
   }
 
-  int parent_frame_id = StackFrameIndexBuilder::kInvalidIndex;
+  int parent_frame_id = BaseStackFrameIndexBuilder::kInvalidIndex;
   while (!locations.empty()) {
     mlir::NameLoc name_location = locations.top();
     locations.pop();
-    parent_frame_id = AddStackFrameLocation(name_location, parent_frame_id);
+
+    mlir::FileLineColLoc file_line_location =
+        cast<mlir::FileLineColLoc>(name_location.getChildLoc());
+
+    int line = file_line_location.getLine();
+    int column = file_line_location.getColumn();
+    std::string filename = file_line_location.getFilename().str();
+    std::string function_name = name_location.getName().str();
+
+    parent_frame_id = builder_.AddStackFrameAndReturnId(
+        std::move(filename), line, std::move(function_name), column,
+        parent_frame_id);
   }
 
   return parent_frame_id;
 }
 
 xla::StackFrameIndexProto StackFrameIndexBuilder::Build() const {
-  return std::move(indexes_);
+  return builder_.Build();
 }
+
 }  // namespace mlir
