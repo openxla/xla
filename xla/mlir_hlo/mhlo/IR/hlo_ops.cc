e7f6dbeb6e2ae6383a7bc7ce6512cc98c87eb149
Yishuang Pang <ypang@google.com>
2023-08-14 10:06:43 -0700

    Remove canonicalizers that are not applicable to mobile.
    PiperOrigin-RevId: 556829566

--- xla/mlir_hlo/mhlo/IR/hlo_ops.cc
+++ xla/mlir_hlo/mhlo/IR/hlo_ops.cc
@@ -1636,7 +1636,7 @@ struct ConvolutionIsDot : public OpRewritePattern<mhlo::ConvolutionOp> {
 
 void ConvolutionOp::getCanonicalizationPatterns(RewritePatternSet& results,
                                                 MLIRContext* context) {
-  results.add<ConvolutionIsDot>(context);
+  // results.add<ConvolutionIsDot>(context);
 }
 
 /*
@@ -6039,7 +6039,7 @@ struct ScatterFullReplace : public OpRewritePattern<ScatterOp> {
 
 void ScatterOp::getCanonicalizationPatterns(RewritePatternSet& results,
                                             MLIRContext* context) {
-  results.add<ScatterFullReplace>(context);
+  // results.add<ScatterFullReplace>(context);
 }
 
 //===----------------------------------------------------------------------===//
