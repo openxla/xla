c0334618552008465247ad0433d86e8e00deb990
Emilio Cota <ecg@google.com>
2023-08-21 11:54:52 -0700

    [xla:gpu] collectives: add missing headers and deps
    PiperOrigin-RevId: 558858308

--- xla/service/all_gather_combiner.cc
+++ xla/service/all_gather_combiner.cc
@@ -26,6 +26,7 @@ limitations under the License.
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/types/span.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_instructions.h"
@@ -35,6 +36,7 @@ limitations under the License.
 #include "xla/service/collective_combiner_utils.h"
 #include "xla/service/hlo_domain_map.h"
 #include "xla/shape_util.h"
+#include "xla/status.h"
 #include "xla/status_macros.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/errors.h"
