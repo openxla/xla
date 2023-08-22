openxla/xla

commit 0b3cbfe4bc7cd68dc20924bd878cdfb2faa1a169

[JAX] Implement the `stream` argument to jax.Array.__dlpack__ for CUDA GPU
Also implements jax.Array.__dlpack_device__. See
https://dmlc.github.io/dlpack/latest/python_spec.html

This requires plumbing the raw CUDA stream pointer through PJRT and
StreamExecutor (since the GPU PJRT implementation is still based on
SE). This is done via the new PJRT method
ExternalReference::WaitUntilBufferReadyOnStream.

I haven't plumbed this through the PJRT C API yet, because I'm still
debating whether this should be part of the main API or a GPU-specific
extension (plus either way it should probably be its own change).

PiperOrigin-RevId: 558245360

xla/python/dlpack.cc
@@ -267,8 +267,9 @@ StatusOr<PjRtDevice*> DeviceForDLDevice(const PjRtClient* cpu_client,
 
 }  // namespace
 
-StatusOr<py::capsule> BufferToDLPackManagedTensor(py::handle py_buffer,
-                                                  bool take_ownership) {
+StatusOr<py::capsule> BufferToDLPackManagedTensor(
+    py::handle py_buffer, bool take_ownership,
+    std::optional<std::intptr_t> stream) {
   ifrt::Array* ifrt_array = py::cast<xla::PyArray>(py_buffer).ifrt_array();
   auto pack = std::make_unique<DLPackTensor>();
   if (ifrt_array == nullptr) {
@@ -303,16 +304,18 @@ StatusOr<py::capsule> BufferToDLPackManagedTensor(py::handle py_buffer,
           "Cannot convert deleted/invalid buffer to DLPack tensor.");
     }
   } else {
-    // Block on outstanding operations, so that it is safe to read or mutate the
-    // returned buffer.
-    {
-      GlobalPyRefManager()->CollectGarbage();
-      py::gil_scoped_release gil_release;
+    // AcquireExternalReference may block; there are no API guarantees.
+    GlobalPyRefManager()->CollectGarbage();
+    py::gil_scoped_release gil_release;
+    TF_ASSIGN_OR_RETURN(pack->external_reference,
+                        pjrt_buffer->AcquireExternalReference());
+    if (stream) {
+      TF_RETURN_IF_ERROR(
+          pack->external_reference->WaitUntilBufferReadyOnStream(*stream));
+    } else {
       TF_RETURN_IF_ERROR(AwaitBuffersReady(ifrt_array));
     }
     pack->buffer_reference = py::reinterpret_borrow<py::object>(py_buffer);
-    TF_ASSIGN_OR_RETURN(pack->external_reference,
-                        pjrt_buffer->AcquireExternalReference());
   }
   dt.data = pack->external_reference->OpaqueDeviceMemoryDataPointer();
   pack->tensor.manager_ctx = pack.get();

xla/pjrt/pjrt_client.h
@@ -950,6 +950,13 @@ class PjRtBuffer {
     // Return opaque device memory pointer to root buffer.
     void* OpaqueDeviceMemoryDataPointer() const { return data_ptr_; }
 
+    // Stream is platform-specific. This is intended to support dlpack on GPU
+    // and is not expected to be implemented for all hardware platforms.
+    virtual Status WaitUntilBufferReadyOnStream(std::intptr_t stream) {
+      return Unimplemented(
+          "WaitUntilBufferReadyOnExternalStream is only implemented for GPU.");
+    }
+
    protected:
     void* data_ptr_;
   };

xla/pjrt/pjrt_stream_executor_client.cc
@@ -634,6 +634,14 @@ class ScopedHoldAsExternalReference : public PjRtBuffer::ExternalReference {
 
   ~ScopedHoldAsExternalReference() override = default;
 
+  Status WaitUntilBufferReadyOnStream(std::intptr_t stream) override {
+    for (const std::shared_ptr<BufferSequencingEvent>& event :
+         external_reference_->definition_events()) {
+      TF_RETURN_IF_ERROR(event->WaitForEventOnExternalStream(stream));
+    }
+    return OkStatus();
+  }
+
  private:
   PjRtStreamExecutorBuffer::ScopedHold external_reference_;
 };

xla/pjrt/tracked_device_buffer.cc
@@ -17,6 +17,7 @@ limitations under the License.
 
 #include <algorithm>
 #include <atomic>
+#include <cinttypes>
 #include <functional>
 #include <iterator>
 #include <memory>
@@ -77,6 +78,20 @@ void BufferSequencingEvent::WaitForEventOnStream(se::Stream* stream) {
 
   stream->ThenWaitFor(event_.event());
   streams_defined_on_.push_back(stream);
+}
+
+Status BufferSequencingEvent::WaitForEventOnExternalStream(
+    std::intptr_t stream) {
+  absl::MutexLock lock(&mu_);
+
+  // We cannot wait for an event until ThenRecordEvent has been called; on GPU
+  // newly created events are deemed to have already happened past.
+  // TODO(skyewm): do we need this? WaitForEventOnExternalStream is only
+  // implemented for GPU.
+  mu_.Await(
+      absl::Condition(this, &BufferSequencingEvent::EventHasBeenRecorded));
+
+  return event_.event()->WaitForEventOnExternalStream(stream);
 }
 
 bool BufferSequencingEvent::DefinedOn(se::Stream* stream) {

xla/pjrt/tracked_device_buffer.h
@@ -78,6 +78,11 @@ class BufferSequencingEvent {
   // called, blocks the calling thread until the event has been recorded.
   void WaitForEventOnStream(se::Stream* stream);
 
+  // Same as WaitForEventOnStream, but takes a raw platform-specific
+  // stream. Currently on implemented for CUDA and ROCM GPU, where stream is a
+  // GpuStreamHandle (e.g. a cudaStream_t).
+  Status WaitForEventOnExternalStream(std::intptr_t stream);
+
   // Returns true if the event is known to have occurred by the tail of
   // 'stream'. If RecordOnStream has not yet been called, blocks the calling
   // thread until the event has been recorded.

xla/python/dlpack.h
@@ -27,8 +27,13 @@ namespace xla {
 // If take_ownership is true, ownership of the buffer is handed to DLPack, and
 // the receiver may mutate the buffer as they see fit. Otherwise PjRt retains
 // ownership of the buffer and it should be immutable.
-StatusOr<pybind11::capsule> BufferToDLPackManagedTensor(pybind11::handle buffer,
-                                                        bool take_ownership);
+//
+// stream, if set, is a GPU stream, e.g. cudaStream_t for CUDA GPUs, that should
+// be synchronized to the buffer as per
+// https://dmlc.github.io/dlpack/latest/python_spec.html#python-specification-for-dlpack.
+StatusOr<pybind11::capsule> BufferToDLPackManagedTensor(
+    pybind11::handle buffer, bool take_ownership,
+    std::optional<std::intptr_t> stream);
 
 StatusOr<pybind11::object> DLPackManagedTensorToBuffer(
     const pybind11::capsule& tensor, std::shared_ptr<PyClient> cpu_client,

xla/python/xla.cc
@@ -789,7 +789,8 @@ PYBIND11_MODULE(xla_extension, m) {
 
   m.def("buffer_to_dlpack_managed_tensor",
         xla::ValueOrThrowWrapper(BufferToDLPackManagedTensor),
-        py::arg("buffer"), py::arg("take_ownership") = true);
+        py::arg("buffer"), py::arg("take_ownership") = true,
+        py::arg("stream") = py::none());
   m.def(
       "dlpack_managed_tensor_to_buffer",
       [](const pybind11::capsule& tensor, std::shared_ptr<PyClient> cpu_client,

xla/python/xla_client.py
@@ -44,7 +44,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes. In JAX, reference this via jax._src.lib.xla_extension_version.
-_version = 185
+_version = 186
 
 # Version number for MLIR:Python components.
 mlir_api_version = 54

commit f67276e1a65c67edc1e370d1cce6a5d582675577

Fix dlpack breakage introduced in https://github.com/openxla/xla/commit/0b3cbfe4bc7cd68dc20924bd878cdfb2faa1a169.
I accidentally released the GIL while trying to increase a Python
refcount, oops.

PiperOrigin-RevId: 559253549

xla/python/dlpack.cc
@@ -304,16 +304,18 @@ StatusOr<py::capsule> BufferToDLPackManagedTensor(
           "Cannot convert deleted/invalid buffer to DLPack tensor.");
     }
   } else {
-    // AcquireExternalReference may block; there are no API guarantees.
-    GlobalPyRefManager()->CollectGarbage();
-    py::gil_scoped_release gil_release;
-    TF_ASSIGN_OR_RETURN(pack->external_reference,
-                        pjrt_buffer->AcquireExternalReference());
-    if (stream) {
-      TF_RETURN_IF_ERROR(
-          pack->external_reference->WaitUntilBufferReadyOnStream(*stream));
-    } else {
-      TF_RETURN_IF_ERROR(AwaitBuffersReady(ifrt_array));
+    {
+      // AcquireExternalReference may block; there are no API guarantees.
+      GlobalPyRefManager()->CollectGarbage();
+      py::gil_scoped_release gil_release;
+      TF_ASSIGN_OR_RETURN(pack->external_reference,
+                          pjrt_buffer->AcquireExternalReference());
+      if (stream) {
+        TF_RETURN_IF_ERROR(
+            pack->external_reference->WaitUntilBufferReadyOnStream(*stream));
+      } else {
+        TF_RETURN_IF_ERROR(AwaitBuffersReady(ifrt_array));
+      }
     }
     pack->buffer_reference = py::reinterpret_borrow<py::object>(py_buffer);
   }