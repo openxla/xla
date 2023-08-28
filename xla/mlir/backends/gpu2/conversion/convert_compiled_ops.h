5e55951a0653be6b1b237f3c5cea7d0a0a24d548
Jacques Pienaar <jpienaar@google.com>
2023-08-28 15:00:48 -0700

    Use different input header to simplify export
    The files have an include path, reduces work needed on conversion.
    
    PiperOrigin-RevId: 560828283

--- xla/mlir/backends/gpu2/conversion/convert_compiled_ops.h
+++ xla/mlir/backends/gpu2/conversion/convert_compiled_ops.h
@@ -16,7 +16,7 @@ limitations under the License.
 #ifndef XLA_MLIR_BACKENDS_GPU2_CONVERSION_CONVERT_COMPILED_OPS_H_
 #define XLA_MLIR_BACKENDS_GPU2_CONVERSION_CONVERT_COMPILED_OPS_H_
 
-#include "third_party/iree/llvm-external-projects/iree-dialects/include/iree-dialects/Dialect/Input/InputOps.h"
+#include "iree-dialects/Dialect/Input/InputOps.h"
 #include "mlir/IR/PatternMatch.h"  // from @llvm-project
 #include "mlir/Transforms/DialectConversion.h"  // from @llvm-project
 #include "xla/mlir/backends/gpu2/conversion/de_bufferization.h"
