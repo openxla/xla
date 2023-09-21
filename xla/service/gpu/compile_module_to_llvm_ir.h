da717f36e0dc370fd7cfbde0f1eaf13e1ce4a07d
Eugene Zhulenev <ezhulenev@google.com>
2023-09-21 11:26:38 -0700

    [stream_executor] NFC: Remove stream_executor_pimpl includes and deps
    `stream_executor_pimpl` is an implementation detail of StreamExecutor, all clients should depend on exported public headers.
    
    https://github.com/openxla/xla/issues/5761
    
    PiperOrigin-RevId: 567366924

--- xla/service/gpu/compile_module_to_llvm_ir.h
+++ xla/service/gpu/compile_module_to_llvm_ir.h
@@ -31,7 +31,6 @@ limitations under the License.
 #include "xla/statusor.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/stream_executor.h"
-#include "xla/stream_executor/stream_executor_pimpl.h"
 #include "xla/util.h"
 
 namespace xla {
