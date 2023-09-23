openxla/xla

commit 198144efc4235162423939b2256505765f78e145

[XLA] Hand-roll the evaluator's sort
This allows us to not crash if the strict weak ordering requirement is not met. Also, try to detect comparators which violate strict weak ordering are passed in.

PiperOrigin-RevId: 567421978

xla/BUILD
@@ -141,7 +141,7 @@ cc_library(
         ":xla_data_proto_cc",
         "@com_google_absl//absl/base:core_headers",
         "@com_google_absl//absl/container:flat_hash_map",
-        "@com_google_absl//absl/meta:type_traits",
+        "@com_google_absl//absl/log:check",
         "@com_google_absl//absl/strings",
         "@tsl//tsl/platform:float8",
         "@tsl//tsl/platform:logging",

xla/comparison_util.h
@@ -18,15 +18,15 @@ limitations under the License.
 
 #include <cstdint>
 #include <functional>
-#include <limits>
 #include <optional>
 #include <ostream>
 #include <string>
 
-#include "absl/meta/type_traits.h"
+#include "absl/log/check.h"
 #include "absl/strings/string_view.h"
 #include "xla/primitive_util.h"
 #include "xla/statusor.h"
+#include "xla/types.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/logging.h"  // IWYU pragma: keep
@@ -185,28 +185,19 @@ class Comparison {
     }
   }
 
-  // Applies the comparison from this Comparison's direction and ordering for
-  // integral types.
-  template <typename T,
-            absl::enable_if_t<std::numeric_limits<T>::is_integer, int> = 0>
+  template <typename T>
   inline bool Compare(const T a, const T b) const {
     DCHECK(primitive_util::IsCanonicalRepresentation<T>(primitive_type_));
-    return GetComparator<T>()(a, b);
-  }
-
-  // Applies the comparison from this Comparison's direction and ordering
-  // for floating point types.
-  template <typename T,
-            absl::enable_if_t<!std::numeric_limits<T>::is_integer, int> = 0>
-  inline bool Compare(const T a, const T b) const {
-    DCHECK(primitive_util::IsCanonicalRepresentation<T>(primitive_type_));
-    if (IsTotalOrder()) {
-      //  -NaN < -Inf < -Finite < -0 < +0 < +Finite < +Inf < +NaN
-      // Reference:
-      // https://www.tensorflow.org/xla/operation_semantics#element-wise_comparison_operations
-      using R = SignedIntegerTypeForSizeType<sizeof(T)>;
-      return GetComparator<R>()(ToSignMagnitude(a), ToSignMagnitude(b));
+    if constexpr (is_specialized_floating_point_v<T>) {
+      if (IsTotalOrder()) {
+        //  -NaN < -Inf < -Finite < -0 < +0 < +Finite < +Inf < +NaN
+        // Reference:
+        // https://www.tensorflow.org/xla/operation_semantics#element-wise_comparison_operations
+        using R = SignedIntegerTypeForSizeType<sizeof(T)>;
+        return GetComparator<R>()(ToSignMagnitude(a), ToSignMagnitude(b));
+      }
     }
+    // Applies the comparison from this Comparison's direction and ordering.
     return GetComparator<T>()(a, b);
   }
 

xla/hlo/evaluator/BUILD
@@ -43,20 +43,23 @@ cc_library(
     hdrs = ["hlo_evaluator.h"],
     deps = [
         "//xla:array2d",
+        "//xla:comparison_util",
         "//xla:literal",
         "//xla:literal_util",
         "//xla:shape_util",
+        "//xla:status",
         "//xla:status_macros",
         "//xla:statusor",
         "//xla:types",
         "//xla:util",
-        "//xla:window_util",
         "//xla:xla_data_proto_cc",
         "//xla/hlo/ir:hlo",
         "//xla/hlo/utils:hlo_query",
         "//xla/service:call_graph",
         "//xla/service:compilation_environments",
         "//xla/service:dynamic_dimension_inference",
+        "//xla/service:hlo_module_config",
+        "//xla/service:logical_buffer",
         "//xla/service:pattern_matcher",
         "//xla/service:shape_inference",
         "//xla/service:tuple_points_to_analysis",
@@ -68,13 +71,20 @@ cc_library(
         "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/container:node_hash_map",
+        "@com_google_absl//absl/functional:function_ref",
+        "@com_google_absl//absl/memory",
+        "@com_google_absl//absl/numeric:bits",
         "@com_google_absl//absl/strings",
+        "@com_google_absl//absl/strings:cord",
+        "@com_google_absl//absl/strings:str_format",
         "@com_google_absl//absl/types:span",
+        "@eigen_archive//:eigen3",
         "@tsl//tsl/lib/core:bitmap",
+        "@tsl//tsl/platform:env",
         "@tsl//tsl/platform:errors",
         "@tsl//tsl/platform:float8",
         "@tsl//tsl/platform:logging",
-        "@tsl//tsl/platform:protobuf",
+        "@tsl//tsl/platform:platform_port",
         "@tsl//tsl/platform:status",
         "@tsl//tsl/platform:statusor",
         "@tsl//tsl/platform:types",

commit 96d1250d70c0bd6adf2778f31a266c1813fd107a

[XLA] Fix build
A bad dep added in 198144efc4235162423939b2256505765f78e145 broke the build.

PiperOrigin-RevId: 567864100

xla/BUILD
@@ -141,7 +141,6 @@ cc_library(
         ":xla_data_proto_cc",
         "@com_google_absl//absl/base:core_headers",
         "@com_google_absl//absl/container:flat_hash_map",
-        "@com_google_absl//absl/log:check",
         "@com_google_absl//absl/strings",
         "@tsl//tsl/platform:float8",
         "@tsl//tsl/platform:logging",

xla/comparison_util.h
@@ -22,7 +22,6 @@ limitations under the License.
 #include <ostream>
 #include <string>
 
-#include "absl/log/check.h"
 #include "absl/strings/string_view.h"
 #include "xla/primitive_util.h"
 #include "xla/statusor.h"