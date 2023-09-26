openxla/xla

commit 25aa1baa87abb259e3e0350737c4d39a715183a0

fix data_ptrs_vec.size() == data_uids_vec.size() check

xla/stream_executor/cuda/cuda_dnn.cc
@@ -6717,10 +6717,11 @@ class CudnnExecutionPlanRunner<void(Args...)>
       }
     }
 
-    // remove empty buffers from the list
-    data_ptrs_vec.erase(std::remove(data_ptrs_vec.begin(), data_ptrs_vec.end(), nullptr), data_ptrs_vec.end());
-    // ensure the size is equal after removing useless pointers
-    CHECK(data_ptrs_vec.size() == data_uids_vec.size());
+    if (!data_ptrs_vec.empty() && data_ptrs_vec.back() == nullptr &&
+        !has_activation_output_) {
+      data_ptrs_vec.pop_back();
+    }
+
     if (should_add_scalars) {
       data_uids_vec.insert(data_uids_vec.end(), scalar_input_uids_.begin(),
                            scalar_input_uids_.end());

commit c957ef3c70400e7845330dcad8e2385b1ca71e80

PR #5881: [XLA:GPU] remove unused buffers to fix cudnn graph
Imported from GitHub PR https://github.com/openxla/xla/pull/5881

This PR is to fix the bug introduced in commit https://github.com/openxla/xla/pull/5184/commits/25aa1baa87abb259e3e0350737c4d39a715183a0 in runner clean up PR: https://github.com/openxla/xla/pull/5184.

Unused buffers in fused attention need to be removed from data_vec so cudnn graph finalization doesn't error out.
Copybara import of the project:

--
0e3302504012794982a2a7acbc157767ccf07fcb by cjkkkk <ske@nvidia.com>:

rm unused buffers to fix cudnn graph

--
b16f756f7f71dd6f1b5959dfe29ff8ec619bae6f by cjkkkk <ske@nvidia.com>:

fix format

Merging this change closes #5881

COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/5881 from Cjkkkk:remove_unused_buffer b16f756f7f71dd6f1b5959dfe29ff8ec619bae6f
PiperOrigin-RevId: 568432367

xla/stream_executor/cuda/cuda_dnn.cc
@@ -6734,6 +6734,16 @@ class CudnnExecutionPlanRunner<void(Args...)>
       data_ptrs_vec.pop_back();
     }
 
+    if (sizeof...(Args) == 7 || sizeof...(Args) == 11) {
+      // is fused attention fwd and bwd
+      // remove empty buffers from the list
+      data_ptrs_vec.erase(
+          std::remove(data_ptrs_vec.begin(), data_ptrs_vec.end(), nullptr),
+          data_ptrs_vec.end());
+      // ensure the size is equal after removing useless pointers
+      CHECK(data_ptrs_vec.size() == data_uids_vec.size());
+    }
+
     if (should_add_scalars) {
       data_uids_vec.insert(data_uids_vec.end(), scalar_input_uids_.begin(),
                            scalar_input_uids_.end());