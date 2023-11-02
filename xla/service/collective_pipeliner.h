c2e29fb35bc344e1c35cc87bdf6dc2845af5e145
TJ <tjx@nvidia.com>
2023-11-02 20:24:09 +0000

    set acceptable_formatting and reuse_pipelined_op_buffer only in gpu compiler pipeline

--- xla/service/collective_pipeliner.h
+++ xla/service/collective_pipeliner.h
@@ -77,15 +77,11 @@ class CollectivePipeliner : public HloModulePass {
     HloPredicate should_process;
     // Filter acceptable formatting ops for for forward piplining to discard
     // cases that pipeline formatting operations that we don't want to support.
-    HloPredicate acceptable_formatting = [](const HloInstruction*) {
-      return true;
-    };
+    HloPredicate acceptable_formatting;
     // If the pipelined op has same input/output size the we reuse  the same
     // buffer we are storing the value in in the output loop for forward
     // pipelining. This function allows to not do it for certain ops.
-    HloPredicate reuse_pipelined_op_buffer = [](const HloInstruction*) {
-      return false;
-    };
+    HloPredicate reuse_pipelined_op_buffer;
   };
   static const char* const kInsertedByPreviousStep;
   static const char* const kSunkByPreviousStep;
