e8bdf87faeb45daa559fd2b5b780d0808917c6a2
Jeffrey Huynh <jthuynh@amazon.com>
2023-11-21 18:53:56 +0000

    Revert "Fix missing parenthesis"
    This reverts commit 5890278fc16c9f900782d32a92d40ecf548aea85.

--- xla/service/hlo_verifier_test.cc
+++ xla/service/hlo_verifier_test.cc
@@ -2364,6 +2364,7 @@ TEST_F(HloVerifierTest, ReduceScatterNonUniformGroups) {
               HasSubstr("Replica groups expected to be of uniform size"));
 }
 
+
 TEST_F(HloVerifierTest, ScatterInvalidScatterDim) {
   const char* const hlo_string = R"(
   HloModule Module
@@ -2390,6 +2391,7 @@ TEST_F(HloVerifierTest, ScatterInvalidScatterDim) {
   EXPECT_THAT(status.message(),
               HasSubstr("Invalid scatter_dims_to_operand_dims mapping"));
 }
+
 
 TEST_F(HloVerifierTest, VerifyBroadcastDimensionsOrder) {
   const char* const hlo = R"(
