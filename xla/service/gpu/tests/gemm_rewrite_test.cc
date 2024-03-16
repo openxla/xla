openxla/xla

commit 4e09e73967247dc0226fec1021bf794d80614059

Add native bfloat support for GPUs based on https://github.com/openxla/xla/pull/9246
PiperOrigin-RevId: 615454357

xla/service/gpu/BUILD
@@ -3346,6 +3346,7 @@ cc_library(
         "//xla:xla_data_proto_cc",
         "//xla/hlo/ir:hlo",
         "//xla/service:float_support",
+        "//xla/stream_executor:device_description",
         "@com_google_absl//absl/log:check",
     ],
 )

xla/service/gpu/gemm_fusion_autotuner.cc
@@ -475,7 +475,8 @@ absl::StatusOr<std::unique_ptr<HloModule>> TritonGemmAutotuneExtractor(
 
   if (config.split_k > 1) {
     TF_RETURN_IF_ERROR(MakeDotSplitKBatch(cloned_dot_fusion, config));
-    GpuFloatSupport bf16_support(BF16);
+    GpuFloatSupport bf16_support(gpu_device_info.cuda_compute_capability(),
+                                 BF16);
     FloatNormalization float_normalization(&bf16_support);
     TF_RETURN_IF_ERROR(float_normalization.Run(new_module.get()).status());
     GpuInstructionFusion instruction_fusion(/*may_duplicate=*/false,

xla/service/gpu/gpu_compiler.cc
@@ -1329,9 +1329,9 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
                       GetAutotuneConfig(stream_exec, debug_options, options,
                                         gpu_target_config));
   // Lambdas and related constants:
-  const GpuFloatSupport bf16_support(BF16);
-  const GpuFloatSupport f8e5m2_support(F8E5M2, F16);
-  const GpuFloatSupport f8e4m3fn_support(F8E4M3FN, F16);
+  const GpuFloatSupport bf16_support(gpu_version, BF16);
+  const GpuFloatSupport f8e5m2_support(gpu_version, F8E5M2, F16);
+  const GpuFloatSupport f8e4m3fn_support(gpu_version, F8E4M3FN, F16);
   const FloatSupport f8e4m3b11fnuz_support(F8E4M3B11FNUZ, F16);
   const FloatSupport f8e5m2fnuz_support(F8E5M2FNUZ, F16);
   const FloatSupport f8e4m3fnuz_support(F8E4M3FNUZ, F16);

xla/service/gpu/gpu_float_support.cc
@@ -15,10 +15,13 @@ limitations under the License.
 
 #include "xla/service/gpu/gpu_float_support.h"
 
+#include <variant>
+
 #include "absl/log/check.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/service/float_support.h"
+#include "xla/stream_executor/device_description.h"
 #include "xla/xla_data.pb.h"
 
 namespace xla {
@@ -73,6 +76,18 @@ bool GpuFloatSupport::IsSupported(const HloInstruction& hlo) const {
     // Other special ops.
     case HloOpcode::kBitcast:
       return true;
+    // Elementwise ops.
+    case HloOpcode::kAdd:
+    case HloOpcode::kSubtract:
+    case HloOpcode::kMultiply: {
+      if (LowPrecisionType() == BF16) {
+        auto* cuda_compute_capability =
+            std::get_if<se::CudaComputeCapability>(&compute_capability_);
+        return cuda_compute_capability != nullptr &&
+               cuda_compute_capability->IsAtLeastHopper();
+      }
+      return false;
+    }
     default:
       return false;
   }

xla/service/gpu/gpu_float_support.h
@@ -20,6 +20,7 @@ limitations under the License.
 
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/service/float_support.h"
+#include "xla/stream_executor/device_description.h"
 #include "xla/xla_data.pb.h"
 
 namespace xla {
@@ -27,9 +28,11 @@ namespace gpu {
 
 class GpuFloatSupport : public FloatSupport {
  public:
-  explicit GpuFloatSupport(PrimitiveType low_precision_type,
+  explicit GpuFloatSupport(se::GpuComputeCapability cc,
+                           PrimitiveType low_precision_type,
                            PrimitiveType high_precision_type = F32)
-      : FloatSupport(low_precision_type, high_precision_type) {}
+      : FloatSupport(low_precision_type, high_precision_type),
+        compute_capability_(cc) {}
 
   bool SupportsLowPrecisionOperand(const HloInstruction& hlo,
                                    int64_t operand_index) const override {
@@ -45,6 +48,8 @@ class GpuFloatSupport : public FloatSupport {
 
  private:
   bool IsSupported(const HloInstruction& hlo) const;
+
+  const se::GpuComputeCapability compute_capability_;
 };
 
 }  // namespace gpu

commit 8edbe17f55749cad4179c9ff8b85f972113cd706

Fix broken GemmRewriteTest.BF16GemmCodeGen on Hopper.
This was broken by https://github.com/openxla/xla/commit/4e09e73967247dc0226fec1021bf794d80614059.

The issue is that the optimized HLO was changed by having a native BF16 multiply, so the filecheck string had to be changed. Because the multiply is done in BF16 instead of FP32, the tolerance must also be lowered.

PiperOrigin-RevId: 616380852

xla/service/gpu/tests/gemm_rewrite_test.cc
@@ -257,18 +257,32 @@ ENTRY bf16gemm {
 }
   )";
 
-  MatchOptimizedHlo(hlo_text, R"(
-; CHECK:  [[P1:%[^ ]+]] = bf16[3]{0} parameter(1)
-; CHECK:  [[INSTR_1:%[^ ]+]] = f32[3]{0} convert([[P1]])
-; CHECK:  [[P0:%[^ ]+]] = bf16[3]{0} parameter(0)
-; CHECK:  [[INSTR_3:%[^ ]+]] = f32[3]{0} convert([[P0]])
-; CHECK:  [[INSTR_4:%[^ ]+]] = f32[3]{0} multiply([[INSTR_1]], [[INSTR_3]])
-; CHECK:  [[INSTR_5:%[^ ]+]] = f32[] constant(0)
-; CHECK:  [[INSTR_6:%[^ ]+]] = f32[] reduce([[INSTR_4]], [[INSTR_5]]), dimensions={0}, to_apply=[[INSTR_7:%[^ ]+]]
-; CHECK:  ROOT [[INSTR_8:%[^ ]+]] = bf16[] convert([[INSTR_6]])
-  )");
+  if (CudaOrRocmCheck(9, 0, Switch::False)) {
+    // The Hopper optimized HLO has a BF16 multiply instruction since Hopper has
+    // native BF16 multiply support.
+    MatchOptimizedHlo(hlo_text, R"(
+  ; CHECK:  [[P0:%[^ ]+]] = bf16[3]{0} parameter(0)
+  ; CHECK:  [[P1:%[^ ]+]] = bf16[3]{0} parameter(1)
+  ; CHECK:  [[INSTR_2:%[^ ]+]] = bf16[3]{0} multiply([[P0]], [[P1]])
+  ; CHECK:  [[INSTR_3:%[^ ]+]] = f32[3]{0} convert([[INSTR_2]])
+  ; CHECK:  [[INSTR_4:%[^ ]+]] = f32[] constant(0)
+  ; CHECK:  [[INSTR_5:%[^ ]+]] = f32[] reduce([[INSTR_3]], [[INSTR_4]]), dimensions={0}, to_apply=[[INSTR_6:%[^ ]+]]
+  ; CHECK:  ROOT [[INSTR_7:%[^ ]+]] = bf16[] convert([[INSTR_5]])
+    )");
+  } else {
+    MatchOptimizedHlo(hlo_text, R"(
+  ; CHECK:  [[P1:%[^ ]+]] = bf16[3]{0} parameter(1)
+  ; CHECK:  [[INSTR_1:%[^ ]+]] = f32[3]{0} convert([[P1]])
+  ; CHECK:  [[P0:%[^ ]+]] = bf16[3]{0} parameter(0)
+  ; CHECK:  [[INSTR_3:%[^ ]+]] = f32[3]{0} convert([[P0]])
+  ; CHECK:  [[INSTR_4:%[^ ]+]] = f32[3]{0} multiply([[INSTR_1]], [[INSTR_3]])
+  ; CHECK:  [[INSTR_5:%[^ ]+]] = f32[] constant(0)
+  ; CHECK:  [[INSTR_6:%[^ ]+]] = f32[] reduce([[INSTR_4]], [[INSTR_5]]), dimensions={0}, to_apply=[[INSTR_7:%[^ ]+]]
+  ; CHECK:  ROOT [[INSTR_8:%[^ ]+]] = bf16[] convert([[INSTR_6]])
+    )");
+  }
 
-  EXPECT_TRUE(RunAndCompare(hlo_text, ErrorSpec{1e-5, 1e-5}));
+  EXPECT_TRUE(RunAndCompare(hlo_text, ErrorSpec{1e-4, 1e-4}));
 }
 
 TEST_F(GemmRewriteTest, BF16Transpose) {