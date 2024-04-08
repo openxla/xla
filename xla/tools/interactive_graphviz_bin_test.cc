ece9eefa224a6d051bcac089fe2a9a393af16a2b
Raunak <mayank.kumar.raunak@intel.com>
2024-04-07 21:14:17 -0700

    Enable XLA Windows

--- xla/tools/interactive_graphviz_bin_test.cc
+++ xla/tools/interactive_graphviz_bin_test.cc
@@ -58,8 +58,13 @@ TEST(InteractiveGraphviz, CPU) {
 
   int status = proc.Communicate(&in, &out, &err);
   EXPECT_TRUE(WIFEXITED(status));
+  #if defined (_WIN32) || defined (_WIN64)
+  EXPECT_TRUE(status==0);
+  EXPECT_EQ(0, status);
+  #else
   EXPECT_EQ(0, WEXITSTATUS(status));
   ASSERT_THAT(err, testing::HasSubstr("Compiling module for Host"));
+  #endif
 }
 
 }  // namespace
