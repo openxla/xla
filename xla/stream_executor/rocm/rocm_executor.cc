openxla/xla

commit 3c5cf0ac2adbc0a651d3a2ec822e47927218b153

Move Dnn caching into the GpuExecutor, the only StreamExecutor that does them.
PiperOrigin-RevId: 623395712

xla/stream_executor/rocm/rocm_executor.cc
@@ -785,17 +785,25 @@ blas::BlasSupport* GpuExecutor::CreateBlas() {
   return status.value()(this);
 }
 
-dnn::DnnSupport* GpuExecutor::CreateDnn() {
+dnn::DnnSupport* GpuExecutor::AsDnn() {
+  absl::MutexLock lock(&mu_);
+  if (dnn_ != nullptr) {
+    return dnn_.get();
+  }
   PluginRegistry* registry = PluginRegistry::Instance();
   absl::StatusOr<PluginRegistry::DnnFactory> status =
-      registry->GetFactory<PluginRegistry::DnnFactory>(rocm::kROCmPlatformId);
+      registry->GetFactory<PluginRegistry::DnnFactory>(cuda::kCudaPlatformId);
   if (!status.ok()) {
     LOG(ERROR) << "Unable to retrieve DNN factory: "
                << status.status().message();
     return nullptr;
   }
 
-  return status.value()(this);
+  auto dnn = status.value()(this);
+
+  dnn_.reset(dnn);
+
+  return dnn_.get();
 }
 
 fft::FftSupport* GpuExecutor::CreateFft() {

xla/stream_executor/cuda/cuda_executor.cc
@@ -879,7 +879,11 @@ blas::BlasSupport* GpuExecutor::CreateBlas() {
   return status.value()(this);
 }
 
-dnn::DnnSupport* GpuExecutor::CreateDnn() {
+dnn::DnnSupport* GpuExecutor::AsDnn() {
+  absl::MutexLock lock(&mu_);
+  if (dnn_ != nullptr) {
+    return dnn_.get();
+  }
   PluginRegistry* registry = PluginRegistry::Instance();
   absl::StatusOr<PluginRegistry::DnnFactory> status =
       registry->GetFactory<PluginRegistry::DnnFactory>(cuda::kCudaPlatformId);
@@ -889,7 +893,11 @@ dnn::DnnSupport* GpuExecutor::CreateDnn() {
     return nullptr;
   }
 
-  return status.value()(this);
+  auto dnn = status.value()(this);
+
+  dnn_.reset(dnn);
+
+  return dnn_.get();
 }
 
 fft::FftSupport* GpuExecutor::CreateFft() {

xla/stream_executor/gpu/gpu_executor.h
@@ -274,7 +274,7 @@ class GpuExecutor : public internal::StreamExecutorInterface {
 
   fft::FftSupport* CreateFft() override;
 
-  dnn::DnnSupport* CreateDnn() override;
+  dnn::DnnSupport* AsDnn() override;
 
   std::unique_ptr<internal::EventInterface> CreateEventImplementation()
       override;
@@ -425,6 +425,13 @@ class GpuExecutor : public internal::StreamExecutorInterface {
   // Lookup map for alive streams, from raw stream pointers.
   absl::flat_hash_map<void*, Stream*> alive_gpu_streams_
       ABSL_GUARDED_BY(alive_gpu_streams_mu_);
+
+  // Reader/writer lock for mutable data structures on this object.
+  absl::Mutex mu_;
+
+  // Memoized DNN support object -- we only want to create this once when asked
+  // for a DNN interface.
+  std::unique_ptr<dnn::DnnSupport> dnn_ ABSL_GUARDED_BY(mu_);
 
   GpuExecutor(const GpuExecutor&) = delete;
   void operator=(const GpuExecutor&) = delete;

xla/stream_executor/stream_executor_internal.h
@@ -282,10 +282,12 @@ class StreamExecutorInterface {
   // not support FFT.
   virtual fft::FftSupport* CreateFft() { return nullptr; }
 
-  // Creates a new DnnSupport object, ownership is transferred to the caller.
-  // This may return null if the DNN initialization fails or this object does
-  // not support Dnns.
-  virtual dnn::DnnSupport* CreateDnn() { return nullptr; }
+  // Gets-or-creates (creates with memoization) a DnnSupport datatype that can
+  // be used for neural network routines on the current platform.
+  //
+  // Returns null if there was an error initializing the DNN support for the
+  // underlying platform.
+  virtual dnn::DnnSupport* AsDnn() { return nullptr; }
 
   // Each call creates a new instance of the platform-specific implementation of
   // the corresponding interface type.

xla/stream_executor/stream_executor_pimpl.cc
@@ -136,15 +136,7 @@ int64_t StreamExecutor::GetDeviceLoad() const {
   return implementation_->GetDeviceLoad();
 }
 
-dnn::DnnSupport* StreamExecutor::AsDnn() {
-  absl::MutexLock lock(&mu_);
-  if (dnn_ != nullptr) {
-    return dnn_.get();
-  }
-
-  dnn_.reset(implementation_->CreateDnn());
-  return dnn_.get();
-}
+dnn::DnnSupport* StreamExecutor::AsDnn() { return implementation_->AsDnn(); }
 
 blas::BlasSupport* StreamExecutor::AsBlas() {
   absl::MutexLock lock(&mu_);
@@ -358,11 +350,9 @@ bool StreamExecutor::AllocateStream(Stream* stream) {
 }
 
 void StreamExecutor::DeallocateStream(Stream* stream) {
-  dnn::DnnSupport* dnn;
-  {
-    absl::MutexLock lock(&mu_);
-    dnn = dnn_.get();
-  }
+  // TODO(b/301020144) Make this part of
+  // StreamExecutorInterface::DeallocateStream methods that care about DNNs.
+  dnn::DnnSupport* dnn = AsDnn();
   if (dnn) {
     dnn->NotifyStreamDestroyed(stream);
   }

xla/stream_executor/stream_executor_pimpl.h
@@ -443,10 +443,6 @@ class StreamExecutor {
   // for a BLAS interface.
   std::unique_ptr<blas::BlasSupport> blas_ ABSL_GUARDED_BY(mu_);
 
-  // Memoized DNN support object -- we only want to create this once when asked
-  // for an DNN interface.
-  std::unique_ptr<dnn::DnnSupport> dnn_ ABSL_GUARDED_BY(mu_);
-
   // Memoized FFT support object -- we only want to create this once when asked
   // for a FFT interface.
   std::unique_ptr<fft::FftSupport> fft_;

commit e1b1b628ac86d6872b269bfcbd5c258a4ba93466

PR #11398: [ROCm] Build fix
Imported from GitHub PR https://github.com/openxla/xla/pull/11398

This contains two fixes:
* A compilation fix for stream_executor/rocm/rocm_executor.cc after it got broken by  the commit https://github.com/openxla/xla/commit/3c5cf0ac2adbc0a651d3a2ec822e47927218b153
* A workaround that allows static_assert(false) to be compiled with compilers not implementing https://cplusplus.github.io/CWG/issues/2518.html
Copybara import of the project:

--
5cdd25c3190140fec619073d25a20952fed2fd8b by Eugene Kuznetsov <eugene.kuznetsov@amd.com>:

Build fix

--
b86d12ff02a2a6247a7f991f77dfdafb08a79703 by Eugene Kuznetsov <eugene.kuznetsov@amd.com>:

Workaround for static_assert needed for some compilers
(https://cplusplus.github.io/CWG/issues/2518.html)

Merging this change closes #11398

COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/11398 from ROCm:ci_rocm_fix_240410 b86d12ff02a2a6247a7f991f77dfdafb08a79703
PiperOrigin-RevId: 623471087

xla/stream_executor/rocm/rocm_executor.cc
@@ -792,7 +792,7 @@ dnn::DnnSupport* GpuExecutor::AsDnn() {
   }
   PluginRegistry* registry = PluginRegistry::Instance();
   absl::StatusOr<PluginRegistry::DnnFactory> status =
-      registry->GetFactory<PluginRegistry::DnnFactory>(cuda::kCudaPlatformId);
+      registry->GetFactory<PluginRegistry::DnnFactory>(rocm::kROCmPlatformId);
   if (!status.ok()) {
     LOG(ERROR) << "Unable to retrieve DNN factory: "
                << status.status().message();

xla/tests/complex_unary_op_samples.h
@@ -29,6 +29,9 @@ limitations under the License.
 
 namespace complex_unary_op_samples {
 
+template <class>
+constexpr bool dependent_false = false;
+
 template <typename T, int default_dps_deficiency = 0>
 struct Log1p {
   typedef std::complex<T> InputType;
@@ -1438,7 +1441,7 @@ struct Log1p {
           /* 288 */ {{inf, inf}, {inf, pi_4}, 1.e+00}};
       return table;
     } else {
-      static_assert(false); /* unreachable */
+      static_assert(dependent_false<T>); /* unreachable */
     }
   }
 };

xla/tests/complex_unary_op_test.cc
@@ -29,6 +29,9 @@ limitations under the License.
 namespace xla {
 namespace {
 
+template <class>
+constexpr bool dependent_false = false;
+
 class ComplexUnaryOpTest : public ClientLibraryTestBase {
  protected:
   template <typename T, size_t index, typename... Types>
@@ -64,7 +67,7 @@ class ComplexUnaryOpTest : public ClientLibraryTestBase {
     } else if constexpr (std::is_same_v<FloatType, double>) {
       atol = std::ldexp(1e-15f, precision_deficiency);
     } else {
-      static_assert(false);  // unreachable
+      static_assert(dependent_false<FloatType>);
     }
 
     XlaBuilder builder(TestName());