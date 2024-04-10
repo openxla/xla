90ad8b2730900d7f82b0fb1a83a73ffa2e452e0e
mraunak <83710963+mraunak@users.noreply.github.com>
2024-04-10 16:38:24 -0700

    Update run_hlo_module_bin_test.cc

--- xla/tools/run_hlo_module_bin_test.cc
+++ xla/tools/run_hlo_module_bin_test.cc
@@ -40,11 +40,11 @@ class RunHloModuleTest : public ::testing::Test {
 
     stdout_output_ = stderr_output_ = "";
     int status = proc.Communicate(nullptr, &stdout_output_, &stderr_output_);
-    exited_normally_ = WIFEXITED(status);
     #if defined (_WIN32) || defined (_WIN64)
     exited_normally_ = (status == 0);
     exit_status_ = (exited_normally_) ? 0 : status;
     #else
+    exited_normally_ = WIFEXITED(status);
     exit_status_ = exited_normally_ ? WEXITSTATUS(status) : -1;
     #endif
   }
