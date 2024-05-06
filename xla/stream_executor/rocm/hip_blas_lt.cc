4ad8abec5e0d0135f0844ae9e6acafc10ed84092
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2024-05-06 17:09:13 +0000

    fixing build brakes

--- xla/stream_executor/rocm/hip_blas_lt.cc
+++ xla/stream_executor/rocm/hip_blas_lt.cc
@@ -31,6 +31,7 @@ limitations under the License.
 #include "xla/stream_executor/rocm/hip_blas_lt.h"
 #include "xla/stream_executor/rocm/rocm_blas.h"
 #include "xla/stream_executor/stream.h"
+#include "xla/stream_executor/scratch_allocator.h"
 
 #define SET_ATTR(setter, handle, attr, value) \
   ToStatus(setter(handle, attr, &value, sizeof(decltype(value))), #setter)
@@ -398,11 +399,11 @@ absl::Status BlasLt::MatmulPlan::DoMatmul(
 absl::Status BlasLt::MatmulPlan::DoMatmul(
     Stream* stream, const void* alpha, DeviceMemoryBase a, DeviceMemoryBase b,
     const void* beta, DeviceMemoryBase c, DeviceMemoryBase d,
-    const MatmulAlgorithm& algorithm, ScratchAllocator& scratch_allocator,
+    const MatmulAlgorithm& algorithm, 
+    ScratchAllocator& scratch_allocator,
     DeviceMemoryBase bias, DeviceMemoryBase aux, DeviceMemoryBase a_scale,
     DeviceMemoryBase b_scale, DeviceMemoryBase c_scale,
     DeviceMemoryBase d_scale, DeviceMemoryBase d_amax,
-    ScratchAllocator& scratch_allocator,
     blas::ProfileResult* profile_result) const {
   return DoMatmul(stream, alpha, a, b, beta, c, d, algorithm, bias, aux,
                   a_scale, b_scale, c_scale, d_scale, d_amax, std::nullopt,
@@ -431,7 +432,7 @@ absl::Status BlasLt::MatmulPlan::DoMatmul(
     workspace_size = workspace.value().size();
     TF_RET_CHECK(workspace_size >= algorithm.workspace_size);
   } else if (algorithm.workspace_size > 0) {
-    TF_RET_CHECK(scratch_allocator.has_value())
+    TF_RET_CHECK(scratch_allocator.has_value());
     TF_ASSIGN_OR_RETURN(
         DeviceMemory<uint8_t> alloc,
         scratch_allocator.value()->AllocateBytes(algorithm.workspace_size));
@@ -577,7 +578,7 @@ absl::Status BlasLt::MatmulPlan::ExecuteOnStream(
         SCALENTYPE, HipToNativeT<ATYPE>::type, HipToNativeT<BTYPE>::type,    \
         HipToNativeT<CTYPE>::type, HipToNativeT<DTYPE>::type>(               \
         stream, alpha_, a, b, beta_, c, d, bias, aux, a_scale, b_scale,      \
-        c_scale, d_scale, d_amax, algorithm, scratch_allocator,              \
+        c_scale, d_scale, d_amax, algorithm, *scratch_allocator.value(),     \
         profile_result);                                                     \
   }
 
@@ -641,7 +642,6 @@ absl::Status BlasLt::MatmulPlan::ExecuteOnStream(
         float, HIP_R_8F_E5M2_FNUZ, HIP_R_8F_E4M3_FNUZ, HIP_R_16F, HIP_R_16F)
     TYPED_MATMUL_WITH_SCRATCH_ALLOCATOR(
         float, HIP_R_8F_E5M2_FNUZ, HIP_R_8F_E4M3_FNUZ, HIP_R_32F, HIP_R_32F)
-#endif
 
     // Other data types:
     TYPED_MATMUL_WITH_SCRATCH_ALLOCATOR(float, HIP_R_16BF, HIP_R_16BF,
