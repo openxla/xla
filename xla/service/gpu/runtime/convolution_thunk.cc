42d5839ffb47362a7f6789ba98ef50b31189dccc
Harsha HS <harsha.havanurshamsundara@amd.com>
2024-05-14 09:05:48 -0500

    [ROCm] Fix build break in `xla/service/gpu/runtime/convolution_thunk.cc`

--- xla/service/gpu/runtime/convolution_thunk.cc
+++ xla/service/gpu/runtime/convolution_thunk.cc
@@ -27,6 +27,7 @@ limitations under the License.
 #include "absl/synchronization/mutex.h"
 #include "absl/types/span.h"
 #include "xla/service/buffer_assignment.h"
+#include "xla/service/gpu/stream_executor_util.h"
 #include "xla/service/gpu/gpu_conv_runner.h"
 #include "xla/service/gpu/runtime/thunk.h"
 #include "xla/stream_executor/device_memory.h"
