426c2bee41800651ab191d830865c210d9f2cbea
Ruturaj4 <ruturaj.vaidya@amd.com>
2024-05-16 05:55:26 +0000

    [ROCm] Avoid hard coded code for mi300 tranpose fusion

--- xla/service/gpu/fusions/fusions.cc
+++ xla/service/gpu/fusions/fusions.cc
@@ -229,7 +229,8 @@ absl::StatusOr<std::unique_ptr<FusionInterface>> GetFusionEmitter(
       if (check_mlir_emitters(nullptr)) {
         return std::make_unique<MlirTransposeFusion>(analysis);
       }
-      return std::make_unique<TransposeFusion>(analysis);
+      return std::make_unique<TransposeFusion>(analysis.device_info(),
+          analysis);
     }
     case HloFusionAnalysis::EmitterFusionKind::kConcatenate: {
       if (check_mlir_emitters(nullptr)) {
