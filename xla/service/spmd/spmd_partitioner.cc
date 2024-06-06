b643b17a5d25d46e838a37fe87a4134b2cc512aa
Shraiysh Vaishay <svaishay@nvidia.com>
2024-06-06 10:00:10 -0700

    Add pipelined while loop annotator
    This patch recognises pipelined while loop using the rotate right pattern.
    It recognises a rotate-right pattern on sharded inputs and labels the
    surrounding while loop as a pipelined while loop. This is an unsafe
    optimization. This is hidden behind a debug flag so it wont be triggered in
    TPU pipelines or other GPU pipelines unexpectedly.

--- xla/service/spmd/spmd_partitioner.cc
+++ xla/service/spmd/spmd_partitioner.cc
@@ -4059,12 +4059,13 @@ absl::Status SpmdPartitioningVisitor::HandleWhile(HloInstruction* hlo) {
                                                 next_channel_id_, logger_,
                                                 call_graph_)
                          .status());
-  SetPartitionedHlo(hlo, [&] {
-    return b_.AddInstruction(HloInstruction::CreateWhile(
-        MakePartitionedShape(hlo->shape(), sharding), hlo->while_condition(),
-        hlo->while_body(),
-        GetPartitionedHlo(hlo->operand(0)).Reshard(sharding).hlo()));
-  });
+
+  HloInstruction* whileOp = b_.AddInstruction(HloInstruction::CreateWhile(
+      MakePartitionedShape(hlo->shape(), sharding), hlo->while_condition(),
+      hlo->while_body(),
+      GetPartitionedHlo(hlo->operand(0)).Reshard(sharding).hlo()));
+  hlo->SetupDerivedInstruction(whileOp);
+  SetPartitionedHlo(hlo, [&] { return whileOp; });
   return absl::OkStatus();
 }
 
@@ -5363,6 +5364,7 @@ absl::Status SpmdPartitioner::PreprocessHlos(
           if (amount < 0) {
             continue;
           }
+          TF_RETURN_IF_ERROR(HandleRotateRightWhilePreprocessing(computation));
           HloInstruction* to_rotate = lhs->mutable_operand(0);
           HloInstruction* rotate = computation->AddInstruction(
               CreateCustomCallSPMDInternal_RotateRight(to_rotate, dim, amount));
