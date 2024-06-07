9ebba63afde51d7ca1f9333678e15653a7a02d5d
Zoran Jovanovic <zjovanov@amd.com>
2024-06-07 15:46:14 +0000

    [ROCm] Fixed an issue with test cases from ir_emitter_triton_test.cc

--- xla/service/gpu/gpu_compiler.cc
+++ xla/service/gpu/gpu_compiler.cc
@@ -1346,8 +1346,9 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
     // and may rewrite quantized FP8 GEMMs as higher-precision GEMMs.
     pipeline.AddPass<GemmRewriter>(gpu_version, GetToolkitVersion(),
                                    /*f8_rewrite=*/true);
-    if (debug_options.xla_gpu_enable_triton_gemm() && cuda_cc != nullptr &&
-        cuda_cc->IsAtLeast(se::CudaComputeCapability::AMPERE)) {
+    if ((debug_options.xla_gpu_enable_triton_gemm() && cuda_cc != nullptr &&
+        cuda_cc->IsAtLeast(se::CudaComputeCapability::AMPERE)) ||
+        rocm_cc != nullptr) {
       pipeline.AddPass<GemvRewriter>();
       pipeline.AddPass<GemmFusion>(gpu_version);
     }
