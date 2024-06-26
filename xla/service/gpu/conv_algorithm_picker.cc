6608cc5347f2b04b56be33027c48db3ce67b3971
Philipp Hack <phack@nvidia.com>
2024-06-26 20:13:11 +0000

    Adds support for tuple outputs in the convolution algorithm picker.

--- xla/service/gpu/conv_algorithm_picker.cc
+++ xla/service/gpu/conv_algorithm_picker.cc
@@ -744,6 +744,7 @@ absl::StatusOr<AutotuneResult> GpuConvAlgorithmPicker::AutotuneOneConvRunner(
     for (int i = 0; i < result_buffers.size(); ++i) {
       Shape output_shape = MaybeTupleElementShape(
           runtime_arguments.rz_buffers.output_shape(), i);
+      XLA_SCOPED_LOGGING_TIMER_LEVEL("BufferComparator::CompareEqual", 2);
       BufferComparator comparator(output_shape,
                                   runtime_arguments.hlo_module_config);
       absl::StatusOr<bool> compare_result = comparator.CompareEqual(
