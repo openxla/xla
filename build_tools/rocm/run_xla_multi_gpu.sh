913b710d9332d5797d40c964bfaa65b52745fdfe
Harsha HS <Harsha.HavanurShamsundara@amd.com>
2024-07-03 03:48:05 -0700

    Add pjrt distributed tests and check for number of gpus

--- build_tools/rocm/run_xla_multi_gpu.sh
+++ build_tools/rocm/run_xla_multi_gpu.sh
@@ -23,9 +23,14 @@ N_BUILD_JOBS=$(grep -c ^processor /proc/cpuinfo)
 # out how many GPUs we have to test with.
 rocm-smi -i
 STATUS=$?
-if [ $STATUS -ne 0 ]; then TF_GPU_COUNT=4; else
+if [ $STATUS -ne 0 ]; then TF_GPU_COUNT=1; else
    TF_GPU_COUNT=$(rocm-smi -i|grep 'Device ID' |grep 'GPU' |wc -l)
 fi
+if [[ $TF_GPU_COUNT -lt 4 ]]; then
+    echo "Found only ${TF_GPU_COUNT} gpus, multi-gpu tests need atleast 4 gpus."
+    exit
+fi
+
 TF_TESTS_PER_GPU=1
 N_TEST_JOBS=$(expr ${TF_GPU_COUNT} \* ${TF_TESTS_PER_GPU})
 
@@ -47,7 +52,7 @@ fi
 export PYTHON_BIN_PATH=`which python3`
 export TF_NEED_ROCM=1
 export ROCM_PATH=$ROCM_INSTALL_DIR
-TAGS_FILTER="gpu,requires-gpu-amd,-requires-gpu-nvidia,-oss_excluded,-oss_serial"
+TAGS_FILTER="-requires-gpu-nvidia,-oss_excluded,-oss_serial"
 UNSUPPORTED_GPU_TAGS="$(echo -requires-gpu-sm{60,70,80,86,89,90}{,-only})"
 TAGS_FILTER="${TAGS_FILTER},${UNSUPPORTED_GPU_TAGS// /,}"
 
@@ -69,4 +74,6 @@ bazel \
     -- //xla/tests:collective_ops_e2e_test_gpu_amd_any \
        //xla/tests:collective_ops_test_gpu_amd_any \
        //xla/tests:replicated_io_feed_test_gpu_amd_any \
-       //xla/tools/multihost_hlo_runner:functional_hlo_runner_test_gpu_amd_any
+       //xla/tools/multihost_hlo_runner:functional_hlo_runner_test_gpu_amd_any \
+       //xla/pjrt/distributed:topology_util_test \
+       //xla/pjrt/distributed:client_server_test
