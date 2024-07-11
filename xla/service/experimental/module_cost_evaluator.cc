55c57116cc397aba9d449cb2358a838fcb926dc0
Deep Patel <deepatel@nvidia.com>
2024-07-11 14:22:53 -0700

    Creating empty functions for cost evaluation

---
+++ xla/service/experimental/module_cost_evaluator.cc
@@ -0,0 +1,12 @@
+// TODO: add license
+
+#include "xla/service/experimental/module_cost_evaluator.h"
+
+namespace xla {
+
+    uint64_t ModuleCostEvaluator::Evaluate(const HloModule* module) {
+        
+        return 0;
+    }
+
+} // xla
---
+++ xla/service/experimental/module_cost_evaluator.h
@@ -0,0 +1,25 @@
+// TODO: add license
+
+#ifndef XLA_SERVICE_EXPERIMENTAL_MODULE_COST_EVALUATOR_H_
+#define XLA_SERVICE_EXPERIMENTAL_MODULE_COST_EVALUATOR_H_
+
+#include "xla/hlo/ir/hlo_module.h"
+
+#include <stdint.h>
+
+// Class for evaluating the cost of a module based on various configurations
+
+namespace xla {
+
+// TODO: add ability to configure cost evaluator for various cost methods
+class ModuleCostEvaluator {
+public:
+    ModuleCostEvaluator() = default;
+    ~ModuleCostEvaluator() = default;
+
+    uint64_t Evaluate(const HloModule* module);
+};
+
+} // xla
+
+#endif // XLA_SERVICE_EXPERIMENTAL_MODULE_COST_EVALUATOR_H_
