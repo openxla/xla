da70c50c1a48bd0da9f3db2e0d185ce45bdeb8de
Ilia Sergachev <isergachev@nvidia.com>
2024-07-16 15:23:04 +0000

    [NFC][GPU] Cleanup the definition of BinaryMap.

--- xla/service/gpu/cudnn_fusion_compiler.cc
+++ xla/service/gpu/cudnn_fusion_compiler.cc
@@ -633,7 +633,7 @@ class CuDnnFusionVisitor : public DfsHloRewriteVisitor {
  public:
   explicit CuDnnFusionVisitor(
       se::dnn::DnnSupport& dnn_support,
-      CuDnnFusionCompiler::BinaryMap& compilation_results)
+      BinaryMap& compilation_results)
       : dnn_support_(dnn_support), compilation_results_(compilation_results) {}
 
   absl::Status HandleFusion(HloInstruction* hlo) override {
@@ -710,7 +710,7 @@ class CuDnnFusionVisitor : public DfsHloRewriteVisitor {
  private:
   se::dnn::DnnSupport& dnn_support_;
   // <HLO computation fingerprint, serialized compiled cuDNN graph>.
-  CuDnnFusionCompiler::BinaryMap& compilation_results_;
+  BinaryMap& compilation_results_;
   absl::flat_hash_map<std::string, int64_t> workspace_sizes_;
 };
 
