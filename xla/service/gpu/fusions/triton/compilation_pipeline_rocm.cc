openxla/xla

commit c8080173233a00e584449af7e03ccd6d535c36e9

Integrate Triton up to fd691c67 (https://github.com/openai/triton/commits/fd691c67ac20958a67693358186d877790f5f48f)
PiperOrigin-RevId: 657162423

xla/service/gpu/fusions/triton/compilation_pipeline_rocm.cc
@@ -106,6 +106,9 @@ absl::Status CreateTritonPipeline(
   // @triton//:third_party/amd/backend/compiler.py
   pm.addPass(mlir::triton::AMD::createDecomposeUnsupportedConversionsPass(
       ccRocm.gfx_version()));
+  const int custom_lds_size = 0;
+  pm.addPass(mlir::triton::AMD::createOptimizeLdsUsagePass(ccRocm.gfx_version(),
+                                                           custom_lds_size));
   pm.addPass(mlir::createConvertSCFToCFPass());
   pm.addPass(mlir::createConvertIndexToLLVMPass());
   pm.addPass(mt::gpu::createAllocateSharedMemoryPass());

third_party/triton/llvm_integration/series.bzl
@@ -8,6 +8,5 @@ LLVM nor MLIR integrator, please do not add any patches to this list.
 """
 
 llvm_patch_list = [
-    "//third_party/triton/llvm_integration:cl656020169.patch",
     # Add new patches just above this line
 ]

third_party/triton/workspace.bzl
@@ -8,8 +8,8 @@ load("//third_party/triton/xla_extensions:series.bzl", "extensions_files_patch_l
 def repo():
     """Imports Triton."""
 
-    TRITON_COMMIT = "cl652414579"
-    TRITON_SHA256 = "06afd4b310b0f8e48432564917f730f20f1f0c69bc15c8f114d7a1b1cb7215af"
+    TRITON_COMMIT = "cl655158651"
+    TRITON_SHA256 = "ac136693d2aeae327896d33e1a4de4852f25c1c2cdca49f85a2b9ac8b6d03b44"
     tf_http_archive(
         name = "triton",
         sha256 = TRITON_SHA256,

commit 028e492832022ee779da5ea5f160814f34118cbd

PR #15477: [ROCm] hot fix rocm build due to triton update LDS pass
Imported from GitHub PR https://github.com/openxla/xla/pull/15477

This build break is introduced by https://github.com/openxla/xla/pull/15257

and ROcm has a new optimized LDS pass on openai/triton https://github.com/triton-lang/triton/pull/3730

@xla-rotation
Copybara import of the project:

--
6f86fdbd090a4fc3fa2346ba6969d7ddeae773e3 by Chao Chen <cchen104@amd.com>:

updated rocm triton OptimizeLDSUsage pass due to https://github.com/triton-lang/triton/pull/3730

Merging this change closes #15477

COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/15477 from ROCm:ci_hotfix_20240730 6f86fdbd090a4fc3fa2346ba6969d7ddeae773e3
PiperOrigin-RevId: 657634867

xla/service/gpu/fusions/triton/compilation_pipeline_rocm.cc
@@ -107,7 +107,7 @@ absl::Status CreateTritonPipeline(
   pm.addPass(mlir::triton::AMD::createDecomposeUnsupportedConversionsPass(
       ccRocm.gfx_version()));
   const int custom_lds_size = 0;
-  pm.addPass(mlir::triton::AMD::createOptimizeLdsUsagePass(ccRocm.gfx_version(),
+  pm.addPass(mlir::triton::AMD::createOptimizeLDSUsagePass(ccRocm.gfx_version(),
                                                            custom_lds_size));
   pm.addPass(mlir::createConvertSCFToCFPass());
   pm.addPass(mlir::createConvertIndexToLLVMPass());