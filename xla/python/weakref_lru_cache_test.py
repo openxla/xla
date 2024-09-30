openxla/xla

commit e0d53caa0474e979551806d77a86aa5851b00f71

[XLA:Python] Fix deadlock in weakref_lru_cache.
If key hash/eq functions throw exceptions, a mutex might never be released.

(Not a new bug, but found while debugging a work-in-progress change.)

PiperOrigin-RevId: 611527188

xla/python/weakref_lru_cache.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include "absl/base/thread_annotations.h"
 #include "absl/cleanup/cleanup.h"
 #include "absl/container/node_hash_map.h"
 #include "absl/strings/str_cat.h"
@@ -183,7 +184,8 @@ class WeakrefLRUCache : public std::enable_shared_from_this<WeakrefLRUCache> {
                 .first->second);
   }
 
-  nb::object Call(nb::object weakref_key, nb::args args, nb::kwargs kwargs) {
+  nb::object Call(nb::object weakref_key, nb::args args,
+                  nb::kwargs kwargs) ABSL_NO_THREAD_SAFETY_ANALYSIS {
     nb::object context = cache_context_fn_();
     std::shared_ptr<Cache> cache_ptr = GetCache(UnboundWeakrefCacheEntry{
         weakref_key, this, static_cast<size_t>(xla::nb_hash(weakref_key))});
@@ -191,6 +193,7 @@ class WeakrefLRUCache : public std::enable_shared_from_this<WeakrefLRUCache> {
     ++total_queries_;
 
     bool inserted = false;
+    std::shared_ptr<CacheEntry> entry;
     {
       // Because the gil can be released during cache insertion, this forces
       // the lock order to be mu_ then gil so we must release the gil first.
@@ -199,12 +202,18 @@ class WeakrefLRUCache : public std::enable_shared_from_this<WeakrefLRUCache> {
       // cache insertion and then a second thread invalidates the cache order.
       mu_.Lock();
     }
-    Key key{context, args, kwargs};
-    auto entry = cache.GetOrCreateIfAbsent(key, [&inserted](const Key& key) {
-      inserted = true;
-      return std::make_shared<CacheEntry>();
-    });
-    mu_.Unlock();
+    {
+      // GetOrCreateIfAbsent calls into Python hash and equality functions,
+      // which may throw exceptions. The use of absl::Cleanup ensures mu_ is
+      // released if that happens.
+      absl::Cleanup unlock = [this]()
+                                 ABSL_UNLOCK_FUNCTION(mu_) { mu_.Unlock(); };
+      Key key{context, args, kwargs};
+      entry = cache.GetOrCreateIfAbsent(key, [&inserted](const Key& key) {
+        inserted = true;
+        return std::make_shared<CacheEntry>();
+      });
+    }
     if (!entry->completed.HasBeenNotified()) {
       if (inserted) {
         ++misses_;

xla/python/weakref_lru_cache_test.py
@@ -111,6 +111,25 @@ class WeakrefLRUCacheTest(absltest.TestCase):
     cache(wrkey, "arg2")
     self.assertLen(cache.cache_keys(), 2)
 
+  def testCrashingKey(self):
+    class WRKey:
+      pass
+
+    class CrashingKey:
+      # A key that raises exceptions if eq or hash is called.
+
+      def __eq__(self, other):
+        raise ValueError("eq")
+
+      def __hash__(self):
+        raise ValueError("hash")
+
+    cache = xla_client.weakref_lru_cache(lambda: None, lambda x, y: y, 2048)
+    wrkey = WRKey()
+    for _ in range(3):
+      with self.assertRaises(ValueError):
+        cache(wrkey, CrashingKey())
+
 
 if __name__ == "__main__":
   absltest.main()

xla/python/BUILD
@@ -919,6 +919,7 @@ cc_library(
     deps = [
         ":nb_helpers",
         # placeholder for index annotation deps
+        "@com_google_absl//absl/base:core_headers",
         "@com_google_absl//absl/cleanup",
         "@com_google_absl//absl/container:node_hash_map",
         "@com_google_absl//absl/strings",

commit d8c81c82b26966873d5068dad3a22329ca01105f

[XLA:Python] Fix crash introduced by https://github.com/openxla/xla/pull/10150
If a non-weakreferenceable object is passed, remove the entry again, otherwise we leave a garbage entry in the map.

(The original code did a double lookup for reasons that I had missed, but this is better.)

PiperOrigin-RevId: 680697569

xla/python/weakref_lru_cache.cc
@@ -158,21 +158,27 @@ class WeakrefLRUCache : public std::enable_shared_from_this<WeakrefLRUCache> {
     auto& value = it->second;
 
     value.cache = std::make_shared<Cache>(&lru_list_);
-    value.weakref =
-        nb::weakref(key.object, nb::cpp_function([this_weak = weak_from_this(),
-                                                  key](nb::handle weakref) {
-                      auto cache = this_weak.lock();
-                      if (cache == nullptr) {
-                        return;
-                      }
-                      auto it = cache->entries_.find(key);
-                      if (it == cache->entries_.end()) {
-                        return;
-                      }
-                      // Create temp-var to avoid re-entrant erase.
-                      auto tmp = std::move(it->second);
-                      cache->entries_.erase(it);
-                    }));
+    auto weakref_gc_callback = nb::cpp_function(
+        [this_weak = weak_from_this(), key](nb::handle weakref) {
+          auto cache = this_weak.lock();
+          if (cache == nullptr) {
+            return;
+          }
+          auto it = cache->entries_.find(key);
+          if (it == cache->entries_.end()) {
+            return;
+          }
+          // Create temp-var to avoid re-entrant erase.
+          auto tmp = std::move(it->second);
+          cache->entries_.erase(it);
+        });
+    PyObject* ref =
+        PyWeakref_NewRef(key.object.ptr(), weakref_gc_callback.ptr());
+    if (!ref) {
+      entries_.erase(it);
+      throw nb::python_error();
+    }
+    value.weakref = nb::steal<nb::weakref>(ref);
     return value.cache;
   }
 

xla/python/weakref_lru_cache_test.py
@@ -15,6 +15,7 @@
 
 import threading
 import time
+import weakref
 
 from absl.testing import absltest
 
@@ -110,6 +111,19 @@ class WeakrefLRUCacheTest(absltest.TestCase):
     cache(wrkey, "arg1")
     cache(wrkey, "arg2")
     self.assertLen(cache.cache_keys(), 2)
+
+  def testNonWeakreferenceableKey(self):
+    class NonWRKey:
+      __slots__ = ()
+
+    non_wr_key = NonWRKey()
+    with self.assertRaises(TypeError):
+      weakref.ref(non_wr_key)
+
+    cache = xla_client.weakref_lru_cache(lambda: None, lambda x: 2048)
+    for _ in range(100):
+      with self.assertRaises(TypeError):
+        cache(non_wr_key)
 
   def testCrashingKey(self):
     class WRKey: