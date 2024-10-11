055c409c51e9c4ef36f19da0c049cf6de8880b34
Yunlong Liu <yunlongl@x.ai>
2024-10-11 15:54:29 -0700

    Creates a new e2e test target for GPU.

--- xla/service/gpu/tests/gpu_compiler_e2e_test.cc
+++ xla/service/gpu/tests/gpu_compiler_e2e_test.cc
@@ -1,4 +1,4 @@
-/* Copyright 2023 The OpenXLA Authors.
+/* Copyright 2024 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@ limitations under the License.
 ==============================================================================*/
 
 #include "absl/strings/string_view.h"
+#include "xla/service/pattern_matcher.h"
+#include "xla/service/pattern_matcher_gmock.h"
 #include "xla/tests/hlo_test_base.h"
 #include "xla/tsl/lib/core/status_test_util.h"
 
@@ -21,21 +23,33 @@ namespace xla {
 namespace gpu {
 namespace {
 
-class SimpleOptimizationTest : public HloTestBase {};
+namespace m = match;
 
-TEST_F(SimpleOptimizationTest, OptimizeModule) {
+class GpuCompilerE2ETest : public HloTestBase {};
+
+TEST_F(GpuCompilerE2ETest, DegeneratedAllReduceRemoval) {
   constexpr absl::string_view kHloText = R"(
-HloModule t
+HloModule m
 
-ENTRY e {
-  p0 = f16[1,16,17,3] parameter(0)
-  p1 = s8[16,17,3] parameter(1)
-  cp1 = f16[16,17,3] convert(p1)
-  ROOT _ = f16[1,16,16] dot(p0, cp1),
-    lhs_contracting_dims={2,3}, rhs_contracting_dims={1,2}
-})";
+sum {
+  a = f32[] parameter(0)
+  b = f32[] parameter(1)
+  ROOT add.2 = f32[] add(a, b)
+}
 
-  TF_EXPECT_OK(GetOptimizedModule(kHloText).status());
+main {
+  p0 = f32[8,16] parameter(0), parameter_replication={false}
+  ROOT all-reduce = f32[8,16] all-reduce(p0),
+    channel_id=1,
+    use_global_device_ids=true,
+    replica_groups={{0},{1},{2},{3},{4},{5},{6},{7}},
+    to_apply=sum
+}
+)";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto optimized_module, GetOptimizedModule(kHloText));
+  EXPECT_THAT(optimized_module->entry_computation()->root_instruction(),
+              GmockMatch(m::Parameter(0)));
 }
 
 }  // namespace
