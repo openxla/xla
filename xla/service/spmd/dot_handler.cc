9b4ff4a7d445488412992abd60ccdc74689c84bb
TJ Xu <tjx@nvidia.com>
2024-10-14 20:28:08 +0000

    add test

--- xla/service/spmd/dot_handler.cc
+++ xla/service/spmd/dot_handler.cc
@@ -460,6 +460,9 @@ bool should_enable_windowed_einsum_with_threshold(
   if (options.total_flops_windowed_einsum_threshold != std::nullopt) {
     int64_t total_flops_threshold =
         options.total_flops_windowed_einsum_threshold.value();
+    LOG(ERROR) << "TOTAL FLOPS: " << total_flops;
+    LOG(ERROR) << "TOTAL total_flops_threshold: " << total_flops_threshold;
+
     return total_flops >= total_flops_threshold;
   } else {
     return operand_or_output_shape_size * 1024 * 1024 >=
@@ -677,7 +680,10 @@ std::optional<WindowedEinsumConfig> GetWindowedEinsumConfiguration(
   if (output_lhs_non_contracting_partitions == num_partitions &&
       output_sharding_transposed_to_match_lhs == lhs_sharding &&
       should_enable_windowed_einsum_with_threshold(
-          options, (rhs_shape_size + lhs_shape_size), rhs_shape_size) &&
+          options,
+          (ShapeUtil::ByteSizeOf(rhs->shape()) +
+           ShapeUtil::ByteSizeOf(lhs->shape())),
+          rhs_shape_size) &&
       (!rhs || check_users_sharding(rhs)) &&
       !disable_windowed_einsum(/*lhs_needs_ag=*/false, /*rhs_needs_ag=*/true) &&
       options.enable_windowed_einsum_for_all_gather) {
@@ -709,7 +715,10 @@ std::optional<WindowedEinsumConfig> GetWindowedEinsumConfiguration(
   if (output_rhs_non_contracting_partitions == num_partitions &&
       output_sharding_transposed_to_match_rhs == rhs_sharding &&
       should_enable_windowed_einsum_with_threshold(
-          options, (rhs_shape_size + lhs_shape_size), lhs_shape_size) &&
+          options,
+          (ShapeUtil::ByteSizeOf(rhs->shape()) +
+           ShapeUtil::ByteSizeOf(lhs->shape())),
+          lhs_shape_size) &&
       (!lhs || check_users_sharding(lhs)) &&
       !disable_windowed_einsum(/*lhs_needs_ag=*/true, /*rhs_needs_ag=*/false) &&
       options.enable_windowed_einsum_for_all_gather) {
@@ -743,7 +752,10 @@ std::optional<WindowedEinsumConfig> GetWindowedEinsumConfiguration(
       (output_lhs_non_contracting_partitions == num_partitions ||
        output_rhs_non_contracting_partitions == num_partitions) &&
       should_enable_windowed_einsum_with_threshold(
-          options, (rhs_shape_size + lhs_shape_size), output_shape_size) &&
+          options,
+          (ShapeUtil::ByteSizeOf(rhs->shape()) +
+           ShapeUtil::ByteSizeOf(lhs->shape())),
+          output_shape_size) &&
       !disable_windowed_einsum(/*lhs_needs_ag=*/false,
                                /*rhs_needs_ag=*/false) &&
       options.enable_windowed_einsum_for_reduce_scatter) {
