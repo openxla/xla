b7f192fcf4ab07f7c684e589e7ee3b7a2d7f0d77
Joshua Lang <joshualang@google.com>
2024-10-21 10:15:40 -0700

    Move default load_ir_module and nvptx_libdevice_path to default directory
    PiperOrigin-RevId: 688181339

--- xla/service/gpu/llvm_gpu_backend/nvptx_utils_test.cc
+++ xla/service/gpu/llvm_gpu_backend/nvptx_utils_test.cc
@@ -1,34 +1,26 @@
-/* Copyright 2017 The OpenXLA Authors.
-
+/* Copyright 2024 The OpenXLA Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include "xla/service/gpu/llvm_gpu_backend/nvptx_utils.h"
 
-#include "xla/service/gpu/llvm_gpu_backend/utils.h"
-
+#include <gtest/gtest.h>
 #include "tsl/platform/test.h"
 
-namespace xla {
-namespace gpu {
+namespace xla::gpu::nvptx {
 namespace {
 
-TEST(UtilsTest, TestReplaceFilenameExtension) {
-  ASSERT_EQ(ReplaceFilenameExtension("baz.tx", "cc"), "baz.cc");
-  ASSERT_EQ(ReplaceFilenameExtension("/foo/baz.txt", "cc"), "/foo/baz.cc");
-  ASSERT_EQ(ReplaceFilenameExtension("/foo/baz.", "-nvptx.dummy"),
-            "/foo/baz.-nvptx.dummy");
-  ASSERT_EQ(ReplaceFilenameExtension("/foo/baz", "cc"), "/foo/baz.cc");
+TEST(NvptxUtilsTest, CantFindCudaMessageTest) {
+  auto msg = CantFindCudaMessage("foo", "/bar");
+  EXPECT_NE(msg.length(), 0);
 }
 
 }  // namespace
-}  // namespace gpu
-}  // namespace xla
+}  // namespace xla::gpu::nvptx
