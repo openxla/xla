openxla/xla

commit bca1aebad8adcad1ac064fcbc9c8ee7bfe27951a

[xla:cpu] Execute grouped convolution in parallel
PiperOrigin-RevId: 668071112

xla/backends/cpu/runtime/convolution_thunk.cc
@@ -335,8 +335,7 @@ ConvolutionThunk::HandleEigen2DConvolution(const ExecuteParams& params,
                                            se::DeviceMemoryBase kernel,
                                            se::DeviceMemoryBase output) {
   auto dispatch = [&](auto type_tag, const auto& eigen_device,
-                      std::optional<std::function<void()>> done_callback =
-                          std::nullopt) {
+                      std::function<void()> done_callback = nullptr) {
     using scalar_type = decltype(type_tag);
     internal::EigenConv2D(
         eigen_device, static_cast<scalar_type*>(output.opaque()),
@@ -375,8 +374,7 @@ ConvolutionThunk::HandleEigen3DConvolution(const ExecuteParams& params,
                                            se::DeviceMemoryBase kernel,
                                            se::DeviceMemoryBase output) {
   auto dispatch = [&](auto type_tag, const auto& eigen_device,
-                      std::optional<std::function<void()>> done_callback =
-                          std::nullopt) {
+                      std::function<void()> done_callback = nullptr) {
     using scalar_type = decltype(type_tag);
     internal::EigenConv3D(
         eigen_device, static_cast<scalar_type*>(output.opaque()),

xla/backends/cpu/runtime/convolution_thunk_internal.h
@@ -16,14 +16,17 @@ limitations under the License.
 #ifndef XLA_BACKENDS_CPU_RUNTIME_CONVOLUTION_THUNK_INTERNAL_H_
 #define XLA_BACKENDS_CPU_RUNTIME_CONVOLUTION_THUNK_INTERNAL_H_
 
-#define EIGEN_USE_THREADS
-
+#include <algorithm>
+#include <cstdint>
 #include <functional>
-#include <optional>
 
+#include "xla/backends/cpu/runtime/concurrency.h"
+#include "xla/tsl/framework/convolution/eigen_spatial_convolutions.h"  // IWYU pragma: keep
+#include "tsl/platform/logging.h"
+
+#define EIGEN_USE_THREADS
 #include "Eigen/Core"
 #include "unsupported/Eigen/CXX11/Tensor"
-#include "xla/tsl/framework/convolution/eigen_spatial_convolutions.h"  // IWYU pragma: keep
 
 namespace xla::cpu::internal {
 
@@ -44,7 +47,7 @@ void EigenConv2D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
                  Eigen::Index padding_y_after, Eigen::Index lhs_x_dilation,
                  Eigen::Index lhs_y_dilation, Eigen::Index rhs_x_dilation,
                  Eigen::Index rhs_y_dilation, Eigen::Index feature_group_count,
-                 std::optional<std::function<void()>> done_callback) {
+                 std::function<void()> done_callback) {
   const Eigen::TensorMap<Eigen::Tensor<const ScalarType, 4, Eigen::RowMajor>,
                          Eigen::Aligned>
       input(lhs, input_batch, input_x, input_y, input_channels);
@@ -94,7 +97,8 @@ void EigenConv2D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
   kernel_dims[1] = feature_group_count;
   kernel_dims[2] = kernel_filters / feature_group_count;
 
-  for (Eigen::Index i = 0; i < feature_group_count; ++i) {
+  // Constructs convolution and output expressions for a given group index.
+  auto convolve_group = [=](int64_t i) {
     // The row and column dimensions must be flipped when passed to Eigen.
     auto convolved =
         input.reshape(input_reshaped_dims)
@@ -108,10 +112,37 @@ void EigenConv2D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
             .contract(kernel.reshape(kernel_dims).chip(i, 1), contract_dims)
             .reshape(post_contract_dims);
     auto output_reshaped = output.reshape(output_reshaped_dims).chip(i, 3);
-    if (done_callback.has_value()) {
-      output_reshaped.device(device, done_callback.value()) = convolved;
-    } else {
-      output_reshaped.device(device) = convolved;
+    return std::make_pair(output_reshaped, convolved);
+  };
+
+  // Use concurrent execution if we have a thread pool device.
+  constexpr bool use_thread_pool =
+      std::is_same_v<EigenDevice, Eigen::ThreadPoolDevice>;
+
+  // Done callback must be provided only if we use a thread pool device.
+  CHECK_EQ(use_thread_pool, static_cast<bool>(done_callback));  // Crash OK
+
+  if constexpr (use_thread_pool) {
+    // Although we schedule at most one tasks for each thread, individual
+    // convolution might also schedule more tasks into the same thread pool.
+    auto max_tasks = static_cast<Eigen::Index>(device.numThreads());
+    auto task_size = Eigen::numext::div_ceil(feature_group_count, max_tasks);
+    auto num_tasks = Eigen::numext::div_ceil(feature_group_count, task_size);
+
+    ScheduleAll(&device, num_tasks, [=, &device](int64_t task_index) {
+      int64_t start = task_index * task_size;
+      int64_t end = std::min(start + task_size, feature_group_count);
+      for (Eigen::Index i = start; i < end; ++i) {
+        auto [output, convolved] = convolve_group(i);
+        output.device(device, done_callback) = convolved;
+      }
+    });
+
+  } else {
+    // Convolve all feature groups sequentially in the caller thread.
+    for (Eigen::Index i = 0; i < feature_group_count; ++i) {
+      auto [output, convolved] = convolve_group(i);
+      output.device(device) = convolved;
     }
   }
 }
@@ -133,7 +164,7 @@ void EigenConv3D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
                  Eigen::Index lhs_y_dilation, Eigen::Index lhs_z_dilation,
                  Eigen::Index rhs_x_dilation, Eigen::Index rhs_y_dilation,
                  Eigen::Index rhs_z_dilation, Eigen::Index feature_group_count,
-                 std::optional<std::function<void()>> done_callback) {
+                 std::function<void()> done_callback) {
   using ConstTType =
       Eigen::TensorMap<Eigen::Tensor<const ScalarType, 5, Eigen::RowMajor>,
                        Eigen::Aligned>;
@@ -205,8 +236,8 @@ void EigenConv3D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
             .reshape(post_contract_dims);
 
     auto output_reshaped = output.reshape(output_reshaped_dims).chip(i, 4);
-    if (done_callback.has_value()) {
-      output_reshaped.device(device, done_callback.value()) = convolved;
+    if (done_callback) {
+      output_reshaped.device(device, done_callback) = convolved;
     } else {
       output_reshaped.device(device) = convolved;
     }
@@ -227,7 +258,7 @@ void EigenConv3D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
       Eigen::Index padding_y_after, Eigen::Index lhs_x_dilation,           \
       Eigen::Index lhs_y_dilation, Eigen::Index rhs_x_dilation,            \
       Eigen::Index rhs_y_dilation, Eigen::Index feature_group_count,       \
-      std::optional<std::function<void()>> done_callback)
+      std::function<void()> done_callback)
 
 CONV2D_EXTERN_TEMPLATE(Eigen::DefaultDevice, Eigen::half);
 CONV2D_EXTERN_TEMPLATE(Eigen::DefaultDevice, float);
@@ -252,8 +283,7 @@ CONV2D_EXTERN_TEMPLATE(Eigen::ThreadPoolDevice, float);
       Eigen::Index lhs_x_dilation, Eigen::Index lhs_y_dilation,                \
       Eigen::Index lhs_z_dilation, Eigen::Index rhs_x_dilation,                \
       Eigen::Index rhs_y_dilation, Eigen::Index rhs_z_dilation,                \
-      Eigen::Index feature_group_count,                                        \
-      std::optional<std::function<void()>> done_callback)
+      Eigen::Index feature_group_count, std::function<void()> done_callback)
 
 CONV3D_EXTERN_TEMPLATE(Eigen::DefaultDevice, Eigen::half);
 CONV3D_EXTERN_TEMPLATE(Eigen::DefaultDevice, float);
@@ -277,7 +307,7 @@ CONV3D_EXTERN_TEMPLATE(Eigen::ThreadPoolDevice, float);
       Eigen::Index padding_y_after, Eigen::Index lhs_x_dilation,           \
       Eigen::Index lhs_y_dilation, Eigen::Index rhs_x_dilation,            \
       Eigen::Index rhs_y_dilation, Eigen::Index feature_group_count,       \
-      std::optional<std::function<void()>> done_callback)
+      std::function<void()> done_callback)
 
 #define CONV3D_INSTANTIATE_TEMPLATE(DEVICE, SCALAR_TYPE)                       \
   template void xla::cpu::internal::EigenConv3D<DEVICE, SCALAR_TYPE>(          \
@@ -294,7 +324,6 @@ CONV3D_EXTERN_TEMPLATE(Eigen::ThreadPoolDevice, float);
       Eigen::Index lhs_x_dilation, Eigen::Index lhs_y_dilation,                \
       Eigen::Index lhs_z_dilation, Eigen::Index rhs_x_dilation,                \
       Eigen::Index rhs_y_dilation, Eigen::Index rhs_z_dilation,                \
-      Eigen::Index feature_group_count,                                        \
-      std::optional<std::function<void()>> done_callback)
+      Eigen::Index feature_group_count, std::function<void()> done_callback)
 
 #endif  // XLA_BACKENDS_CPU_RUNTIME_CONVOLUTION_THUNK_INTERNAL_H_

xla/service/cpu/runtime_conv2d.cc
@@ -43,7 +43,7 @@ ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv2DF32(
       kernel_channels, kernel_filters, output_rows, output_cols, row_stride,
       col_stride, padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, std::nullopt);
+      feature_group_count, nullptr);
 }
 
 ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv2DF16(
@@ -65,5 +65,5 @@ ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv2DF16(
       kernel_channels, kernel_filters, output_rows, output_cols, row_stride,
       col_stride, padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, std::nullopt);
+      feature_group_count, nullptr);
 }

xla/service/cpu/runtime_single_threaded_conv2d.cc
@@ -37,7 +37,7 @@ __xla_cpu_runtime_EigenSingleThreadedConv2DF16(
       kernel_filters, output_rows, output_cols, row_stride, col_stride,
       padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, std::nullopt);
+      feature_group_count, nullptr);
 }
 
 ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void
@@ -57,5 +57,5 @@ __xla_cpu_runtime_EigenSingleThreadedConv2DF32(
       kernel_filters, output_rows, output_cols, row_stride, col_stride,
       padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, std::nullopt);
+      feature_group_count, nullptr);
 }

xla/backends/cpu/runtime/BUILD
@@ -28,7 +28,10 @@ filegroup(
 
 filegroup(
     name = "runtime_hdrs",
-    srcs = ["convolution_thunk_internal.h"],
+    srcs = [
+        "concurrency.h",
+        "convolution_thunk_internal.h",
+    ],
     visibility = internal_visibility([":friends"]),
 )
 
@@ -275,25 +278,8 @@ cc_library(
 )
 
 cc_library(
-    name = "convolution_thunk_internal",
-    srcs = [
-        "convolution_thunk_f16.cc",
-        "convolution_thunk_f32.cc",
-    ],
-    hdrs = ["convolution_thunk_internal.h"],
-    copts = runtime_copts(),
-    visibility = internal_visibility([":friends"]),
-    deps = [
-        "//xla/tsl/framework/contraction:eigen_contraction_kernel",
-        "//xla/tsl/framework/convolution:eigen_helpers",
-        "@eigen_archive//:eigen3",
-    ],
-)
-
-cc_library(
     name = "concurrency",
     hdrs = ["concurrency.h"],
-    copts = runtime_copts(),
     deps = [
         "@eigen_archive//:eigen3",
         "@tsl//tsl/platform:logging",
@@ -313,6 +299,24 @@ xla_cc_test(
         "@tsl//tsl/platform:statusor",
         "@tsl//tsl/platform:test",
         "@tsl//tsl/platform:test_main",
+    ],
+)
+
+cc_library(
+    name = "convolution_thunk_internal",
+    srcs = [
+        "convolution_thunk_f16.cc",
+        "convolution_thunk_f32.cc",
+    ],
+    hdrs = ["convolution_thunk_internal.h"],
+    copts = runtime_copts(),
+    visibility = internal_visibility([":friends"]),
+    deps = [
+        ":concurrency",
+        "//xla/tsl/framework/contraction:eigen_contraction_kernel",
+        "//xla/tsl/framework/convolution:eigen_helpers",
+        "@eigen_archive//:eigen3",
+        "@tsl//tsl/platform:logging",
     ],
 )
 

xla/service/cpu/benchmarks/convolution_benchmark_test.cc
@@ -41,8 +41,7 @@ static void BM_Conv2D(benchmark::State& state) {
   int output_channels = state.range(6);
 
   // Padding values for 'SAME' padding. Only odd kernel sizes are supported.
-  CHECK(IsOdd(kernel_h));
-  CHECK(IsOdd(kernel_w));
+  CHECK(IsOdd(kernel_h) && IsOdd(kernel_w));
   int padding_h = (kernel_h - 1) / 2;
   int padding_w = (kernel_w - 1) / 2;
 
@@ -65,6 +64,7 @@ static void BM_Conv2D(benchmark::State& state) {
   // Filter format is HWIO.
   auto kernel_shape = ShapeUtil::MakeShape(
       ElementType, {kernel_h, kernel_w, input_channels, output_channels});
+
   auto input = *LiteralUtil::CreateRandomLiteral<ElementType>(
       input_shape, &engine, 1.0f, 0.1f);
   auto kernel = *LiteralUtil::CreateRandomLiteral<ElementType>(
@@ -81,21 +81,42 @@ static void BM_Conv2D(benchmark::State& state) {
 }
 
 static void BM_GroupedConv2D(benchmark::State& state) {
+  int batch = state.range(0);
+  int height = state.range(1);
+  int width = state.range(2);
+  int input_channels = state.range(3);
+  int kernel_h = state.range(4);
+  int kernel_w = state.range(5);
+  int output_channels = state.range(6);
+  int feature_group_count = state.range(7);
+
+  // Derive filter channels from input channels and feature group count.
+  int filter_channels = input_channels / feature_group_count;
+
+  // Padding values for 'SAME' padding. Only odd kernel sizes are supported.
+  CHECK(IsOdd(kernel_h) && IsOdd(kernel_w));
+  int padding_h = (kernel_h - 1) / 2;
+  int padding_w = (kernel_w - 1) / 2;
+
   std::string hlo_module = R"(
     HloModule TestModule
 
     ENTRY TestComputation {
-      %p0 = f32[1,45,45,1024] parameter(0)
-      %p1 = f32[5,5,1,1024] parameter(1)
-      ROOT conv = convolution(p0, p1), window={size=5x5 pad=2_2x2_2},
-        dim_labels=b01f_01io->b01f, feature_group_count=1024
+      %p0 = $input_shape parameter(0)
+      %p1 = $kernel_shape parameter(1)
+      ROOT conv = convolution(p0, p1), window={size=$window_size pad=$padding},
+        dim_labels=b01f_01io->b01f, feature_group_count=$feature_group_count
     }
   )";
 
   std::minstd_rand0 engine;
 
-  auto input_shape = ShapeUtil::MakeShape(F32, {1, 45, 45, 1024});
-  auto kernel_shape = ShapeUtil::MakeShape(F32, {5, 5, 1, 1024});
+  // Input format is NHWC.
+  auto input_shape =
+      ShapeUtil::MakeShape(F32, {batch, height, width, input_channels});
+  // Filter format is HWIO.
+  auto kernel_shape = ShapeUtil::MakeShape(
+      F32, {kernel_h, kernel_w, filter_channels, output_channels});
 
   auto input =
       *LiteralUtil::CreateRandomLiteral<F32>(input_shape, &engine, 1.0f, 0.1f);
@@ -104,7 +125,14 @@ static void BM_GroupedConv2D(benchmark::State& state) {
 
   std::vector<const Literal*> args = {&input, &kernel};
 
-  CHECK_OK(RunHloBenchmark(state, hlo_module, args));
+  CHECK_OK(RunHloBenchmark(
+      state, hlo_module, args,
+      {{"$input_shape", input_shape.ToString()},
+       {"$kernel_shape", kernel_shape.ToString()},
+       {"$window_size", absl::StrCat(kernel_h, "x", kernel_w)},
+       {"$padding", absl::StrCat(padding_h, "_", padding_h, "x", padding_w, "_",
+                                 padding_w)},
+       {"$feature_group_count", absl::StrCat(feature_group_count)}}));
 }
 
 // -------------------------------------------------------------------------- //
@@ -112,21 +140,21 @@ static void BM_GroupedConv2D(benchmark::State& state) {
 // -------------------------------------------------------------------------- //
 
 // Shapes from XLA convolution tests
-BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
+BENCHMARK(BM_Conv2D<F32>)
     ->MeasureProcessCPUTime()
     ->Args({8, 5, 5, 1, 1, 1, 32})
     ->Args({8, 5, 5, 4, 1, 1, 32})
     ->Args({8, 128, 128, 4, 1, 1, 8});
 
 // Shapes from TF convolution benchmarks.
-BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
+BENCHMARK(BM_Conv2D<F32>)
     ->MeasureProcessCPUTime()
     ->Args({8, 32, 32, 128, 1, 1, 1024})
     ->Args({16, 32, 32, 128, 1, 1, 1024})
     ->Args({32, 32, 32, 128, 1, 1, 1024});
 
 // Shapes similar to Eigen spatial convolution benchmarks.
-BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
+BENCHMARK(BM_Conv2D<F32>)
     ->MeasureProcessCPUTime()
     ->Args({32, 64, 64, 32, 1, 1, 64})
     ->Args({32, 256, 256, 4, 1, 1, 16})
@@ -138,21 +166,21 @@ BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
 // -------------------------------------------------------------------------- //
 
 // Shapes from XLA convolution tests
-BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
+BENCHMARK(BM_Conv2D<F32>)
     ->MeasureProcessCPUTime()
     ->Args({8, 5, 5, 1, 3, 3, 32})
     ->Args({8, 5, 5, 4, 3, 3, 32})
     ->Args({8, 128, 128, 4, 3, 3, 8});
 
 // Shapes from TF convolution benchmarks
-BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
+BENCHMARK(BM_Conv2D<F32>)
     ->MeasureProcessCPUTime()
     ->Args({8, 32, 32, 128, 3, 3, 1024})
     ->Args({16, 32, 32, 128, 3, 3, 1024})
     ->Args({32, 32, 32, 128, 3, 3, 1024});
 
 // Shapes similar to Eigen spatial convolution benchmarks.
-BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
+BENCHMARK(BM_Conv2D<F32>)
     ->MeasureProcessCPUTime()
     ->Args({32, 64, 64, 32, 3, 3, 64})
     ->Args({32, 256, 256, 4, 3, 3, 16})
@@ -163,7 +191,9 @@ BENCHMARK(BM_Conv2D<PrimitiveType::F32>)
 // Grouped convolution
 // -------------------------------------------------------------------------- //
 
-BENCHMARK(BM_GroupedConv2D)->MeasureProcessCPUTime();
+BENCHMARK(BM_GroupedConv2D)
+    ->MeasureProcessCPUTime()
+    ->Args({1, 45, 45, 1024, 5, 5, 1024, 1024});
 
 }  // namespace
 }  // namespace xla::cpu

xla/service/cpu/runtime_conv3d.cc
@@ -46,7 +46,7 @@ ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv3DF32(
       y_stride, z_stride, padding_x_before, padding_x_after, padding_y_before,
       padding_y_after, padding_z_before, padding_z_after, lhs_x_dilation,
       lhs_y_dilation, lhs_z_dilation, rhs_x_dilation, rhs_y_dilation,
-      rhs_z_dilation, feature_group_count, std::nullopt);
+      rhs_z_dilation, feature_group_count, nullptr);
 }
 
 ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv3DF16(
@@ -71,5 +71,5 @@ ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv3DF16(
       y_stride, z_stride, padding_x_before, padding_x_after, padding_y_before,
       padding_y_after, padding_z_before, padding_z_after, lhs_x_dilation,
       lhs_y_dilation, lhs_z_dilation, rhs_x_dilation, rhs_y_dilation,
-      rhs_z_dilation, feature_group_count, std::nullopt);
+      rhs_z_dilation, feature_group_count, nullptr);
 }

xla/service/cpu/runtime_single_threaded_conv3d.cc
@@ -40,7 +40,7 @@ __xla_cpu_runtime_EigenSingleThreadedConv3DF32(
       z_stride, padding_x_before, padding_x_after, padding_y_before,
       padding_y_after, padding_z_before, padding_z_after, lhs_x_dilation,
       lhs_y_dilation, lhs_z_dilation, rhs_x_dilation, rhs_y_dilation,
-      rhs_z_dilation, feature_group_count, std::nullopt);
+      rhs_z_dilation, feature_group_count, nullptr);
 }
 
 ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void
@@ -63,5 +63,5 @@ __xla_cpu_runtime_EigenSingleThreadedConv3DF16(
       z_stride, padding_x_before, padding_x_after, padding_y_before,
       padding_y_after, padding_z_before, padding_z_after, lhs_x_dilation,
       lhs_y_dilation, lhs_z_dilation, rhs_x_dilation, rhs_y_dilation,
-      rhs_z_dilation, feature_group_count, std::nullopt);
+      rhs_z_dilation, feature_group_count, nullptr);
 }

commit 542cc28ebe5b48377e6748a97b04d2a47eb115ef

[xla:cpu] Fix Conv2D in the classic runtime.
The error was introduced in https://github.com/openxla/xla/pull/16482
This is because the classic runtime uses a thread pool but doesn't provide a `done_callback` function.

Command to reproduce:
```
bazel test //xla/tests:convolution_test_cpu --test_env=XLA_FLAGS=--xla_cpu_use_thunk_runtime=false
```

PiperOrigin-RevId: 693377845

xla/backends/cpu/runtime/convolution_thunk.cc
@@ -19,7 +19,6 @@ limitations under the License.
 #include <cstdint>
 #include <functional>
 #include <memory>
-#include <optional>
 #include <utility>
 
 #include "absl/container/inlined_vector.h"
@@ -346,7 +345,7 @@ ConvolutionThunk::HandleEigen2DConvolution(const ExecuteParams& params,
         strides_.x, strides_.y, padding_before_.x, padding_after_.x,
         padding_before_.y, padding_after_.y, base_dilation_.x, base_dilation_.y,
         window_dilation_.x, window_dilation_.y, feature_group_count_,
-        std::move(done_callback));
+        std::move(done_callback), /*use_thunk_runtime=*/true);
   };
 
   if (options_.multi_threaded) {

xla/backends/cpu/runtime/convolution_thunk_internal.h
@@ -26,6 +26,7 @@ limitations under the License.
 
 #define EIGEN_USE_THREADS
 #include "Eigen/Core"
+#include "Eigen/ThreadPool"
 #include "unsupported/Eigen/CXX11/Tensor"
 
 namespace xla::cpu::internal {
@@ -47,7 +48,7 @@ void EigenConv2D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
                  Eigen::Index padding_y_after, Eigen::Index lhs_x_dilation,
                  Eigen::Index lhs_y_dilation, Eigen::Index rhs_x_dilation,
                  Eigen::Index rhs_y_dilation, Eigen::Index feature_group_count,
-                 std::function<void()> done_callback) {
+                 std::function<void()> done_callback, bool use_thunk_runtime) {
   const Eigen::TensorMap<Eigen::Tensor<const ScalarType, 4, Eigen::RowMajor>,
                          Eigen::Aligned>
       input(lhs, input_batch, input_x, input_y, input_channels);
@@ -119,8 +120,12 @@ void EigenConv2D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
   constexpr bool use_thread_pool =
       std::is_same_v<EigenDevice, Eigen::ThreadPoolDevice>;
 
-  // Done callback must be provided only if we use a thread pool device.
-  CHECK_EQ(use_thread_pool, static_cast<bool>(done_callback));  // Crash OK
+  // For thunk runtime, `done_callback` must be provided only if we use a thread
+  // pool device. This check is not true for classic runtime which does not
+  // support async execution.
+  if (use_thunk_runtime) {
+    CHECK_EQ(use_thread_pool, static_cast<bool>(done_callback));  // Crash OK
+  }
 
   if constexpr (use_thread_pool) {
     // Although we schedule at most one tasks for each thread, individual
@@ -129,14 +134,29 @@ void EigenConv2D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
     auto task_size = Eigen::numext::div_ceil(feature_group_count, max_tasks);
     auto num_tasks = Eigen::numext::div_ceil(feature_group_count, task_size);
 
-    ScheduleAll(&device, num_tasks, [=, &device](Eigen::Index task_index) {
-      Eigen::Index start = task_index * task_size;
-      Eigen::Index end = std::min(start + task_size, feature_group_count);
-      for (Eigen::Index i = start; i < end; ++i) {
-        auto [output, convolved] = convolve_group(i);
-        output.device(device, done_callback) = convolved;
-      }
-    });
+    if (use_thunk_runtime) {
+      ScheduleAll(&device, num_tasks, [=, &device](Eigen::Index task_index) {
+        Eigen::Index start = task_index * task_size;
+        Eigen::Index end = std::min(start + task_size, feature_group_count);
+        for (Eigen::Index i = start; i < end; ++i) {
+          auto [output, convolved] = convolve_group(i);
+          output.device(device, done_callback) = convolved;
+        }
+      });
+    } else {
+      Eigen::Barrier barrier(num_tasks);
+      ScheduleAll(
+          &device, num_tasks, [=, &device, &barrier](Eigen::Index task_index) {
+            Eigen::Index start = task_index * task_size;
+            Eigen::Index end = std::min(start + task_size, feature_group_count);
+            for (Eigen::Index i = start; i < end; ++i) {
+              auto [output, convolved] = convolve_group(i);
+              output.device(device) = convolved;
+            }
+            barrier.Notify();
+          });
+      barrier.Wait();
+    }
 
   } else {
     // Convolve all feature groups sequentially in the caller thread.
@@ -258,7 +278,7 @@ void EigenConv3D(const EigenDevice& device, ScalarType* out, ScalarType* lhs,
       Eigen::Index padding_y_after, Eigen::Index lhs_x_dilation,           \
       Eigen::Index lhs_y_dilation, Eigen::Index rhs_x_dilation,            \
       Eigen::Index rhs_y_dilation, Eigen::Index feature_group_count,       \
-      std::function<void()> done_callback)
+      std::function<void()> done_callback, bool use_thunk_runtime)
 
 CONV2D_EXTERN_TEMPLATE(Eigen::DefaultDevice, Eigen::half);
 CONV2D_EXTERN_TEMPLATE(Eigen::DefaultDevice, float);
@@ -307,7 +327,7 @@ CONV3D_EXTERN_TEMPLATE(Eigen::ThreadPoolDevice, float);
       Eigen::Index padding_y_after, Eigen::Index lhs_x_dilation,           \
       Eigen::Index lhs_y_dilation, Eigen::Index rhs_x_dilation,            \
       Eigen::Index rhs_y_dilation, Eigen::Index feature_group_count,       \
-      std::function<void()> done_callback)
+      std::function<void()> done_callback, bool use_thunk_runtime)
 
 #define CONV3D_INSTANTIATE_TEMPLATE(DEVICE, SCALAR_TYPE)                       \
   template void xla::cpu::internal::EigenConv3D<DEVICE, SCALAR_TYPE>(          \

xla/service/cpu/BUILD
@@ -1121,10 +1121,7 @@ cc_library(
         ":runtime_lightweight_check",
         "//xla:executable_run_options",
         "//xla/backends/cpu/runtime:convolution_thunk_internal",
-        "//xla/tsl/framework/contraction:eigen_contraction_kernel",
-        "//xla/tsl/framework/convolution:eigen_helpers",
         "@com_google_absl//absl/base:core_headers",
-        "@com_google_absl//absl/base:dynamic_annotations",
         "@eigen_archive//:eigen3",
         "@tsl//tsl/platform:mutex",  # build_cleaner: keep
     ],

xla/service/cpu/runtime_conv2d.cc
@@ -44,7 +44,7 @@ ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv2DF32(
       kernel_channels, kernel_filters, output_rows, output_cols, row_stride,
       col_stride, padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, nullptr);
+      feature_group_count, nullptr, /*use_thunk_runtime=*/false);
 }
 
 ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv2DF16(
@@ -66,5 +66,5 @@ ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void __xla_cpu_runtime_EigenConv2DF16(
       kernel_channels, kernel_filters, output_rows, output_cols, row_stride,
       col_stride, padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, nullptr);
+      feature_group_count, nullptr, /*use_thunk_runtime=*/false);
 }

xla/service/cpu/runtime_single_threaded_conv2d.cc
@@ -37,7 +37,7 @@ __xla_cpu_runtime_EigenSingleThreadedConv2DF16(
       kernel_filters, output_rows, output_cols, row_stride, col_stride,
       padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, nullptr);
+      feature_group_count, nullptr, /*use_thunk_runtime=*/false);
 }
 
 ABSL_ATTRIBUTE_NO_SANITIZE_MEMORY void
@@ -57,5 +57,5 @@ __xla_cpu_runtime_EigenSingleThreadedConv2DF32(
       kernel_filters, output_rows, output_cols, row_stride, col_stride,
       padding_top, padding_bottom, padding_left, padding_right,
       lhs_row_dilation, lhs_col_dilation, rhs_row_dilation, rhs_col_dilation,
-      feature_group_count, nullptr);
+      feature_group_count, nullptr, /*use_thunk_runtime=*/false);
 }