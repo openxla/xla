openxla/xla

commit d8909c9b8f6333ce357dc1bb5cb531bc10c7fb34

[GpuCommandBuffer] Move GraphAddKernelNode logic into subclasses
- Adds pure virtual functions for creating and updating kernel nodes
- Implements both methods for both platforms in the subclasses

PiperOrigin-RevId: 690603676

xla/stream_executor/rocm/rocm_command_buffer.cc
@@ -32,7 +32,10 @@ limitations under the License.
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/gpu/gpu_command_buffer.h"
 #include "xla/stream_executor/gpu/gpu_executor.h"
+#include "xla/stream_executor/kernel.h"
+#include "xla/stream_executor/launch_dim.h"
 #include "xla/stream_executor/rocm/rocm_driver_wrapper.h"
+#include "xla/stream_executor/rocm/rocm_kernel.h"
 #include "xla/stream_executor/rocm/rocm_status.h"
 #include "tsl/platform/casts.h"
 #include "tsl/platform/errors.h"
@@ -240,4 +243,92 @@ absl::Status RocmCommandBuffer::UpdateChildNode(GraphNodeHandle node_handle,
                       exec_, ToHipGraphHandle(node_handle), child_graph),
                   "Failed to set HIP graph child node params");
 }
+
+absl::StatusOr<GraphNodeHandle> RocmCommandBuffer::CreateKernelNode(
+    const Dependencies& dependencies, const ThreadDim& threads,
+    const BlockDim& blocks, const Kernel& kernel,
+    const KernelArgsPackedArrayBase& args) {
+  const uint64_t shared_mem_bytes = args.number_of_shared_bytes();
+
+  VLOG(2) << "Add kernel node to a graph " << graph_
+          << "; kernel: " << kernel.name() << "; gdx: " << blocks.x
+          << " gdy: " << blocks.y << " gdz: " << blocks.z
+          << " bdx: " << threads.x << " bdy: " << threads.y
+          << " bdz: " << threads.z << "; shmem: " << shared_mem_bytes
+          << "; deps: " << dependencies.size();
+
+  hipKernelNodeParams params{};
+
+  hipFunction_t function =
+      static_cast<const RocmKernel&>(kernel).gpu_function();
+  params.func = function;
+  params.gridDim.x = blocks.x;
+  params.gridDim.z = blocks.y;
+  params.gridDim.z = blocks.z;
+  params.blockDim.x = threads.x;
+  params.blockDim.y = threads.y;
+  params.blockDim.z = threads.z;
+  params.sharedMemBytes = shared_mem_bytes;
+  params.kernelParams = const_cast<void**>(args.argument_addresses().data());
+  params.extra = nullptr;
+
+  if (shared_mem_bytes != 0) {
+    TF_RETURN_IF_ERROR(ToStatus(
+        wrap::hipFuncSetAttribute(function,
+                                  hipFuncAttributeMaxDynamicSharedMemorySize,
+                                  shared_mem_bytes),
+        "Failed to set shared memory size"));
+  }
+
+  std::vector<hipGraphNode_t> deps = ToHipGraphHandles(dependencies);
+
+  hipGraphNode_t node_handle = nullptr;
+  TF_RETURN_IF_ERROR(
+      ToStatus(wrap::hipGraphAddKernelNode(&node_handle, graph_, deps.data(),
+                                           deps.size(), &params),
+               "Failed to add kernel node to a HIP graph"));
+
+  return FromHipGraphHandle(node_handle);
+}
+
+absl::Status RocmCommandBuffer::UpdateKernelNode(
+    GraphNodeHandle node_handle, const ThreadDim& threads,
+    const BlockDim& blocks, const Kernel& kernel,
+    const KernelArgsPackedArrayBase& args) {
+  const uint64_t shared_mem_bytes = args.number_of_shared_bytes();
+
+  VLOG(2) << "Set kernel node params " << node_handle << " in graph executable "
+          << exec_ << "; kernel: " << kernel.name() << "; gdx: " << blocks.x
+          << " gdy: " << blocks.y << " gdz: " << blocks.z
+          << " bdx: " << threads.x << " bdy: " << threads.y
+          << " bdz: " << threads.z << "; shmem: " << shared_mem_bytes;
+
+  hipKernelNodeParams params{};
+
+  hipFunction_t function =
+      static_cast<const RocmKernel&>(kernel).gpu_function();
+  params.func = function;
+  params.gridDim.x = blocks.x;
+  params.gridDim.z = blocks.y;
+  params.gridDim.z = blocks.z;
+  params.blockDim.x = threads.x;
+  params.blockDim.y = threads.y;
+  params.blockDim.z = threads.z;
+  params.sharedMemBytes = shared_mem_bytes;
+  params.kernelParams = const_cast<void**>(args.argument_addresses().data());
+  params.extra = nullptr;
+
+  if (shared_mem_bytes != 0) {
+    TF_RETURN_IF_ERROR(ToStatus(
+        wrap::hipFuncSetAttribute(function,
+                                  hipFuncAttributeMaxDynamicSharedMemorySize,
+                                  shared_mem_bytes),
+        "Failed to set shared memory size"));
+  }
+
+  return ToStatus(wrap::hipGraphExecKernelNodeSetParams(
+                      exec_, ToHipGraphHandle(node_handle), &params),
+                  "Failed to set HIP graph kernel node params");
+}
+
 }  // namespace stream_executor::gpu

xla/stream_executor/cuda/BUILD
@@ -1337,10 +1337,13 @@ cc_library(
     deps = [
         ":command_buffer_kernels",
         ":cuda_context",
+        ":cuda_kernel",
         ":cuda_status",
         "//xla/stream_executor:bit_pattern",
         "//xla/stream_executor:command_buffer",
         "//xla/stream_executor:device_memory",
+        "//xla/stream_executor:kernel",
+        "//xla/stream_executor:launch_dim",
         "//xla/stream_executor:typed_kernel_factory",
         "//xla/stream_executor/gpu:gpu_command_buffer",
         "//xla/stream_executor/gpu:gpu_executor_header",

xla/stream_executor/cuda/cuda_command_buffer.cc
@@ -30,10 +30,13 @@ limitations under the License.
 #include "xla/stream_executor/command_buffer.h"
 #include "xla/stream_executor/cuda/command_buffer_kernels.h"
 #include "xla/stream_executor/cuda/cuda_context.h"
+#include "xla/stream_executor/cuda/cuda_kernel.h"
 #include "xla/stream_executor/cuda/cuda_status.h"
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/gpu/gpu_command_buffer.h"
 #include "xla/stream_executor/gpu/gpu_executor.h"
+#include "xla/stream_executor/kernel.h"
+#include "xla/stream_executor/launch_dim.h"
 #include "xla/stream_executor/typed_kernel_factory.h"  // IWYU pragma: keep
 #include "tsl/platform/casts.h"
 #include "tsl/platform/errors.h"
@@ -303,4 +306,94 @@ absl::Status CudaCommandBuffer::UpdateChildNode(GraphNodeHandle node_handle,
                             exec_, ToCudaGraphHandle(node_handle), child_graph),
                         "Failed to set CUDA graph child node params");
 }
+
+absl::StatusOr<GraphNodeHandle> CudaCommandBuffer::CreateKernelNode(
+    const Dependencies& dependencies, const ThreadDim& threads,
+    const BlockDim& blocks, const Kernel& kernel,
+    const KernelArgsPackedArrayBase& args) {
+  const uint64_t shared_mem_bytes = args.number_of_shared_bytes();
+
+  VLOG(2) << "Add kernel node to a graph " << graph_
+          << "; kernel: " << kernel.name() << "; gdx: " << blocks.x
+          << " gdy: " << blocks.y << " gdz: " << blocks.z
+          << " bdx: " << threads.x << " bdy: " << threads.y
+          << " bdz: " << threads.z << "; shmem: " << shared_mem_bytes
+          << "; deps: " << dependencies.size();
+
+  CUDA_KERNEL_NODE_PARAMS params{};
+
+  CUfunction function = static_cast<const CudaKernel&>(kernel).gpu_function();
+  params.func = function;
+  params.gridDimX = blocks.x;
+  params.gridDimY = blocks.y;
+  params.gridDimZ = blocks.z;
+  params.blockDimX = threads.x;
+  params.blockDimY = threads.y;
+  params.blockDimZ = threads.z;
+  params.sharedMemBytes = shared_mem_bytes;
+  params.kernelParams = const_cast<void**>(args.argument_addresses().data());
+  params.extra = nullptr;
+
+  // TODO(ezhulenev): Why do we do it on every call to launch kernel? This
+  // should be moved one level up to se::Kernel level, and done just once (or
+  // updated once we get a new larger shared memory request).
+  if (shared_mem_bytes != 0) {
+    TF_RETURN_IF_ERROR(cuda::ToStatus(
+        cuFuncSetAttribute(function,
+                           CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES,
+                           shared_mem_bytes),
+        "Failed to set shared memory size"));
+  }
+
+  std::vector<CUgraphNode> deps = ToCudaGraphHandles(dependencies);
+
+  CUgraphNode node_handle = nullptr;
+  TF_RETURN_IF_ERROR(
+      cuda::ToStatus(cuGraphAddKernelNode(&node_handle, graph_, deps.data(),
+                                          deps.size(), &params),
+                     "Failed to add kernel node to a CUDA graph"));
+  return FromCudaGraphHandle(node_handle);
+}
+
+absl::Status CudaCommandBuffer::UpdateKernelNode(
+    GraphNodeHandle node_handle, const ThreadDim& threads,
+    const BlockDim& blocks, const Kernel& kernel,
+    const KernelArgsPackedArrayBase& args) {
+  const uint64_t shared_mem_bytes = args.number_of_shared_bytes();
+
+  VLOG(2) << "Set kernel node params " << node_handle << " in graph executable "
+          << exec_ << "; kernel: " << kernel.name() << "; gdx: " << blocks.x
+          << " gdy: " << blocks.y << " gdz: " << blocks.z
+          << " bdx: " << threads.x << " bdy: " << threads.y
+          << " bdz: " << threads.z << "; shmem: " << shared_mem_bytes;
+
+  CUDA_KERNEL_NODE_PARAMS params{};
+  CUfunction function = static_cast<const CudaKernel&>(kernel).gpu_function();
+  params.func = function;
+  params.gridDimX = blocks.x;
+  params.gridDimY = blocks.y;
+  params.gridDimZ = blocks.z;
+  params.blockDimX = threads.x;
+  params.blockDimY = threads.y;
+  params.blockDimZ = threads.z;
+  params.sharedMemBytes = shared_mem_bytes;
+  params.kernelParams = const_cast<void**>(args.argument_addresses().data());
+  params.extra = nullptr;
+
+  // TODO(ezhulenev): Why do we do it on every call to launch kernel? This
+  // should be moved one level up to se::Kernel level, and done just once (or
+  // updated once we get a new larger shared memory request).
+  if (shared_mem_bytes != 0) {
+    TF_RETURN_IF_ERROR(cuda::ToStatus(
+        cuFuncSetAttribute(function,
+                           CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES,
+                           shared_mem_bytes),
+        "Failed to set shared memory size"));
+  }
+
+  return cuda::ToStatus(cuGraphExecKernelNodeSetParams(
+                            exec_, ToCudaGraphHandle(node_handle), &params),
+                        "Failed to set CUDA graph kernel node params");
+}
+
 }  // namespace stream_executor::gpu

xla/stream_executor/cuda/cuda_command_buffer.h
@@ -28,6 +28,8 @@ limitations under the License.
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/gpu/gpu_command_buffer.h"
 #include "xla/stream_executor/gpu/gpu_executor.h"
+#include "xla/stream_executor/kernel.h"
+#include "xla/stream_executor/launch_dim.h"
 
 namespace stream_executor::gpu {
 
@@ -79,6 +81,16 @@ class CudaCommandBuffer final : public GpuCommandBuffer {
 
   absl::Status UpdateChildNode(GraphNodeHandle node_handle,
                                const CommandBuffer& nested) override;
+
+  absl::StatusOr<GraphNodeHandle> CreateKernelNode(
+      const Dependencies& dependencies, const ThreadDim& threads,
+      const BlockDim& blocks, const Kernel& kernel,
+      const KernelArgsPackedArrayBase& args) override;
+
+  absl::Status UpdateKernelNode(GraphNodeHandle node_handle,
+                                const ThreadDim& threads,
+                                const BlockDim& blocks, const Kernel& kernel,
+                                const KernelArgsPackedArrayBase& args) override;
 
   // Lazy loaded auxiliary kernels required for building CUDA graphs (no-op
   // barriers, updating conditional handles, etc.).

xla/stream_executor/cuda/cuda_driver.cc
@@ -415,47 +415,6 @@ absl::Status GpuDriver::GraphAddKernelNode(
       "Failed to add kernel node to a CUDA graph");
 }
 
-/*static*/ absl::Status GpuDriver::GraphExecKernelNodeSetParams(
-    CUgraphExec exec, CUgraphNode node, absl::string_view kernel_name,
-    CUfunction function, unsigned int grid_dim_x, unsigned int grid_dim_y,
-    unsigned int grid_dim_z, unsigned int block_dim_x, unsigned int block_dim_y,
-    unsigned int block_dim_z, unsigned int shared_mem_bytes,
-    void** kernel_params, void** extra) {
-  VLOG(2) << "Set kernel node params " << node << " in graph executable "
-          << exec << "; kernel: " << kernel_name << "; gdx: " << grid_dim_x
-          << " gdy: " << grid_dim_y << " gdz: " << grid_dim_z
-          << " bdx: " << block_dim_x << " bdy: " << block_dim_y
-          << " bdz: " << block_dim_z << "; shmem: " << shared_mem_bytes;
-
-  CUDA_KERNEL_NODE_PARAMS params;
-  memset(&params, 0, sizeof(params));
-
-  params.func = function;
-  params.gridDimX = grid_dim_x;
-  params.gridDimY = grid_dim_y;
-  params.gridDimZ = grid_dim_z;
-  params.blockDimX = block_dim_x;
-  params.blockDimY = block_dim_y;
-  params.blockDimZ = block_dim_z;
-  params.sharedMemBytes = shared_mem_bytes;
-  params.kernelParams = kernel_params;
-  params.extra = extra;
-
-  // TODO(ezhulenev): Why do we do it on every call to launch kernel? This
-  // should be moved one level up to se::Kernel level, and done just once (or
-  // updated once we get a new larger shared memory request).
-  if (shared_mem_bytes != 0) {
-    TF_RETURN_IF_ERROR(cuda::ToStatus(
-        cuFuncSetAttribute(function,
-                           CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES,
-                           shared_mem_bytes),
-        "Failed to set shared memory size"));
-  }
-
-  return cuda::ToStatus(cuGraphExecKernelNodeSetParams(exec, node, &params),
-                        "Failed to set CUDA graph kernel node params");
-}
-
 int GpuDriver::GetDeviceCount() {
   int device_count = 0;
   auto status = cuda::ToStatus(cuDeviceGetCount(&device_count));

xla/stream_executor/gpu/gpu_command_buffer.cc
@@ -473,34 +473,20 @@ absl::Status GpuCommandBuffer::LaunchWithPackedArgs(
   CHECK_EQ(kernel.Arity() + (packed_args.number_of_shared_bytes() > 0),
            packed_args.number_of_arguments());
 
-  const GpuKernel* gpu_kernel = AsGpuKernel(&kernel);
-  GpuFunctionHandle gpu_func = gpu_kernel->gpu_function();
-
-  void** kernel_params =
-      const_cast<void**>(packed_args.argument_addresses().data());
-
   // Adds a new kernel node to the graph under construction.
   if (state_ == State::kCreate) {
     Dependencies barrier = GetBarrier(execution_scope_id);
-    GpuGraphNodeHandle node_handle = nullptr;
-    TF_RETURN_IF_ERROR(GpuDriver::GraphAddKernelNode(
-        &node_handle, graph_, ToPlatformSpecificHandles(barrier), kernel.name(),
-        gpu_func, blocks.x, blocks.y, blocks.z, threads.x, threads.y, threads.z,
-        packed_args.number_of_shared_bytes(), kernel_params,
-        /*extra=*/nullptr));
-    execution_scope.nodes.emplace_back().handle =
-        FromPlatformSpecificHandle(node_handle);
+    TF_ASSIGN_OR_RETURN(
+        execution_scope.nodes.emplace_back().handle,
+        CreateKernelNode(barrier, threads, blocks, kernel, packed_args));
     return absl::OkStatus();
   }
 
   // Updates kernel node in the executable graph.
   if (state_ == State::kUpdate) {
-    GraphNodeHandle node =
-        execution_scope.nodes[execution_scope.update_state.node_idx++].handle;
-    return GpuDriver::GraphExecKernelNodeSetParams(
-        exec_, ToPlatformSpecificHandle(node), kernel.name(), gpu_func,
-        blocks.x, blocks.y, blocks.z, threads.x, threads.y, threads.z,
-        packed_args.number_of_shared_bytes(), kernel_params, /*extra=*/nullptr);
+    return UpdateKernelNode(
+        execution_scope.nodes[execution_scope.update_state.node_idx++].handle,
+        threads, blocks, kernel, packed_args);
   }
 
   return UnsupportedStateError(state_);

xla/stream_executor/gpu/gpu_command_buffer.h
@@ -397,6 +397,19 @@ class GpuCommandBuffer : public CommandBuffer {
   // error if the given node has not been created as a child node.
   virtual absl::Status UpdateChildNode(GraphNodeHandle node_handle,
                                        const CommandBuffer& nested) = 0;
+
+  // Adds a new kernel launch node to the graph.
+  virtual absl::StatusOr<GraphNodeHandle> CreateKernelNode(
+      const Dependencies& dependencies, const ThreadDim& threads,
+      const BlockDim& blocks, const Kernel& kernel,
+      const KernelArgsPackedArrayBase& args) = 0;
+
+  // Updates the kernel launch node with the given parameters. Will return an
+  // error if the given node has not been created as a kernel launch node.
+  virtual absl::Status UpdateKernelNode(
+      GraphNodeHandle node_handle, const ThreadDim& threads,
+      const BlockDim& blocks, const Kernel& kernel,
+      const KernelArgsPackedArrayBase& args) = 0;
 };
 
 }  // namespace stream_executor::gpu

xla/stream_executor/gpu/gpu_driver.h
@@ -231,17 +231,6 @@ class GpuDriver {
   // https://docs.amd.com/projects/HIP/en/docs-5.0.0/doxygen/html/group___graph.html#gaf006701d98164ed3492755bbb19bab83
   static absl::StatusOr<size_t> GraphGetNodeCount(GpuGraphHandle graph);
 
-  // Sets the parameters for a kernel node in the given graph exec.
-  // https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__GRAPH.html#group__CUDA__GRAPH_1gd84243569e4c3d6356b9f2eea20ed48c
-  // https://docs.amd.com/projects/HIP/en/docs-5.0.0/doxygen/html/group___graph.html#ga5b1918dae65224863b7370e6d4ad3f2a
-  static absl::Status GraphExecKernelNodeSetParams(
-      GpuGraphExecHandle exec, GpuGraphNodeHandle node,
-      absl::string_view kernel_name, GpuFunctionHandle function,
-      unsigned int grid_dim_x, unsigned int grid_dim_y, unsigned int grid_dim_z,
-      unsigned int block_dim_x, unsigned int block_dim_y,
-      unsigned int block_dim_z, unsigned int shared_mem_bytes,
-      void** kernel_params, void** extra);
-
   // The CUDA stream callback type signature.
   // The data passed to AddStreamCallback is subsequently passed to this
   // callback when it fires.

commit c9211b183905d6ff81d40545cd004d9acf61cc66

PR #19162: [ROCm] Fix grid dimension issue introduced in d8909c9b8
Imported from GitHub PR https://github.com/openxla/xla/pull/19162

Copybara import of the project:

--
dd86f21a20d1391cefaf6f74f89abce787bb236a by Harsha HS <Harsha.HavanurShamsundara@amd.com>:

[ROCm] Fix grid dimension issue introduced in d8909c9b8

Merging this change closes #19162

COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/19162 from ROCm:ci_fix_gridy_20241107 dd86f21a20d1391cefaf6f74f89abce787bb236a
PiperOrigin-RevId: 695562833

xla/stream_executor/rocm/rocm_command_buffer.cc
@@ -273,7 +273,7 @@ absl::StatusOr<GraphNodeHandle> RocmCommandBuffer::CreateKernelNode(
       static_cast<const RocmKernel&>(kernel).gpu_function();
   params.func = function;
   params.gridDim.x = blocks.x;
-  params.gridDim.z = blocks.y;
+  params.gridDim.y = blocks.y;
   params.gridDim.z = blocks.z;
   params.blockDim.x = threads.x;
   params.blockDim.y = threads.y;
@@ -319,7 +319,7 @@ absl::Status RocmCommandBuffer::UpdateKernelNode(
       static_cast<const RocmKernel&>(kernel).gpu_function();
   params.func = function;
   params.gridDim.x = blocks.x;
-  params.gridDim.z = blocks.y;
+  params.gridDim.y = blocks.y;
   params.gridDim.z = blocks.z;
   params.blockDim.x = threads.x;
   params.blockDim.y = threads.y;