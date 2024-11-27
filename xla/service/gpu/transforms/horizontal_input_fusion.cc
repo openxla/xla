openxla/xla

commit b29b6545298718853873fd7e9296857a12831866

[XLA:GPU] Fix a bug in horizontal input fusion sorting.
The sorting function in horizontal input fusion is not stable, which can cause non-deterministic behavior. We fix this by breaking ties on unique id.

While here, use a similar approach to make horizontal loop fusion use a normal sort by breaking ties the same way.

PiperOrigin-RevId: 700479039

xla/service/gpu/transforms/horizontal_input_fusion.cc
@@ -17,6 +17,8 @@ limitations under the License.
 
 #include <algorithm>
 #include <cstddef>
+#include <tuple>
+#include <utility>
 #include <vector>
 
 #include "absl/container/flat_hash_set.h"
@@ -69,23 +71,6 @@ class HorizontalInputFusionImpl {
   const se::DeviceDescription& device_info_;
 };  // HorizontalInputFusionImpl
 
-// Compares one-by-one the dimensions of `shape_a` and `shape_b` from left to
-// right.
-bool CompareShapeDimsFromLeftToRight(const Shape& shape_a,
-                                     const Shape& shape_b) {
-  if (shape_a.rank() != shape_b.rank()) {
-    return shape_a.rank() < shape_b.rank();
-  }
-  auto dims_a = shape_a.dimensions();
-  auto dims_b = shape_b.dimensions();
-  for (size_t i = 0; i < dims_a.size(); ++i) {
-    if (dims_a[i] != dims_b[i]) {
-      return dims_a[i] < dims_b[i];
-    }
-  }
-  return true;
-}
-
 std::vector<HloInstruction*> FindAndSortFusionCandidates(
     HloInstruction* consumer) {
   absl::flat_hash_set<HloInstruction*> fusion_instr_set;
@@ -106,16 +91,16 @@ std::vector<HloInstruction*> FindAndSortFusionCandidates(
 
   std::sort(fusion_instrs.begin(), fusion_instrs.end(),
             [&](const HloInstruction* a, const HloInstruction* b) {
-              Shape shape_a = GetInputShapeForMultiOutputFusion(*a);
-              Shape shape_b = GetInputShapeForMultiOutputFusion(*b);
-              if (!ShapeUtil::EqualIgnoringElementType(shape_a, shape_b)) {
+              auto tuple_for_op = [](const HloInstruction* op) {
+                Shape shape = GetInputShapeForMultiOutputFusion(*op);
                 // Sort shapes according to dimensions, so that the same input
                 // shapes will be placed adjacent each other.
-                return CompareShapeDimsFromLeftToRight(shape_a, shape_b);
-              }
-              // Sort `fusion_instrs` according to instruction counts, because
-              // we'd like to fuse together computations of similar sizes.
-              return GetInstrCountOfFusible(*a) < GetInstrCountOfFusible(*b);
+                // Sort `fusion_instrs` according to instruction counts, because
+                // we'd like to fuse together computations of similar sizes.
+                return std::tuple{shape.rank(), shape.dimensions(),
+                                  GetInstrCountOfFusible(*op), op->unique_id()};
+              };
+              return tuple_for_op(a) < tuple_for_op(b);
             });
 
   return fusion_instrs;

xla/service/gpu/gpu_fusible.cc
@@ -769,7 +769,7 @@ bool IsConsumerTheOnlyNonRootUser(const HloInstruction& instr,
   });
 }
 
-size_t GetInstrCountOfFusible(const HloInstruction& instr) {
+int64_t GetInstrCountOfFusible(const HloInstruction& instr) {
   return instr.opcode() == HloOpcode::kFusion ? instr.fused_instruction_count()
                                               : 1;
 }

xla/service/gpu/gpu_fusible.h
@@ -179,7 +179,7 @@ bool IsConsumerTheOnlyNonRootUser(const HloInstruction& instr,
 
 // Returns number of instructions in the fusible `instr`. If `instr` is not a
 // fusion instruction, 1 is returned.
-size_t GetInstrCountOfFusible(const HloInstruction& instr);
+int64_t GetInstrCountOfFusible(const HloInstruction& instr);
 
 // Returns the outputs of the fusible `instr`.
 absl::InlinedVector<const HloInstruction*, 2> GetOutputsOfFusible(

xla/service/gpu/transforms/cudnn_norm_rewriter_test.cc
@@ -1184,7 +1184,7 @@ TEST_F(CudnnNormRewriterTest, LayerNormTrainBackward4D2) {
 ; CHECK-NEXT:    [[P0_BITCAST:%[^ ]+]] = f32[64,6,1,1]{3,2,1,0} bitcast([[TRANSPOSE0]])
 ; CHECK-NEXT:    [[P1:%[^ ]+]] = f32[6]{0} parameter(1)
 ; CHECK-NEXT:    [[P1_BITCAST:%[^ ]+]] = f32[1,6,1,1]{3,2,1,0} bitcast([[P1]])
-; CHECK-NEXT:    [[P2:%[^ ]+]] = f32[6]{0} parameter(2)
+; CHECK:         [[P2:%[^ ]+]] = f32[6]{0} parameter(2)
 ; CHECK-NEXT:    [[P2_BITCAST:%[^ ]+]] = f32[1,6,1,1]{3,2,1,0} bitcast([[P2]])
 ; CHECK-NEXT:    [[CC0:%[^ ]+]] = (f32[64,6,1,1]{3,2,1,0}, f32[64,1,1,1]{3,2,1,0}, f32[64,1,1,1]{3,2,1,0}, u8[{{.*}}]{0}) custom-call([[P0_BITCAST]], [[P1_BITCAST]], [[P2_BITCAST]]),
 ; CHECK:           custom_call_target="__cudnn$norm",
@@ -1277,7 +1277,7 @@ TEST_F(CudnnNormRewriterTest, LayerNormTrainBackward4D12) {
 ; CHECK-NEXT:    [[P0_BITCAST:%[^ ]+]] = f32[16,4,6,1]{3,2,1,0} bitcast([[TRANSPOSE0]])
 ; CHECK-NEXT:    [[P1:%[^ ]+]] = f32[4,6]{1,0} parameter(1)
 ; CHECK-NEXT:    [[P1_BITCAST:%[^ ]+]] = f32[1,4,6,1]{3,2,1,0} bitcast([[P1]])
-; CHECK-NEXT:    [[P2:%[^ ]+]] = f32[4,6]{1,0} parameter(2)
+; CHECK:         [[P2:%[^ ]+]] = f32[4,6]{1,0} parameter(2)
 ; CHECK-NEXT:    [[P2_BITCAST:%[^ ]+]] = f32[1,4,6,1]{3,2,1,0} bitcast([[P2]])
 ; CHECK-NEXT:    [[CC0:%[^ ]+]] = (f32[16,4,6,1]{3,2,1,0}, f32[16,1,1,1]{3,2,1,0}, f32[16,1,1,1]{3,2,1,0}, u8[{{.*}}]{0}) custom-call([[P0_BITCAST]], [[P1_BITCAST]], [[P2_BITCAST]]),
 ; CHECK:           custom_call_target="__cudnn$norm",
@@ -1370,7 +1370,7 @@ TEST_F(CudnnNormRewriterTest, LayerNormTrainBackward4D12Degenerate2) {
 ; CHECK-NEXT:    [[P0_BITCAST:%[^ ]+]] = f32[16,4,1,1]{3,2,1,0} bitcast([[TRANSPOSE0]])
 ; CHECK-NEXT:    [[P1:%[^ ]+]] = f32[4,1]{1,0} parameter(1)
 ; CHECK-NEXT:    [[P1_BITCAST:%[^ ]+]] = f32[1,4,1,1]{3,2,1,0} bitcast([[P1]])
-; CHECK-NEXT:    [[P2:%[^ ]+]] = f32[4,1]{1,0} parameter(2)
+; CHECK:         [[P2:%[^ ]+]] = f32[4,1]{1,0} parameter(2)
 ; CHECK-NEXT:    [[P2_BITCAST:%[^ ]+]] = f32[1,4,1,1]{3,2,1,0} bitcast([[P2]])
 ; CHECK-NEXT:    [[CC0:%[^ ]+]] = (f32[16,4,1,1]{3,2,1,0}, f32[16,1,1,1]{3,2,1,0}, f32[16,1,1,1]{3,2,1,0}, u8[{{.*}}]{0}) custom-call([[P0_BITCAST]], [[P1_BITCAST]], [[P2_BITCAST]]),
 ; CHECK:           custom_call_target="__cudnn$norm",

xla/service/gpu/transforms/horizontal_loop_fusion.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -311,22 +312,17 @@ void HorizontalLoopFusionImpl::FusionCandidates::Initialize(
   // For kLoop fusion, we in addition require the same output shape.
   // We did a sort here so the fusion candidates is
   // populating a continuous span.
-  std::stable_sort(
-      fusible_instrs_.begin(), fusible_instrs_.end(),
-      [&](const HloInstruction* a, const HloInstruction* b) {
-        if (GetUniqueOutputTypeOfFusible(*a) !=
-            GetUniqueOutputTypeOfFusible(*b)) {
-          return GetUniqueOutputTypeOfFusible(*a) <
-                 GetUniqueOutputTypeOfFusible(*b);
-        } else if (GetOutputSizeOfFusible(*a) != GetOutputSizeOfFusible(*b)) {
-          return GetOutputSizeOfFusible(*a) < GetOutputSizeOfFusible(*b);
-        } else if (GetInstrCountOfFusible(*a) != GetInstrCountOfFusible(*b)) {
-          return GetInstrCountOfFusible(*a) < GetInstrCountOfFusible(*b);
-        } else {
-          return ShapeUtil::ElementsIn(GetOutputsOfFusible(*a)[0]->shape()) <
-                 ShapeUtil::ElementsIn(GetOutputsOfFusible(*b)[0]->shape());
-        }
-      });
+  std::sort(fusible_instrs_.begin(), fusible_instrs_.end(),
+            [&](const HloInstruction* a, const HloInstruction* b) {
+              auto make_tuple_for_op = [](const HloInstruction* op) {
+                return std::tuple{
+                    GetUniqueOutputTypeOfFusible(*op),
+                    GetOutputSizeOfFusible(*op), GetInstrCountOfFusible(*op),
+                    ShapeUtil::ElementsIn(GetOutputsOfFusible(*op)[0]->shape()),
+                    op->unique_id()};
+              };
+              return make_tuple_for_op(a) < make_tuple_for_op(b);
+            });
 }
 
 // Gets a next span of fusion instructions to be fused.

xla/service/gpu/transforms/layout_assignment_test.cc
@@ -51,18 +51,16 @@ using ::tsl::testing::IsOkAndHolds;
 
 class LayoutAssignmentTest : public HloTestBase {
  public:
+  se::DeviceDescription GetDeviceDescription() {
+    return backend().default_stream_executor()->GetDeviceDescription();
+  }
+
   se::CudaComputeCapability GetCudaComputeCapability() {
-    return backend()
-        .default_stream_executor()
-        ->GetDeviceDescription()
-        .cuda_compute_capability();
+    return GetDeviceDescription().cuda_compute_capability();
   }
 
   se::GpuComputeCapability GetGpuComputeCapability() {
-    return backend()
-        .default_stream_executor()
-        ->GetDeviceDescription()
-        .gpu_compute_capability();
+    return GetDeviceDescription().gpu_compute_capability();
   }
 
   se::dnn::VersionInfo GetDnnVersion() {

commit 36d332c6fc5e0bd2a74ab0105a102e76a1f90967

[XLA:GPU] Fix a stack use after scope in horizontal fusion
Shape::dimensions returns a reference. Found by asan.

Regression introduced in https://github.com/openxla/xla/commit/b29b6545298718853873fd7e9296857a12831866

PiperOrigin-RevId: 700669502

xla/service/gpu/transforms/horizontal_input_fusion.cc
@@ -91,8 +91,10 @@ std::vector<HloInstruction*> FindAndSortFusionCandidates(
 
   std::sort(fusion_instrs.begin(), fusion_instrs.end(),
             [&](const HloInstruction* a, const HloInstruction* b) {
-              auto tuple_for_op = [](const HloInstruction* op) {
-                Shape shape = GetInputShapeForMultiOutputFusion(*op);
+              Shape shape_a = GetInputShapeForMultiOutputFusion(*a);
+              Shape shape_b = GetInputShapeForMultiOutputFusion(*b);
+              auto tuple_for_op = [](const Shape& shape,
+                                     const HloInstruction* op) {
                 // Sort shapes according to dimensions, so that the same input
                 // shapes will be placed adjacent each other.
                 // Sort `fusion_instrs` according to instruction counts, because
@@ -100,7 +102,7 @@ std::vector<HloInstruction*> FindAndSortFusionCandidates(
                 return std::tuple{shape.rank(), shape.dimensions(),
                                   GetInstrCountOfFusible(*op), op->unique_id()};
               };
-              return tuple_for_op(a) < tuple_for_op(b);
+              return tuple_for_op(shape_a, a) < tuple_for_op(shape_b, b);
             });
 
   return fusion_instrs;