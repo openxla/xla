48ad313c39fc75f5f6402c408a52f88b7f642bf8
Kiran Sai Ramineni <106319630+kiransair@users.noreply.github.com>
2024-11-28 12:17:11 +0530

    Typos

--- xla/service/gpu/kernels/custom_kernel_fusion.h
+++ xla/service/gpu/kernels/custom_kernel_fusion.h
@@ -60,7 +60,7 @@ namespace xla::gpu {
 // XLA:GPU has multiple strategies for executing this fusion on device:
 //
 // (1) cuBLAS library call: a lot of simple gemm operations are supported by
-//     cuBLAS out of the box. However some combinations of paramters casting and
+//     cuBLAS out of the box. However some combinations of parameters casting and
 //     epilogue fusion are not supported, which means that XLA has to form
 //     smaller fusions or use code generation to compiled a device kernel.
 //
