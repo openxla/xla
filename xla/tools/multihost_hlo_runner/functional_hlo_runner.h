83d53c26b03477a4ba727681d1158c1e185f407d
Rachel Han <hanrach@google.com>
2025-01-16 18:16:36 -0800

    Add argument to fix a random seed when generating random arguments for HLO runner. Also add OutputFormat so that literal dumps can be saved as a pb file.
    PiperOrigin-RevId: 716454943

--- xla/tools/multihost_hlo_runner/functional_hlo_runner.h
+++ xla/tools/multihost_hlo_runner/functional_hlo_runner.h
@@ -64,6 +64,11 @@ enum class InputFormat {
                                     // in conjunction with xla_dump_as_text.
 };
 
+enum class OutputFormat : std::uint8_t {
+  kText,
+  kSerializedProto,
+};
+
 // Interface for profiler plugins. If being set in RunningOptions, profiling
 // session will be created for the last run of the HLO module.
 class ProfilerInterface {
@@ -285,7 +290,8 @@ class FunctionalHloRunner {
       const PreprocessingOptions& preproc_options,
       const CompileOptions& compile_options,
       const RunningOptions& running_options, absl::string_view hlo_text,
-      InputFormat input_format, const PerDeviceLiteralVecType& arguments = {});
+      InputFormat input_format, const PerDeviceLiteralVecType& arguments = {},
+      std::minstd_rand0* engine = nullptr);
 
   // Loads and compiles an HLO for debugging purposes.
   //
@@ -307,7 +313,8 @@ class FunctionalHloRunner {
       const PreprocessingOptions& preproc_options,
       const CompileOptions& compile_options,
       const RunningOptions& running_options, HloModule* hlo_module,
-      const PerDeviceLiteralVecType& arguments = {});
+      const PerDeviceLiteralVecType& arguments = {},
+      std::minstd_rand0* engine = nullptr);
 
   // Compiles the HLO module.
   static absl::StatusOr<std::unique_ptr<PjRtLoadedExecutable>> Compile(
@@ -368,7 +375,8 @@ class FunctionalHloRunner {
 
   static absl::Status DumpOutput(
       const FunctionalHloRunner::PerDeviceLiteralVecType& output,
-      absl::string_view dump_output_to, int task_id);
+      absl::string_view dump_output_to, int task_id,
+      OutputFormat output_format = OutputFormat::kText);
 
  private:
   // Calculates the requested number of replicas and partitions.
