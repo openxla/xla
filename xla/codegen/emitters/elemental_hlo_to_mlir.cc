21a14b09f26b812b8da41f5553ea01a00a86ae48
Emilio Cota <ecg@google.com>
2025-01-20 11:15:37 -0800

    [xla:emitters] elemental_hlo_to_mlir: drop first operand of 'xla.entry' functions
    This paves the way for CPU emitters.
    
    PiperOrigin-RevId: 717583000

--- xla/codegen/emitters/elemental_hlo_to_mlir.cc
+++ xla/codegen/emitters/elemental_hlo_to_mlir.cc
@@ -1186,8 +1186,15 @@ ValueRange ProvideParameter(const PartitionedComputation& computation,
   }
 
   auto callee = call_target_provider(operand);
-  SmallVector<Value> operands(
-      this_fn.getArguments().take_front(instr->parent()->num_parameters()));
+  SmallVector<Value> operands;
+  if (this_fn->getAttr("xla.entry")) {
+    operands =
+        SmallVector<Value>{this_fn.getArguments().drop_front().take_front(
+            instr->parent()->num_parameters())};
+  } else {
+    operands = SmallVector<Value>{
+        this_fn.getArguments().take_front(instr->parent()->num_parameters())};
+  }
   absl::c_copy(indices, std::back_inserter(operands));
   auto results = builder.create<PureCallOp>(callee, operands).getResults();
   auto callee_subgraph = computation.FindSubgraph(operand);
