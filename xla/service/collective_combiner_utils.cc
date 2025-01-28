835a1047188cc8db19c35cb30fd7de635432994c
Yunlong Liu <yunlongl@x.ai>
2025-01-28 19:37:25 +0000

    removes redundancy in the metadata.

--- xla/service/collective_combiner_utils.cc
+++ xla/service/collective_combiner_utils.cc
@@ -1,5 +1,6 @@
 #include "xla/service/collective_combiner_utils.h"
 
+#include "absl/container/flat_hash_set.h"
 #include "absl/strings/str_cat.h"
 
 namespace xla {
@@ -8,17 +9,20 @@ namespace xla {
 // as much as possible.
 OpMetadata MergeOpMetadata(absl::Span<HloInstruction* const> to_combine) {
   OpMetadata op_metadata;
-  std::string source_info;
+  absl::flat_hash_set<std::string> source_info_set;
   for (const HloInstruction* instr : to_combine) {
     const auto& metadata = instr->metadata();
     if (metadata.source_file().empty()) {
       continue;
     }
+    std::string source_info;
     absl::StrAppend(&source_info, metadata.source_file(), ":",
-                    metadata.source_line(), ",");
+                    metadata.source_line());
+    source_info_set.insert(std::move(source_info));
   }
-  source_info = absl::StripSuffix(source_info, ",");
-  op_metadata.set_source_file(source_info);
+  if (!source_info_set.empty()) {
+    op_metadata.set_source_file(absl::StrJoin(source_info_set, ","));
+  }
   return op_metadata;
 }
 
