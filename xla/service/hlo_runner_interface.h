48383f2873e8614afc678c3d7842ca1815d53d40
Ilia Sergachev <1894984+sergachev@users.noreply.github.com>
2025-02-13 02:26:57 -0800

    Update xla/service/hlo_runner_interface.h
    Co-authored-by: Allan Renucci <allanrenucci@users.noreply.github.com>

--- xla/service/hlo_runner_interface.h
+++ xla/service/hlo_runner_interface.h
@@ -233,7 +233,7 @@ class HloRunnerInterface {
   // Reads the hlo text dump file in HloModule::ToString format, creates and
   // returns the HloModule.
   static absl::StatusOr<std::unique_ptr<HloModule>> ReadModuleFromHloTextFile(
-      absl::string_view filename, const DebugOptions& = {},
+      absl::string_view filename, const DebugOptions& = DebugOptions::default_instance(),
       const HloParserOptions& options = HloParserOptions());
 
   // Creates a runner-internal executable object given an HLO module and returns
