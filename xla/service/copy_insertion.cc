5b0592791ae9891bd01fec7a6223fe0b0b580d90
Johannes Reifferscheid <jreiffers@nvidia.com>
2025-03-13 15:36:58 +0100

    Remove HloComputation::WhileCallInstruction.
    The function is deprecated and broken. I'll remove the remaining
    similar functions one by one.

--- xla/service/copy_insertion.cc
+++ xla/service/copy_insertion.cc
@@ -2114,7 +2114,7 @@ absl::Status CopyInsertion::AddCopiesForAsyncSendRecv(
 
   // For other cases that send/recv are outside of the while loop, live times
   // are disjoint. No copies are needed.
-  if (!parent->IsWhileBodyComputation()) {
+  if (!parent->GetUniqueCaller(HloOpcode::kWhile)) {
     return absl::OkStatus();
   }
 
