deb1cf5dfbb0ab5dc25d9b8b395f35148fe9c987
Akhil Goel <akhil.goel@intel.com>
2025-04-03 10:01:28 -0700

    SiLU activation

--- xla/service/cpu/onednn_util.cc
+++ xla/service/cpu/onednn_util.cc
@@ -67,6 +67,9 @@ dnnl::post_ops PopulateOneDnnPostOps(
       case OneDnnFusionConfig::SIGMOID:
         post_ops.append_eltwise(dnnl::algorithm::eltwise_logistic, 0.f, 0.f);
         break;
+      case OneDnnFusionConfig::SWISH:
+        post_ops.append_eltwise(dnnl::algorithm::eltwise_swish, 1.0f, 0.0f);
+        break;
       case OneDnnFusionConfig::BIAS: {
         *bias_md = fused_mds.at(fused_operand_idx);
         if (fused_operands_ref) {
--- xla/service/cpu/tests/onednn_convolution_test.cc
+++ xla/service/cpu/tests/onednn_convolution_test.cc
@@ -624,6 +624,30 @@ TEST_P(ConvolutionTest, Conv2DWithBiasAndGeluExactPattern2Test) {
   RunCompareAndMatchOptimizedHlo(outline, {"BIAS", "GELU_ERF"});
 }
 
+TEST_P(ConvolutionTest, Conv2DWithBiasAndSwishTest) {
+  const absl::string_view outline = R"(
+  HloModule convolution.test.with.bias.swish
+  ENTRY convolution.test.with.bias.swish {
+    arg.0 = $dtype[2,40,40,128] parameter(0)
+    arg.1 = $dtype[1,1,128,128] parameter(1)
+    constant.1 = $pdtype[] constant(1)
+    broadcast.1 = $pdtype[2,40,40,128] broadcast(constant.1), dimensions={}
+    convert.1 = $dtype[2,40,40,128] convert(broadcast.1)
+    convolution.0 = $dtype[2,40,40,128] convolution(arg.0, arg.1),
+                      window={size=1x1}, dim_labels=b01f_01io->b01f
+    constant.0 = $dtype[128]{0} constant({...})
+    broadcast.0 = $dtype[2,40,40,128] broadcast(constant.0), dimensions={3}
+    add.0 = $dtype[2,40,40,128] add(convolution.0, broadcast.0)
+    negate.0 = $dtype[2,40,40,128] negate(add.0)
+    exponential.0 = $dtype[2,40,40,128] exponential(negate.0)
+    add.1 = $dtype[2,40,40,128] add(convert.1, exponential.0)
+    divide.0 = $dtype[2,40,40,128] divide(convert.1, add.1)
+    ROOT multiply.0 = $dtype[2,40,40,128] multiply(divide.0, add.0)
+})";
+
+  RunCompareAndMatchOptimizedHlo(outline, {"BIAS", "SWISH"});
+}
+
 INSTANTIATE_TEST_SUITE_P(
     OneDnnConvolutionTestSuite, ConvolutionTest,
     ::testing::Values(F32, BF16, F16),
