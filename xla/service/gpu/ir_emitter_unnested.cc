35ffd2748248b15e4dd4760acf81b39b67783a2c
Henning Becker <hebecker@google.com>
2025-04-08 01:16:08 -0700

    Move CUB sort kernels behind FFI
    This means the cub_sort_thunk and the sort_rewriter
    won't directly depend on the CUB implementation anymore.
    
    Instead they go through the FFI registry.
    This decouples CUDA and ROCm implementations of CUB from the
    users of CUB and will also help with splitting the
    compiler from the runtime.
    
    This also removes the requirement for having a stub for SortRewriter.
    
    PiperOrigin-RevId: 745027614

--- xla/service/gpu/ir_emitter_unnested.cc
+++ xla/service/gpu/ir_emitter_unnested.cc
@@ -1021,7 +1021,8 @@ absl::Status IrEmitterUnnested::EmitCubDeviceRadixSort(
           : std::nullopt,
       operands, results, scratch, options.descending(),
       Product(operand_shape.dimensions()) /
-          operand_shape.dimensions(operand_shape.dimensions().size() - 1));
+          operand_shape.dimensions(operand_shape.dimensions().size() - 1),
+      ir_emitter_context_->platform_name());
   AddThunkToThunkSequence(std::move(thunk));
   return absl::OkStatus();
 }
