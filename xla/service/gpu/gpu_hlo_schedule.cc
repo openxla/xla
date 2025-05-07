491aba8738623ff6cb5328df13dc2bb7c3462c62
TJ Xu <tjx@nvidia.com>
2025-05-07 16:54:09 +0000

    force delay scheduling start to extend overlap interval

--- xla/service/gpu/gpu_hlo_schedule.cc
+++ xla/service/gpu/gpu_hlo_schedule.cc
@@ -267,6 +267,7 @@ SchedulerConfig MakeGPUSchedulerConfig(uint64_t memory_limit,
   config.collective_permute_overlap_limit = 1;
   config.use_real_cost_model = false;
   config.aggressive_scheduling_policies = true;
+  config.prioritize_compute_over_async_start = overlap_limit > 1;
   config.schedule_send_recvs = true;
   config.memory_limit = memory_limit;
   config.parallel_collective_overlap_limit = overlap_limit;
