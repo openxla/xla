41887f4bc8505dceb654b6b65e9c98eb91761e4f
cjkkkk <ske@nvidia.com>
2025-05-16 21:20:32 +0000

    fix clang format

--- xla/service/gpu/tests/gpu_fused_mha_test.cc
+++ xla/service/gpu/tests/gpu_fused_mha_test.cc
@@ -1388,8 +1388,8 @@ class FlashAttentionPagedAttention : public MultiHeadedAttentionTest {
 
 class FlashAttentionFlexAttention : public MultiHeadedAttentionTest {
  protected:
-  const std::string                                  // NOLINT
-  GetModuleFlash_Attention_Flex_Attention_Soft_Capping_BF16() {  // NOLINT
+  const std::string
+  GetModuleFlash_Attention_Flex_Attention_Soft_Capping_BF16() {
     const std::string hlo_text = R"(
     HloModule jit__unnamed_wrapped_function_, entry_computation_layout={(bf16[4,1024,4,64]{3,2,1,0}, bf16[4,1024,4,64]{3,2,1,0}, bf16[4,1024,4,64]{3,2,1,0})->bf16[4,1024,4,64]{3,2,1,0}}, allow_spmd_sharding_propagation_to_parameters={true,true,true}, allow_spmd_sharding_propagation_to_output={true}
 
@@ -1416,8 +1416,8 @@ class FlashAttentionFlexAttention : public MultiHeadedAttentionTest {
     return hlo_text;
   }
 
-  const std::string                                            // NOLINT
-  GetModuleFlash_Attention_Flex_Attention_Soft_Capping_Reference_BF16() {  // NOLINT
+  const std::string
+  GetModuleFlash_Attention_Flex_Attention_Soft_Capping_Reference_BF16() {
     const std::string hlo_text = R"(
     HloModule jit__unnamed_wrapped_function_, entry_computation_layout={(bf16[4,1024,4,64]{3,2,1,0}, bf16[4,1024,4,64]{3,2,1,0}, bf16[4,1024,4,64]{3,2,1,0})->bf16[4,1024,4,64]{3,2,1,0}}, allow_spmd_sharding_propagation_to_parameters={true,true,true}, allow_spmd_sharding_propagation_to_output={true}
 
@@ -1481,10 +1481,10 @@ class FlashAttentionFlexAttention : public MultiHeadedAttentionTest {
     }
     // Extend cudnn sdpa soft capping using flex attention
     std::string hlo_string =
-        GetModuleFlash_Attention_Flex_Attention_Soft_Capping_BF16();  // NOLINT
+        GetModuleFlash_Attention_Flex_Attention_Soft_Capping_BF16();
     // Reference implementation is pure xla sdpa soft capping.
     std::string hlo_string_ref =
-        GetModuleFlash_Attention_Flex_Attention_Soft_Capping_Reference_BF16();  // NOLINT
+        GetModuleFlash_Attention_Flex_Attention_Soft_Capping_Reference_BF16();
     EXPECT_TRUE(RunAndCompareTwoModules(hlo_string, hlo_string_ref,
                                         ErrorSpec{1e-3, 1e-3}));
   }
