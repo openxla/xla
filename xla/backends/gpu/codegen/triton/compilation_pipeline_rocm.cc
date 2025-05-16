25316156e8c2531c920bc22d16b8e28cc23ded46
gulsumgudukbay <gudukbay@gmail.com>
2025-05-16 05:24:09 +0000

    converted all LLVM::getFixedVectorType function calls to mlir::VectorType::get(4, i8_ty) since this commit: https://github.com/openxla/xla/commit/ca404278c0ab0b92169727d5e90c6e32662133b9#diff-f544cd3a6407e84b997c53037aeca8959233398347ba7bc48e61bb5f68f40a3f had these changes already and they were not carried to the file after cherry-pick

--- xla/backends/gpu/codegen/triton/compilation_pipeline_rocm.cc
+++ xla/backends/gpu/codegen/triton/compilation_pipeline_rocm.cc
@@ -152,7 +152,7 @@ absl::Status CreateTritonPipeline(mlir::OpPassManager* pm,
   pm->addPass(mlir::createCSEPass());
   pm->addPass(mlir::createSymbolDCEPass());
   pm->addPass(mt::createTritonAMDGPULowerInstructionSchedHintsPass(
-      cc.gfx_version(), num_stages, "none"));
+      cc.gfx_version(), num_stages));
   pm->addPass(mt::createConvertBuiltinFuncToLLVMPass(/*ftz=*/true));
   // There is no clusters in ROCm for now.
   out_cluster_info.clusterDimX = 1;
