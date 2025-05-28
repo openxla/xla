e7bcac41d5d6f4d549ecfcbc52ae08dbcd332324
songlin <Songlin.Piao@amd.com>
2025-05-28 13:22:11 +0000

    enabled matmul_perf_table_gen_run on rocm platforms (fixed clang format error)

--- xla/tools/matmul_perf_table_gen.h
+++ xla/tools/matmul_perf_table_gen.h
@@ -67,8 +67,8 @@ class MatmulPerfTableGen {
   };
 
   explicit MatmulPerfTableGen(Config config)
-      : runner_(PlatformUtil::GetPlatform("cuda").value()),
-        config_(std::move(config)) {};
+      : runner_(PlatformUtil::GetPlatform("gpu").value()),
+        config_(std::move(config)){};
 
   // Computes a performance table for a given `config`.
   DeviceHloInstructionProfiles ComputeTable();
