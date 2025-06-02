177fa7427b383f7e69701cea859cd679a936c5ba
Jane Liu <janeliu@nvidia.com>
2025-06-02 17:49:00 +0000

    Fix for tensorflow side error message

--- xla/stream_executor/cuda/cuda_executor_test.cc
+++ xla/stream_executor/cuda/cuda_executor_test.cc
@@ -144,9 +144,12 @@ TEST(CudaExecutorTest, CreateCollectiveMemoryAllocatorFailsForExcessiveSize) {
       executor->CreateMemoryAllocator(MemoryType::kCollective));
   constexpr uint64_t kTooBig = 1125899906842624;  // 1 PiB
   auto should_fail = allocator->Allocate(kTooBig);
+  EXPECT_FALSE(should_fail.ok());
   EXPECT_THAT(should_fail.status().ToString(),
-              testing::HasSubstr("failed to allocate 1.00PiB (1125899906842624 "
-                                 "bytes) from device collective memory:"));
+              testing::AnyOf(testing::HasSubstr(
+                                 "failed to allocate 1.00PiB (1125899906842624 "
+                                 "bytes) from device collective memory:"),
+                             testing::HasSubstr("out of memory")));
 }
 
 TEST(CudaExecutorTest, CreateUnsupportedMemoryAllocatorsFail) {
