tensorflow/tensorflow

Use `.default.bzl` naming scheme consistently
PiperOrigin-RevId: 739219672

third_party/mpitrampoline/mpitrampoline.BUILD
@@ -2,7 +2,7 @@
 #  A forwarding MPI implementation that can use any other MPI implementation via an MPI ABI
 
 load("@bazel_skylib//rules:expand_template.bzl", "expand_template")
-load("@local_xla//xla:strict.default.bzl", "py_strict_binary")
+load("@local_xla//xla:py_strict.bzl", "py_strict_binary")
 
 package(
     default_visibility = ["//visibility:public"],

openxla/xla

third_party/mpitrampoline/mpitrampoline.BUILD
@@ -2,7 +2,7 @@
 #  A forwarding MPI implementation that can use any other MPI implementation via an MPI ABI
 
 load("@bazel_skylib//rules:expand_template.bzl", "expand_template")
-load("@xla//xla:strict.default.bzl", "py_strict_binary")
+load("@xla//xla:py_strict.bzl", "py_strict_binary")
 
 package(
     default_visibility = ["//visibility:public"],