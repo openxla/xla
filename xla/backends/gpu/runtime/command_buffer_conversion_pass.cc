5977daebb41d63a015ca1edbaf11fa2bde92b864
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2025-08-28 08:11:36 -0700

    adding convolution to command buffers
    added UTs  and convolution command
    
    test fixes
    
    added rebase fixes
    
    capture only those convolution targets which are explictly
    
    Revert "adding coll permute and convolution to command buffers"
    
    This reverts commit 75847e67261b4589162411c9846ed9c0b9fc1ed5.
    
    added conv to command buffers
    
    fixing build and test
    
    fixing build
    
    rewritten ConvolutionCmd, adapted command_buffer_conv_pass
    
    some cosmetics
    
    added extra param for convolution cmd buf
    
    small fix
    
    fix after rebase
    
    fixing after rebase
    
    simplified runner cache call
    
    WIP fixing compile errors and new conv test
    
    updated the conv subtest and ensure command buffer update is called
    
    clang format
    
    fixing test
    
    applied clang format
    
    applied clang format
    
    fixing test

--- xla/backends/gpu/runtime/command_buffer_conversion_pass.cc
+++ xla/backends/gpu/runtime/command_buffer_conversion_pass.cc
@@ -87,7 +87,8 @@ CommandBufferConfig GetCommandBufferConfig(
                                                 DebugOptions::WHILE};
   static constexpr auto kRequireTracing = {
       DebugOptions::CUBLAS, DebugOptions::CUBLASLT, DebugOptions::CUDNN,
-      DebugOptions::CUSTOM_CALL, DebugOptions::COLLECTIVES};
+      DebugOptions::CUSTOM_CALL, DebugOptions::COLLECTIVES, 
+      DebugOptions::CONVOLUTION};
 
   auto erase = [&](absl::Span<const DebugOptions::CommandBufferCmdType> cmds) {
     for (auto cmd : cmds) {
@@ -161,6 +162,8 @@ std::optional<DebugOptions::CommandBufferCmdType> GetCommandBufferCmdType(
       return DebugOptions::COLLECTIVES;
     case Thunk::kCuDnn:
       return DebugOptions::CUDNN;
+    case Thunk::kConvolution:
+      return DebugOptions::CONVOLUTION;
     case Thunk::kCustomCall:
       return DebugOptions::CUSTOM_CALL;
     case Thunk::kCublasLtMatmul:
