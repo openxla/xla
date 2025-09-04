d5997e02cb30bad52ec95509f4ff760ce9c52121
xla authors <google-ml-automation@google.com>
2025-09-04 03:55:37 -0700

    [SymbolicExpr] Make `ReplaceDimsAndSymbols` more flexible by allowing partial dimension replacements.
    The `ReplaceDimsAndSymbols` function now takes a `num_dims` argument. If `dim_replacements` does not cover all dimensions from 0 to `num_dims - 1`, the unreplaced dimensions are kept as their original symbolic variables.
    
    PiperOrigin-RevId: 802956587

--- xla/service/gpu/model/experimental/symbolic_expr.h
+++ xla/service/gpu/model/experimental/symbolic_expr.h
@@ -73,6 +73,18 @@ class SymbolicExpr {
   int64_t Evaluate(absl::Span<const int64_t> variable_values) const;
   SymbolicExpr ReplaceVariables(
       absl::Span<const SymbolicExpr> substitutions) const;
+  // TODO(karupayun): These methods are needed for IndexingMap, but dimensions
+  // and symbols are SymbolicMap specific. We should remove them once we have a
+  // better way to integrate SymbolicExpr with IndexingMap. It is assuming that
+  // dimensions are the first (0...num_dims-1) variables and symbols are the
+  // rest.
+  SymbolicExpr ReplaceSymbols(absl::Span<const SymbolicExpr> replacements,
+                              int64_t num_dims) const;
+  SymbolicExpr ReplaceDimsAndSymbols(
+      absl::Span<const SymbolicExpr> dim_replacements,
+      absl::Span<const SymbolicExpr> symbol_replacements,
+      int64_t num_dims) const;
+
   SymbolicExpr Canonicalize() const;
 
   /// Sparse replace method. Replace `expr` by `replacement` and return the
