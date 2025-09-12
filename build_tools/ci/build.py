openxla/xla

commit 06ea00b30494e0d73dc29e7d3e785b09d1437213

Now that we don't steal from `tensorflow/third_party` don't do any post hoc Copybara for TensorFlow builds
(*) Except for `tensorflow/third_party/py`

PiperOrigin-RevId: 782106216

build_tools/ci/build.py
@@ -648,34 +648,6 @@ Build(
         color="yes",
     ),
     repo_env={"USE_PYWRAP_RULES": "True"},
-    extra_setup_commands=(
-        # This is pretty devious - but we have to do some adhoc extra Copybara
-        # work here to get XLA into the shape TF expects. b/407638223
-        # pyformat:disable
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            "-type", "f",
-            "-exec", "sed", "-i", "s/@xla/@local_xla/g", "{}", "+",
-        ],
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            "-type", "f",
-            "-exec", "sed", "-i", "s/@tsl/@local_tsl/g", "{}", "+",
-        ],
-        [
-            "cp", "-r",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            f"{_GITHUB_WORKSPACE}/tensorflow/tensorflow/third_party",
-        ],
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla/third_party/",
-            "-maxdepth", "1", "-exec", "cp", "-r", "{}",
-            f"{_GITHUB_WORKSPACE}/tensorflow/tensorflow/third_party", ";",
-        ],
-    ),
 )
 
 Build(
@@ -705,32 +677,6 @@ Build(
     ),
     repo_env={"USE_PYWRAP_RULES": "True"},
     extra_setup_commands=(
-        # This is pretty devious - but we have to do some adhoc extra Copybara
-        # work here to get XLA into the shape TF expects. b/407638223
-        # pyformat:disable
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            "-type", "f",
-            "-exec", "sed", "-i", "s/@xla/@local_xla/g", "{}", "+",
-        ],
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            "-type", "f",
-            "-exec", "sed", "-i", "s/@tsl/@local_tsl/g", "{}", "+",
-        ],
-        [
-            "cp", "-r",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            f"{_GITHUB_WORKSPACE}/tensorflow/tensorflow/third_party",
-        ],
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla/third_party/",
-            "-maxdepth", "1", "-exec", "cp", "-r", "{}",
-            f"{_GITHUB_WORKSPACE}/tensorflow/tensorflow/third_party", ";",
-        ],
         ["nvidia-smi"],
     ),
 )

commit 3e12b6bed2e3ad40016db37d2b119756a3358fa6

Use `local_xla` rather than `xla` when overriding repository for TensorFlow builds
Example log where TF reads the changed XLA: https://github.com/openxla/xla/actions/runs/17687532820/job/50275317061?pr=31310#step:6:418

This fixes the issue introduced by https://github.com/openxla/xla/commit/06ea00b30494e0d73dc29e7d3e785b09d1437213. In that commit, I mistakenly removed the extra `setup_commands` without reintroducing `override_repository`. This rectifies it - we still need the sed commands to ensure that things inside of xla which refer to their repo explicitly like `@xla` or `@tsl` use the `@local_xla` or `@local_tsl` repo names like TensorFlow expects due to vendoring.

The other sed commands previously used are no longer necessary after b/391640066, since TensorFlow always refers to XLA by using a label that references the repo itself instead of the vendored location (`@local_xla//` vs `//third_party/xla`)

PiperOrigin-RevId: 806427200

build_tools/ci/build.py
@@ -644,9 +644,26 @@ Build(
         color="yes",
     ),
     override_repository=dict(
-        xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
+        local_xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
     ),
     repo_env={"USE_PYWRAP_RULES": "True"},
+    extra_setup_commands=(
+        # This is pretty devious - but we have to do some adhoc extra Copybara
+        # work here to get XLA into the shape TF expects. b/407638223
+        # pyformat:disable
+        [
+            "find",
+            f"{_GITHUB_WORKSPACE}/openxla/xla",
+            "-type", "f",
+            "-exec", "sed", "-i", "s/@xla/@local_xla/g", "{}", "+",
+        ],
+        [
+            "find",
+            f"{_GITHUB_WORKSPACE}/openxla/xla",
+            "-type", "f",
+            "-exec", "sed", "-i", "s/@tsl/@local_tsl/g", "{}", "+",
+        ],
+    ),
 )
 
 Build(
@@ -668,7 +685,7 @@ Build(
     build_tag_filters=tensorflow_gpu_tag_filters,
     test_tag_filters=tensorflow_gpu_tag_filters,
     override_repository=dict(
-        xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
+        local_xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
     ),
     options=dict(
         verbose_failures=True,
@@ -678,7 +695,24 @@ Build(
         color="yes",
     ),
     repo_env={"USE_PYWRAP_RULES": "True"},
-    extra_setup_commands=(["nvidia-smi"],),
+    extra_setup_commands=(
+        # This is pretty devious - but we have to do some adhoc extra Copybara
+        # work here to get XLA into the shape TF expects. b/407638223
+        # pyformat:disable
+        [
+            "find",
+            f"{_GITHUB_WORKSPACE}/openxla/xla",
+            "-type", "f",
+            "-exec", "sed", "-i", "s/@xla/@local_xla/g", "{}", "+",
+        ],
+        [
+            "find",
+            f"{_GITHUB_WORKSPACE}/openxla/xla",
+            "-type", "f",
+            "-exec", "sed", "-i", "s/@tsl/@local_tsl/g", "{}", "+",
+        ],
+        ["nvidia-smi"],
+    ),
 )
 
 