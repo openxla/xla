6a85e5cfc4b1696b47df94b7e1082567e3cdac8a
cj401-amd <chunyjin@amd.com>
2025-09-18 08:28:08 +0000

    update the macro

--- xla/backends/profiler/gpu/rocm_tracer_utils.h
+++ xla/backends/profiler/gpu/rocm_tracer_utils.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef XLA_BACKENDS_PROFILER_GPU_ROCM_UTILS_H_
-#define XLA_BACKENDS_PROFILER_GPU_ROCM_UTILS_H_
+#ifndef XLA_BACKENDS_PROFILER_GPU_ROCM_TRACER_UTILS_H_
+#define XLA_BACKENDS_PROFILER_GPU_ROCM_TRACER_UTILS_H_
 
 #include <cstdint>
 #include <cstring>
@@ -203,4 +203,4 @@ class AnnotationMap {
 }  // namespace profiler
 }  // namespace xla
 
-#endif  // XLA_BACKENDS_PROFILER_GPU_ROCM_UTILS_H_
+#endif  // XLA_BACKENDS_PROFILER_GPU_ROCM_TRACER_UTILS_H_
