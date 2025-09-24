905d0596d199598036032f0f84b4487e9afd2bef
Alexander Grund <alexander.grund@tu-dresden.de>
2025-09-24 17:11:29 +0200

    Don't add empty TF_CUDA_TOOLKIT_PATH to libdevice search
    At least in some environments that define is the empty string which
    doesn't make sense to add to the search paths.
    Add a check for that.

--- xla/tsl/platform/default/cuda_root_path.cc
+++ xla/tsl/platform/default/cuda_root_path.cc
@@ -68,7 +68,9 @@ std::vector<std::string> CandidateCudaRoots() {
   const char* cuda_home = getenv("CUDA_HOME");
   if (cuda_home)
     roots.emplace_back(cuda_home);
-  roots.push_back(TF_CUDA_TOOLKIT_PATH);
+  std::string cuda_toolkit_path = TF_CUDA_TOOLKIT_PATH;
+  if (!cuda_toolkit_path.empty())
+    roots.push_back(std::move(cuda_toolkit_path));
   roots.emplace_back(std::string("/usr/local/cuda"));
   roots.emplace_back(std::string("/opt/cuda"));
 
