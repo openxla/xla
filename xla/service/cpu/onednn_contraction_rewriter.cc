9a4ba20c1624fb3d51e3a43cb805e1f4ce7dede7
Om Thakkar <om.thakkar@intel.com>
2025-09-30 13:40:04 -0700

    fixup: onednn_contraction_rewriter - lint related

--- xla/service/cpu/onednn_contraction_rewriter.cc
+++ xla/service/cpu/onednn_contraction_rewriter.cc
@@ -554,7 +554,6 @@ bool OneDnnContractionRewriter::ShouldRewriteDot(
 
 bool OneDnnContractionRewriter::ShouldRewriteConv(
     const HloInstruction* conv_instr) {
-  // NOLINTBEGIN(clang-diagnostic-unreachable-code)
   if (conv_instr->opcode() != HloOpcode::kConvolution ||
       conv_instr->HasControlDependencies() ||
       !IsSupportedType(conv_instr->shape().element_type()) ||
@@ -588,7 +587,6 @@ bool OneDnnContractionRewriter::ShouldRewriteConv(
   }
 
   return true;
-  // NOLINTEND(clang-diagnostic-unreachable-code)
 }
 
 class OneDnnContractionRewriteVisitor : public DfsHloRewriteVisitor {
