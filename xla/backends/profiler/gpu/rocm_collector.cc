925b9c2d1412c0530eaed7ddfefe73d80c56afad
cj401-amd <chunyjin@amd.com>
2025-11-12 15:55:14 -0600

    update flag and id in xla.proto for rocm_max_trace_events

--- xla/backends/profiler/gpu/rocm_collector.cc
+++ xla/backends/profiler/gpu/rocm_collector.cc
@@ -469,9 +469,11 @@ void RocmTraceCollectorImpl::AddEvent(RocmTracerEvent&& event,
   if (event.source == RocmTracerEventSource::ApiCallback) {
     if (!is_auxiliary) {
       if (num_callback_events_ >= options_.max_callback_api_events) {
-        OnEventsDropped("max callback event capacity reached",
-                        event.correlation_id);
-        PrintRocmTracerEvent(event, ". Dropped!");
+        LOG(WARNING) << "!!! Number of callback events = "
+                     << num_callback_events_
+                     << " is greater than/equal to the max callback api events = "
+                     << options_.max_callback_api_events
+                     << ". To collect more GPU events, please set XLA_FLAGS=--xla_gpu_rocm_max_trace_events=X ";
         return;
       }
       num_callback_events_++;
@@ -488,9 +490,11 @@ void RocmTraceCollectorImpl::AddEvent(RocmTracerEvent&& event,
     if (event.domain == RocmTracerEventDomain::HIP_API) {
       // we do not count HIP_OPS activities.
       if (num_activity_events_ >= options_.max_activity_api_events) {
-        OnEventsDropped("max activity event capacity reached",
-                        event.correlation_id);
-        PrintRocmTracerEvent(event, ". Dropped!");
+        LOG(WARNING) << "!!! Number of activity events = "
+                     << num_activity_events_
+                     << " is greater than/equal to the max activity api events = "
+                     << options_.max_activity_api_events
+                     << ". To collect more GPU events, please set XLA_FLAGS=--xla_gpu_rocm_max_trace_events=X ";
         return;
       }
       num_activity_events_++;
