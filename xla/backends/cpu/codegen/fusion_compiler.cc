56588c53b46a7ec5ad1db339785863c646815c5a
Will Froom <willfroom@google.com>
2025-11-19 02:40:24 -0800

    Fold no-op reshape when converting to linalg.
    Prior to this change we would hit an assert when constructing the tensor::CollapseShapeOp in the 0D->0D case.
    
    PiperOrigin-RevId: 834202393

--- xla/backends/cpu/codegen/fusion_compiler.cc
+++ xla/backends/cpu/codegen/fusion_compiler.cc
@@ -317,7 +317,13 @@ static void AddTiledOptimizationPasses(mlir::OpPassManager& pm) {
   pm.addNestedPass<mlir::func::FuncOp>(
       mlir::vector::createLowerVectorMultiReductionPass(
           mlir::vector::VectorMultiReductionLowering::InnerParallel));
-  pm.addPass(CreateTensorOpsToVectorPass());
+  pm.addPass(CreateTensorOpsToBufferizablePass());
+
+  mlir::stablehlo::StablehloLegalizeToLinalgPassOptions
+      stablehlo_to_linalg_options;
+  stablehlo_to_linalg_options.enablePrimitiveOps = true;
+  pm.addPass(mlir::stablehlo::createStablehloLegalizeToLinalgPass());
+  pm.addPass(xtile::createConvertElementwise0DTensorToScalarPass());
 
   pm.addPass(mlir::createConvertElementwiseToLinalgPass());
   pm.addPass(CreateFuseElementwisePass());
@@ -325,6 +331,9 @@ static void AddTiledOptimizationPasses(mlir::OpPassManager& pm) {
   AddBufferizationPasses(pm);
 
   pm.addPass(CreateLinalgElementwiseToVectorPass());
+
+  pm.addPass(mlir::createCanonicalizerPass());
+  pm.addPass(mlir::createCSEPass());
 }
 
 // Lowering passes for the tiled emitter.
@@ -342,6 +351,8 @@ static void AddTiledLoweringPasses(mlir::OpPassManager& pm, bool fast_min_max) {
 
   pm.addPass(mlir::createConvertComplexToStandardPass());
   pm.addPass(mlir::memref::createExpandStridedMetadataPass());
+
+  pm.addPass(emitters::CreateSafeIntegerArithmeticPass());
 
   AddGenericLoweringPasses(pm, fast_min_max);
 }
