bfd4a77bf896d61301ccf8b349b723c3ea4329a5
Alexandros Theodoridis <atheodor@amd.com>
2025-11-27 14:24:26 +0000

    Use rocminfo instead of lspci as it will report all connected gpus even inside docker container

--- build_tools/rocm/parallel_gpu_execute.sh
+++ build_tools/rocm/parallel_gpu_execute.sh
@@ -21,7 +21,7 @@
 # Required environment variables:
 #     TF_GPU_COUNT = Number of GPUs available.
 
-TF_GPU_COUNT=$(lspci | grep -e 'controller' -e 'accelerators' | grep 'AMD/ATI' | wc -l)
+TF_GPU_COUNT=$(rocminfo | grep "Name: *gfx*" | wc -l)
 TF_TESTS_PER_GPU=${TF_TESTS_PER_GPU:-8}
 
 # This function is used below in rlocation to check that a path is absolute
