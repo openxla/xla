ai-jz/tensorflow

[XLA:GPU] Expand device description to include fine-grained execution unit info
We need accurate peak FLOP/s for the GEMM cost model. Both AMD and NVidia have similar distinctions between standard and accelerated computation. Performance characteristics are different per data type.

This change introduces a data structure that can be used to estimate peak ops more precisely. Along with it we add a simple utility function for this estimation and an example usage in tests.

Filling the actual device properties in the executor will be coming in following changes.

PiperOrigin-RevId: 839249186

third_party/xla/xla/service/gpu/model/gpu_performance_model_base_test.cc
@@ -32,7 +32,7 @@ limitations under the License.
 #include "xla/service/gpu/model/gpu_hlo_cost_analysis.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/tsl/platform/statusor.h"
-#include "tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
@@ -328,6 +328,26 @@ TEST_F(GpuPerformanceModelBaseTest,
       h100_device_info, /*num_blocks=*/h100_device_info.core_count(),
       /*num_threads_per_block=*/h100_device_info.fpus_per_core());
   // H100 has a peak of 66.9 TFLOPS/s for TF32.
+  EXPECT_GT(flops_per_ns, 66000);
+  EXPECT_LT(flops_per_ns, 68000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakFP16FlopsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixFlopsPerNs(
+      h100_device_info, xla::PrimitiveType::BF16);
+  // H100 has a peak of 989.4 TFLOPS/s for BF16.
+  EXPECT_GT(flops_per_ns, 988000);
+  EXPECT_LT(flops_per_ns, 991000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakFP64FlopsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixFlopsPerNs(
+      h100_device_info, xla::PrimitiveType::F64);
+  // H100 has a peak of 66.8 TFLOPS/s for FP64.
   EXPECT_GT(flops_per_ns, 66000);
   EXPECT_LT(flops_per_ns, 68000);
 }

third_party/xla/xla/stream_executor/device_description.cc
@@ -30,6 +30,35 @@ limitations under the License.
 
 namespace stream_executor {
 
+ExecutionUnitDescriptionProto ExecutionUnitDescription::ToProto() const {
+  ExecutionUnitDescriptionProto proto;
+  for (const auto& [type, info] : rate_infos_) {
+    auto& entry = (*proto.mutable_rate_infos())[type];
+    entry.set_clock_rate_ghz(info.clock_rate_ghz);
+    entry.set_units_per_core(info.units_per_core);
+    entry.set_ops_per_clock(info.ops_per_clock);
+  }
+  return proto;
+}
+
+absl::StatusOr<ExecutionUnitDescription> ExecutionUnitDescription::FromProto(
+    const ExecutionUnitDescriptionProto& proto) {
+  ExecutionUnitDescription desc;
+  for (const auto& [type_int, info_proto] : proto.rate_infos()) {
+    if (!xla::PrimitiveType_IsValid(type_int)) {
+      VLOG(2) << "Invalid PrimitiveType encountered, ExecutionUnitDescription "
+                 "might be malformed: "
+              << type_int;
+      continue;
+    }
+    desc.SetRateInfo(
+        static_cast<xla::PrimitiveType>(type_int),
+        RateInfo{info_proto.units_per_core(), info_proto.clock_rate_ghz(),
+                 info_proto.ops_per_clock()});
+  }
+  return desc;
+}
+
 absl::StatusOr<DeviceDescription> DeviceDescription::FromProto(
     const GpuDeviceInfoProto& proto) {
   DeviceDescription device_description;
@@ -64,6 +93,17 @@ absl::StatusOr<DeviceDescription> DeviceDescription::FromProto(
   device_description.core_count_ = proto.core_count();
   device_description.fpus_per_core_ = proto.fpus_per_core();
 
+  if (proto.has_scalar_unit_description()) {
+    TF_ASSIGN_OR_RETURN(
+        device_description.scalar_unit_description_,
+        ExecutionUnitDescription::FromProto(proto.scalar_unit_description()));
+  }
+  if (proto.has_matrix_unit_description()) {
+    TF_ASSIGN_OR_RETURN(
+        device_description.matrix_unit_description_,
+        ExecutionUnitDescription::FromProto(proto.matrix_unit_description()));
+  }
+
   return device_description;
 }
 
@@ -93,6 +133,14 @@ GpuDeviceInfoProto DeviceDescription::ToGpuProto() const {
   proto.set_device_memory_size(device_memory_size_);
   proto.set_registers_per_core_limit(registers_per_core_limit_);
   proto.set_registers_per_block_limit(registers_per_block_limit_);
+  if (scalar_unit_description_.has_value()) {
+    *proto.mutable_scalar_unit_description() =
+        scalar_unit_description_->ToProto();
+  }
+  if (matrix_unit_description_.has_value()) {
+    *proto.mutable_matrix_unit_description() =
+        matrix_unit_description_->ToProto();
+  }
   return proto;
 }
 
@@ -100,7 +148,7 @@ std::string DeviceDescription::ToString() const {
   return ToGpuProto().DebugString();
 }
 
-const GpuComputeCapability &DeviceDescription::gpu_compute_capability() const {
+const GpuComputeCapability& DeviceDescription::gpu_compute_capability() const {
   return gpu_compute_capability_;
 }
 
@@ -119,8 +167,8 @@ RocmComputeCapability DeviceDescription::rocm_compute_capability() const {
   return RocmComputeCapability{};
 }
 
-bool ThreadDimOk(const DeviceDescription &device_description,
-                 const ThreadDim &thread_dim) {
+bool ThreadDimOk(const DeviceDescription& device_description,
+                 const ThreadDim& thread_dim) {
   const int64_t total_threads = thread_dim.x * thread_dim.y * thread_dim.z;
   const int64_t threads_per_block_limit =
       device_description.threads_per_block_limit();
@@ -130,7 +178,7 @@ bool ThreadDimOk(const DeviceDescription &device_description,
     return false;
   }
 
-  const auto &limit = device_description.thread_dim_limit();
+  const auto& limit = device_description.thread_dim_limit();
   bool ok = thread_dim.x <= limit.x && thread_dim.y <= limit.y &&
             thread_dim.z <= limit.z;
   if (!ok) {
@@ -140,9 +188,9 @@ bool ThreadDimOk(const DeviceDescription &device_description,
   return ok;
 }
 
-void CalculateDimensionality(const DeviceDescription &device_description,
-                             int64_t element_count, int64_t *threads_per_block,
-                             int64_t *block_count) {
+void CalculateDimensionality(const DeviceDescription& device_description,
+                             int64_t element_count, int64_t* threads_per_block,
+                             int64_t* block_count) {
   *threads_per_block = device_description.threads_per_block_limit();
   *block_count = tsl::MathUtil::CeilOfRatio(element_count, *threads_per_block);
   if (*block_count == 1) {

third_party/xla/xla/stream_executor/device_description.h
@@ -22,18 +22,80 @@ limitations under the License.
 
 #include <cassert>
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <utility>
 #include <variant>
 
+#include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
+#include "absl/types/span.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.pb.h"
 #include "xla/stream_executor/launch_dim.h"
 #include "xla/stream_executor/rocm/rocm_compute_capability.h"
 #include "xla/stream_executor/semantic_version.h"
+#include "xla/xla_data.pb.h"
 
 namespace stream_executor {
+
+// Describes the capabilities and performance characteristics of a specific
+// execution unit within a device, such as scalar units (e.g., CUDA Cores) or
+// matrix units (e.g., Tensor Cores).
+class ExecutionUnitDescription {
+ public:
+  // Information about operations on a particular datatype.
+  struct RateInfo {
+    int32_t units_per_core = 0;
+    float clock_rate_ghz = 0.;
+    int32_t ops_per_clock = 0;
+
+    bool operator==(const RateInfo& rhs) const {
+      return units_per_core == rhs.units_per_core &&
+             clock_rate_ghz == rhs.clock_rate_ghz &&
+             ops_per_clock == rhs.ops_per_clock;
+    }
+
+    bool operator!=(const RateInfo& rhs) const { return !(*this == rhs); }
+  };
+
+  void SetRateInfo(xla::PrimitiveType dtype, RateInfo rate_info) {
+    rate_infos_[dtype] = rate_info;
+  }
+
+  void SetRateInfo(absl::Span<const xla::PrimitiveType> dtypes,
+                   RateInfo rate_info) {
+    for (xla::PrimitiveType dtype : dtypes) {
+      SetRateInfo(dtype, rate_info);
+    }
+  }
+
+  // Returns the RateInfo for a specific PrimitiveType, or std::nullopt if
+  // no information is available for the given dtype.
+  std::optional<RateInfo> GetRateInfo(xla::PrimitiveType dtype) const {
+    auto it = rate_infos_.find(dtype);
+    if (it == rate_infos_.end()) {
+      return std::nullopt;
+    }
+    return it->second;
+  }
+
+  ExecutionUnitDescriptionProto ToProto() const;
+
+  static absl::StatusOr<ExecutionUnitDescription> FromProto(
+      const ExecutionUnitDescriptionProto& proto);
+
+  bool operator==(const ExecutionUnitDescription& other) const {
+    return rate_infos_ == other.rate_infos_;
+  }
+
+  bool operator!=(const ExecutionUnitDescription& other) const {
+    return !(*this == other);
+  }
+
+ private:
+  absl::flat_hash_map<xla::PrimitiveType, RateInfo> rate_infos_;
+};
 
 class GpuComputeCapability {
  public:
@@ -167,6 +229,26 @@ class DeviceDescription {
   // Number of floating point operations one core (SM, compute unit) can execute
   // in parallel. Corresponds to the number of "CUDA cores" for NVIDIA devices.
   int fpus_per_core() const { return fpus_per_core_; }
+
+  // Returns a pointer to the description of the scalar execution unit, or
+  // nullptr if not available.
+  // These units are typically referred to as "CUDA Cores" or "FP32/FP64/INT32
+  // Cores" on NVIDIA and "Stream Processors" on AMD devices.
+  const ExecutionUnitDescription* scalar_unit_description() const {
+    return scalar_unit_description_.has_value()
+               ? &scalar_unit_description_.value()
+               : nullptr;
+  }
+
+  // Returns a pointer to the description of the matrix execution unit, or
+  // nullptr if not available.
+  // These units are known as "Tensor Cores" on NVIDIA and "Matrix Cores" on
+  // AMD.
+  const ExecutionUnitDescription* matrix_unit_description() const {
+    return matrix_unit_description_.has_value()
+               ? &matrix_unit_description_.value()
+               : nullptr;
+  }
 
   // Returns the limit on the thread dimensionality values in each of the
   // respective dimensions. These limits affect what constitutes a legitimate
@@ -402,11 +484,17 @@ class DeviceDescription {
     interconnect_info_ = std::move(info);
   }
 
+  void set_scalar_unit_description(ExecutionUnitDescription descr) {
+    scalar_unit_description_ = std::move(descr);
+  }
+
+  void set_matrix_unit_description(ExecutionUnitDescription descr) {
+    matrix_unit_description_ = std::move(descr);
+  }
+
  private:
   // For description of the following members, see the corresponding accessor
   // above.
-  //
-  // N.B. If another field is added, update ToMap() above.
   std::string device_vendor_ = kUndefinedString;
   std::string platform_version_ = kUndefinedString;
   std::string pci_bus_id_ = kUndefinedString;
@@ -449,6 +537,9 @@ class DeviceDescription {
   int core_count_ = kUninitialized<int>;
   int fpus_per_core_ = kUninitialized<int>;
   bool ecc_enabled_ = false;
+
+  std::optional<ExecutionUnitDescription> scalar_unit_description_;
+  std::optional<ExecutionUnitDescription> matrix_unit_description_;
 
   SemanticVersion driver_version_{0, 0, 0};
   SemanticVersion runtime_version_{0, 0, 0};

openxla/xla

xla/service/gpu/model/gpu_performance_model_base_test.cc
@@ -32,7 +32,7 @@ limitations under the License.
 #include "xla/service/gpu/model/gpu_hlo_cost_analysis.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/tsl/platform/statusor.h"
-#include "tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
@@ -328,6 +328,26 @@ TEST_F(GpuPerformanceModelBaseTest,
       h100_device_info, /*num_blocks=*/h100_device_info.core_count(),
       /*num_threads_per_block=*/h100_device_info.fpus_per_core());
   // H100 has a peak of 66.9 TFLOPS/s for TF32.
+  EXPECT_GT(flops_per_ns, 66000);
+  EXPECT_LT(flops_per_ns, 68000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakFP16OpsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixOpsPerNs(
+      h100_device_info, xla::PrimitiveType::BF16);
+  // H100 has a peak of 989.4 TFLOPS/s for BF16.
+  EXPECT_GT(flops_per_ns, 988000);
+  EXPECT_LT(flops_per_ns, 991000);
+}
+
+TEST_F(GpuPerformanceModelBaseTest, CalculatePeakFP64OpsPerNsH100) {
+  se::DeviceDescription h100_device_info =
+      TestGpuDeviceInfo::RTXH100SXMDeviceInfo();
+  int64_t flops_per_ns = GpuPerformanceModelBase::CalculatePeakMatrixOpsPerNs(
+      h100_device_info, xla::PrimitiveType::F64);
+  // H100 has a peak of 66.8 TFLOPS/s for FP64.
   EXPECT_GT(flops_per_ns, 66000);
   EXPECT_LT(flops_per_ns, 68000);
 }

xla/stream_executor/device_description.cc
@@ -30,6 +30,35 @@ limitations under the License.
 
 namespace stream_executor {
 
+ExecutionUnitDescriptionProto ExecutionUnitDescription::ToProto() const {
+  ExecutionUnitDescriptionProto proto;
+  for (const auto& [type, info] : rate_infos_) {
+    auto& entry = (*proto.mutable_rate_infos())[type];
+    entry.set_clock_rate_ghz(info.clock_rate_ghz);
+    entry.set_units_per_core(info.units_per_core);
+    entry.set_ops_per_clock(info.fused_ops_per_clock);
+  }
+  return proto;
+}
+
+absl::StatusOr<ExecutionUnitDescription> ExecutionUnitDescription::FromProto(
+    const ExecutionUnitDescriptionProto& proto) {
+  ExecutionUnitDescription desc;
+  for (const auto& [type_int, info_proto] : proto.rate_infos()) {
+    if (!xla::PrimitiveType_IsValid(type_int)) {
+      VLOG(2) << "Invalid PrimitiveType encountered, ExecutionUnitDescription "
+                 "might be malformed: "
+              << type_int;
+      continue;
+    }
+    desc.SetRateInfo(
+        static_cast<xla::PrimitiveType>(type_int),
+        RateInfo{info_proto.units_per_core(), info_proto.clock_rate_ghz(),
+                 info_proto.ops_per_clock()});
+  }
+  return desc;
+}
+
 absl::StatusOr<DeviceDescription> DeviceDescription::FromProto(
     const GpuDeviceInfoProto& proto) {
   DeviceDescription device_description;
@@ -64,6 +93,17 @@ absl::StatusOr<DeviceDescription> DeviceDescription::FromProto(
   device_description.core_count_ = proto.core_count();
   device_description.fpus_per_core_ = proto.fpus_per_core();
 
+  if (proto.has_scalar_unit_description()) {
+    TF_ASSIGN_OR_RETURN(
+        device_description.scalar_unit_description_,
+        ExecutionUnitDescription::FromProto(proto.scalar_unit_description()));
+  }
+  if (proto.has_matrix_unit_description()) {
+    TF_ASSIGN_OR_RETURN(
+        device_description.matrix_unit_description_,
+        ExecutionUnitDescription::FromProto(proto.matrix_unit_description()));
+  }
+
   return device_description;
 }
 
@@ -93,6 +133,14 @@ GpuDeviceInfoProto DeviceDescription::ToGpuProto() const {
   proto.set_device_memory_size(device_memory_size_);
   proto.set_registers_per_core_limit(registers_per_core_limit_);
   proto.set_registers_per_block_limit(registers_per_block_limit_);
+  if (scalar_unit_description_.has_value()) {
+    *proto.mutable_scalar_unit_description() =
+        scalar_unit_description_->ToProto();
+  }
+  if (matrix_unit_description_.has_value()) {
+    *proto.mutable_matrix_unit_description() =
+        matrix_unit_description_->ToProto();
+  }
   return proto;
 }
 
@@ -100,7 +148,7 @@ std::string DeviceDescription::ToString() const {
   return ToGpuProto().DebugString();
 }
 
-const GpuComputeCapability &DeviceDescription::gpu_compute_capability() const {
+const GpuComputeCapability& DeviceDescription::gpu_compute_capability() const {
   return gpu_compute_capability_;
 }
 
@@ -119,8 +167,8 @@ RocmComputeCapability DeviceDescription::rocm_compute_capability() const {
   return RocmComputeCapability{};
 }
 
-bool ThreadDimOk(const DeviceDescription &device_description,
-                 const ThreadDim &thread_dim) {
+bool ThreadDimOk(const DeviceDescription& device_description,
+                 const ThreadDim& thread_dim) {
   const int64_t total_threads = thread_dim.x * thread_dim.y * thread_dim.z;
   const int64_t threads_per_block_limit =
       device_description.threads_per_block_limit();
@@ -130,7 +178,7 @@ bool ThreadDimOk(const DeviceDescription &device_description,
     return false;
   }
 
-  const auto &limit = device_description.thread_dim_limit();
+  const auto& limit = device_description.thread_dim_limit();
   bool ok = thread_dim.x <= limit.x && thread_dim.y <= limit.y &&
             thread_dim.z <= limit.z;
   if (!ok) {
@@ -140,9 +188,9 @@ bool ThreadDimOk(const DeviceDescription &device_description,
   return ok;
 }
 
-void CalculateDimensionality(const DeviceDescription &device_description,
-                             int64_t element_count, int64_t *threads_per_block,
-                             int64_t *block_count) {
+void CalculateDimensionality(const DeviceDescription& device_description,
+                             int64_t element_count, int64_t* threads_per_block,
+                             int64_t* block_count) {
   *threads_per_block = device_description.threads_per_block_limit();
   *block_count = tsl::MathUtil::CeilOfRatio(element_count, *threads_per_block);
   if (*block_count == 1) {

xla/stream_executor/device_description.h
@@ -22,18 +22,84 @@ limitations under the License.
 
 #include <cassert>
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <utility>
 #include <variant>
 
+#include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
+#include "absl/types/span.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.pb.h"
 #include "xla/stream_executor/launch_dim.h"
 #include "xla/stream_executor/rocm/rocm_compute_capability.h"
 #include "xla/stream_executor/semantic_version.h"
+#include "xla/xla_data.pb.h"
 
 namespace stream_executor {
+
+// Describes the capabilities and performance characteristics of a specific
+// execution unit within a device, such as scalar units (e.g., CUDA Cores) or
+// matrix units (e.g., Tensor Cores).
+class ExecutionUnitDescription {
+ public:
+  // Information about operations on a particular datatype.
+  struct RateInfo {
+    int32_t units_per_core = 0;
+    float clock_rate_ghz = 0.;
+    // Note: Here FMA is counted as 1 operation. Models that count FMA as 2 ops
+    // need to multiply this number by 2 in their calculations.
+    int32_t fused_ops_per_clock = 0;
+
+    bool operator==(const RateInfo& rhs) const {
+      return units_per_core == rhs.units_per_core &&
+             clock_rate_ghz == rhs.clock_rate_ghz &&
+             fused_ops_per_clock == rhs.fused_ops_per_clock;
+    }
+
+    bool operator!=(const RateInfo& rhs) const { return !(*this == rhs); }
+  };
+
+  // Sets or overwrites the `RateInfo` for a specific `dtype`.
+  void SetRateInfo(xla::PrimitiveType dtype, RateInfo rate_info) {
+    rate_infos_[dtype] = rate_info;
+  }
+
+  // Sets or overwrites the `RateInfo` for multiple `dtypes`.
+  void SetRateInfo(absl::Span<const xla::PrimitiveType> dtypes,
+                   RateInfo rate_info) {
+    for (xla::PrimitiveType dtype : dtypes) {
+      SetRateInfo(dtype, rate_info);
+    }
+  }
+
+  // Returns the `RateInfo` for a specific `dtype`, or `std::nullopt` if
+  // no information is available for the given dtype.
+  std::optional<RateInfo> GetRateInfo(xla::PrimitiveType dtype) const {
+    auto it = rate_infos_.find(dtype);
+    if (it == rate_infos_.end()) {
+      return std::nullopt;
+    }
+    return it->second;
+  }
+
+  ExecutionUnitDescriptionProto ToProto() const;
+
+  static absl::StatusOr<ExecutionUnitDescription> FromProto(
+      const ExecutionUnitDescriptionProto& proto);
+
+  bool operator==(const ExecutionUnitDescription& other) const {
+    return rate_infos_ == other.rate_infos_;
+  }
+
+  bool operator!=(const ExecutionUnitDescription& other) const {
+    return !(*this == other);
+  }
+
+ private:
+  absl::flat_hash_map<xla::PrimitiveType, RateInfo> rate_infos_;
+};
 
 class GpuComputeCapability {
  public:
@@ -167,6 +233,26 @@ class DeviceDescription {
   // Number of floating point operations one core (SM, compute unit) can execute
   // in parallel. Corresponds to the number of "CUDA cores" for NVIDIA devices.
   int fpus_per_core() const { return fpus_per_core_; }
+
+  // Returns a pointer to the description of the scalar execution unit, or
+  // nullptr if not available.
+  // These units are typically referred to as "CUDA Cores" or "FP32/FP64/INT32
+  // Cores" on NVIDIA and "Stream Processors" on AMD devices.
+  const ExecutionUnitDescription* scalar_unit_description() const {
+    return scalar_unit_description_.has_value()
+               ? &scalar_unit_description_.value()
+               : nullptr;
+  }
+
+  // Returns a pointer to the description of the matrix execution unit, or
+  // nullptr if not available.
+  // These units are known as "Tensor Cores" on NVIDIA and "Matrix Cores" on
+  // AMD.
+  const ExecutionUnitDescription* matrix_unit_description() const {
+    return matrix_unit_description_.has_value()
+               ? &matrix_unit_description_.value()
+               : nullptr;
+  }
 
   // Returns the limit on the thread dimensionality values in each of the
   // respective dimensions. These limits affect what constitutes a legitimate
@@ -402,11 +488,17 @@ class DeviceDescription {
     interconnect_info_ = std::move(info);
   }
 
+  void set_scalar_unit_description(ExecutionUnitDescription descr) {
+    scalar_unit_description_ = std::move(descr);
+  }
+
+  void set_matrix_unit_description(ExecutionUnitDescription descr) {
+    matrix_unit_description_ = std::move(descr);
+  }
+
  private:
   // For description of the following members, see the corresponding accessor
   // above.
-  //
-  // N.B. If another field is added, update ToMap() above.
   std::string device_vendor_ = kUndefinedString;
   std::string platform_version_ = kUndefinedString;
   std::string pci_bus_id_ = kUndefinedString;
@@ -449,6 +541,9 @@ class DeviceDescription {
   int core_count_ = kUninitialized<int>;
   int fpus_per_core_ = kUninitialized<int>;
   bool ecc_enabled_ = false;
+
+  std::optional<ExecutionUnitDescription> scalar_unit_description_;
+  std::optional<ExecutionUnitDescription> matrix_unit_description_;
 
   SemanticVersion driver_version_{0, 0, 0};
   SemanticVersion runtime_version_{0, 0, 0};