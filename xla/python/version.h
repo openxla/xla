585eb0ded65529b94f480cbbf3f2813b96ff5742
Hyeontaek Lim <hyeontaek@google.com>
2025-12-12 17:11:12 -0800

    [PjRt-IFRT] Create `ifrt::PjRtExecutable` only from `ifrt::PjRtCompiler` and `CompileOnlyIfrtCompiler`
    This change migrates direct calls to `ifrt::PjRtExecutable::Create()` outside to use a public IFRT API `ifrt::PjRtCompiler::Compile()` instead.
    
    This change should be no-op in practice. For PjRt-IFRT, it now performs IFRT device ID -> PjRt device ID conversion in `xla::CompileOptions::executable_build_options` (which was missing before) and thus can handle a client using a different device ID mapping.
    
    PiperOrigin-RevId: 843885753

--- xla/python/version.h
+++ xla/python/version.h
@@ -18,6 +18,7 @@ limitations under the License.
 
 // An increasing version number to protect jax code against breaking changes.
 // In JAX, reference this via jax._src.lib.ifrt_version.
-#define JAX_IFRT_VERSION_NUMBER 40  // Shardy sharded -> unreduced
+#define JAX_IFRT_VERSION_NUMBER \
+  41  // PjRtExecutable is created using IFRT Compiler::Compile() API.
 
 #endif  // XLA_PYTHON_VERSION_H_
