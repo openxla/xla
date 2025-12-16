910f7454e4130e7220794ef3ea107cec5d7a14cb
Eetu Sjöblom <eetu.sjoblom@amd.com>
2025-12-16 13:49:17 +0200

    Make cublaslt_test platform independent (#489)

--- xla/backends/gpu/autotuner/cublaslt_test.cc
+++ xla/backends/gpu/autotuner/cublaslt_test.cc
@@ -29,10 +29,10 @@ limitations under the License.
 #include "xla/hlo/testlib/filecheck.h"
 #include "xla/hlo/testlib/hlo_hardware_independent_test_base.h"
 #include "xla/service/compiler.h"
-#include "xla/service/gpu/nvptx_compiler.h"
 #include "xla/service/platform_util.h"
 #include "xla/stream_executor/blas.h"
 #include "xla/stream_executor/device_description.pb.h"
+#include "xla/stream_executor/platform.h"
 #include "xla/stream_executor/stream_executor.h"
 #include "xla/tsl/lib/core/status_test_util.h"
 #include "xla/tsl/platform/status_matchers.h"
@@ -105,18 +105,18 @@ const char kUnsupportedHlo[] = R"(
 class CublasLtBackendTest : public HloHardwareIndependentTestBase {
  protected:
   DebugOptions debug_options_;
-  NVPTXCompiler compiler_;
+  se::Platform* platform_;
   se::StreamExecutor* stream_executor_;
+  std::unique_ptr<Compiler> compiler_;
   Compiler::TargetConfig target_config_;
   CublasLtBackend backend_;
 
   CublasLtBackendTest()
-      : stream_executor_(PlatformUtil::GetDefaultPlatform()
-                             .value()
-                             ->ExecutorForDevice(0)
-                             .value()),
+      : platform_(PlatformUtil::GetDefaultPlatform().value()),
+        stream_executor_(platform_->ExecutorForDevice(0).value()),
+        compiler_(Compiler::GetForPlatform(platform_).value()),
         target_config_(stream_executor_),
-        backend_(stream_executor_, &debug_options_, &compiler_,
+        backend_(stream_executor_, &debug_options_, compiler_.get(),
                  &target_config_) {}
 
   CublasLtBackendConfig ExpectedDefaultAlgorithm() {
