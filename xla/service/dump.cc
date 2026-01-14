phanipaladugula/tensorflow

Use SplitProto format to dump GpuExecutables
GpuExecutable will now use the new SplitProto format to dump itself. This allows us to dump executables bigger than 2GiB.

PiperOrigin-RevId: 855252284

third_party/xla/xla/service/dump.cc
@@ -46,7 +46,10 @@ limitations under the License.
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/Transforms/LocationSnapshot.h"
 #include "google/protobuf/descriptor.h"
+#include "google/protobuf/io/zero_copy_stream_impl_lite.h"
 #include "google/protobuf/text_format.h"
+#include "riegeli/bytes/file_writer.h"
+#include "riegeli/bytes/writer.h"
 #include "xla/debug_options_flags.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
@@ -1237,6 +1240,24 @@ absl::Status DumpProtoToDirectory(const tsl::protobuf::Message& message,
   }
   *full_path = tsl::io::JoinPath(directory, safe_file_name);
   return tsl::WriteBinaryProto(env, *full_path, message);
+}
+
+absl::StatusOr<std::unique_ptr<riegeli::Writer>>
+CreatePerModuleRiegeliDumpWriter(const HloModule& module,
+                                 const DebugOptions& debug_options,
+                                 absl::string_view filename) {
+  return CreateRiegeliDumpWriter(
+      debug_options, FilenameFor(module, TimestampFor(module), filename));
+}
+
+absl::StatusOr<std::unique_ptr<riegeli::Writer>> CreateRiegeliDumpWriter(
+    const DebugOptions& debug_options, absl::string_view filename) {
+  CanonicalDebugOptions opts(debug_options);
+  std::optional<std::string> file_path = GetDumpFilePath(filename, opts);
+  if (!file_path.has_value()) {
+    return absl::InvalidArgumentError("Failed to get dump file path");
+  }
+  return std::make_unique<riegeli::FileWriter<>>(*file_path);
 }
 
 }  // namespace xla

third_party/xla/xla/service/gpu/gpu_executable_test.cc
@@ -29,6 +29,8 @@ limitations under the License.
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "riegeli/bytes/file_reader.h"
+#include "riegeli/bytes/string_reader.h"
 #include "xla/backends/gpu/runtime/copy_thunk.h"
 #include "xla/backends/gpu/runtime/custom_kernel_thunk.h"
 #include "xla/backends/gpu/runtime/kernel_thunk.h"
@@ -66,6 +68,7 @@ limitations under the License.
 #include "xla/tsl/testing/temporary_directory.h"
 #include "xla/tsl/util/proto/parse_text_proto.h"
 #include "xla/tsl/util/proto/proto_matchers.h"
+#include "xla/util/split_proto/split_proto_reader.h"
 #include "tsl/platform/path.h"
 
 namespace xla::gpu {
@@ -557,20 +560,23 @@ TEST(GpuExecutableTest, GpuExecutableDump) {
 
   std::vector<std::string> dump_files;
   TF_ASSERT_OK(env->GetMatchingPaths(
-      tsl::io::JoinPath(debug_options.xla_dump_to(), "*gpu_executable.txt"),
+      tsl::io::JoinPath(debug_options.xla_dump_to(), "*gpu_executable.riegeli"),
       &dump_files));
   ASSERT_EQ(dump_files.size(), 1);
 
   ExecutableAndOptionsProto dump_content;
-  TF_ASSERT_OK(tsl::ReadTextProto(env, dump_files[0], &dump_content));
+  auto reader = std::make_unique<riegeli::FileReader<>>(dump_files[0]);
+  TF_ASSERT_OK(ReadSplitProto(std::move(reader), dump_content));
   EXPECT_THAT(dump_content.compile_options().executable_build_options(),
               Partially(EqualsProto(R"pb(
                 num_replicas: 2 num_partitions: 1
               )pb")));
 
   GpuExecutableProto gpu_executable_proto;
-  ASSERT_TRUE(gpu_executable_proto.ParseFromString(
-      dump_content.serialized_executable()));
+  auto executable_reader = std::make_unique<riegeli::StringReader<>>(
+      dump_content.serialized_executable());
+  TF_ASSERT_OK(
+      ReadSplitProto(std::move(executable_reader), gpu_executable_proto));
   ASSERT_THAT(gpu_executable_proto, Partially(EqualsProto(R"pb(
                 module_name: "test_module"
                 thunk {

openxla/xla

xla/service/dump.cc
@@ -47,6 +47,8 @@ limitations under the License.
 #include "mlir/Transforms/LocationSnapshot.h"
 #include "google/protobuf/descriptor.h"
 #include "google/protobuf/text_format.h"
+#include "riegeli/bytes/cfile_writer.h"
+#include "riegeli/bytes/writer.h"
 #include "xla/debug_options_flags.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
@@ -1237,6 +1239,24 @@ absl::Status DumpProtoToDirectory(const tsl::protobuf::Message& message,
   }
   *full_path = tsl::io::JoinPath(directory, safe_file_name);
   return tsl::WriteBinaryProto(env, *full_path, message);
+}
+
+absl::StatusOr<std::unique_ptr<riegeli::Writer>>
+CreatePerModuleRiegeliDumpWriter(const HloModule& module,
+                                 const DebugOptions& debug_options,
+                                 absl::string_view filename) {
+  return CreateRiegeliDumpWriter(
+      debug_options, FilenameFor(module, TimestampFor(module), filename));
+}
+
+absl::StatusOr<std::unique_ptr<riegeli::Writer>> CreateRiegeliDumpWriter(
+    const DebugOptions& debug_options, absl::string_view filename) {
+  CanonicalDebugOptions opts(debug_options);
+  std::optional<std::string> file_path = GetDumpFilePath(filename, opts);
+  if (!file_path.has_value()) {
+    return absl::InvalidArgumentError("Failed to get dump file path");
+  }
+  return std::make_unique<riegeli::CFileWriter<>>(*file_path);
 }
 
 }  // namespace xla

xla/service/gpu/gpu_executable_test.cc
@@ -29,6 +29,8 @@ limitations under the License.
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "riegeli/bytes/cfile_reader.h"
+#include "riegeli/bytes/string_reader.h"
 #include "xla/backends/gpu/runtime/copy_thunk.h"
 #include "xla/backends/gpu/runtime/custom_kernel_thunk.h"
 #include "xla/backends/gpu/runtime/kernel_thunk.h"
@@ -66,6 +68,7 @@ limitations under the License.
 #include "xla/tsl/testing/temporary_directory.h"
 #include "xla/tsl/util/proto/parse_text_proto.h"
 #include "xla/tsl/util/proto/proto_matchers.h"
+#include "xla/util/split_proto/split_proto_reader.h"
 #include "tsl/platform/path.h"
 
 namespace xla::gpu {
@@ -557,20 +560,23 @@ TEST(GpuExecutableTest, GpuExecutableDump) {
 
   std::vector<std::string> dump_files;
   TF_ASSERT_OK(env->GetMatchingPaths(
-      tsl::io::JoinPath(debug_options.xla_dump_to(), "*gpu_executable.txt"),
+      tsl::io::JoinPath(debug_options.xla_dump_to(), "*gpu_executable.riegeli"),
       &dump_files));
   ASSERT_EQ(dump_files.size(), 1);
 
   ExecutableAndOptionsProto dump_content;
-  TF_ASSERT_OK(tsl::ReadTextProto(env, dump_files[0], &dump_content));
+  auto reader = std::make_unique<riegeli::CFileReader<>>(dump_files[0]);
+  TF_ASSERT_OK(ReadSplitProto(std::move(reader), dump_content));
   EXPECT_THAT(dump_content.compile_options().executable_build_options(),
               Partially(EqualsProto(R"pb(
                 num_replicas: 2 num_partitions: 1
               )pb")));
 
   GpuExecutableProto gpu_executable_proto;
-  ASSERT_TRUE(gpu_executable_proto.ParseFromString(
-      dump_content.serialized_executable()));
+  auto executable_reader = std::make_unique<riegeli::StringReader<>>(
+      dump_content.serialized_executable());
+  TF_ASSERT_OK(
+      ReadSplitProto(std::move(executable_reader), gpu_executable_proto));
   ASSERT_THAT(gpu_executable_proto, Partially(EqualsProto(R"pb(
                 module_name: "test_module"
                 thunk {