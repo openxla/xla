d690bb5ce7428e751f3da98c00c6fa960a9b1d0b
Will Froom <willfroom@google.com>
2026-01-15 02:47:54 -0800

    [XLA:CPU] Enable Multi-Output Fusion by default.
    PiperOrigin-RevId: 856574977

--- xla/service/cpu/cpu_compiler.cc
+++ xla/service/cpu/cpu_compiler.cc
@@ -1011,11 +1011,7 @@ absl::Status CpuCompiler::RunHloPassesAfterLayoutAssn(
   }
 
   AliasInfo alias_info;
-  bool use_multi_output_fusion =
-      options::UseMultiOutputFusion(module->config());
-  pipeline.AddPass<CpuInstructionFusion>(
-      &alias_info,
-      /*may_duplicate=*/!use_multi_output_fusion);
+  pipeline.AddPass<CpuInstructionFusion>(&alias_info);
 
   if (is_fusion_emitters) {
     bool use_experimental_loop_fusion =
@@ -1025,6 +1021,8 @@ absl::Status CpuCompiler::RunHloPassesAfterLayoutAssn(
                                     use_tiled_emitter);
   }
 
+  bool use_multi_output_fusion =
+      options::UseMultiOutputFusion(module->config());
   if (use_multi_output_fusion) {
     pipeline.AddPass<CpuMultiOutputFusion>(&alias_info);
     pipeline.AddPass<TupleSimplifier>();
