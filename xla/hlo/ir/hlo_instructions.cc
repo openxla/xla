1000a85ef0cc790cbbecafb7556f8527e027d446
Shanbin Ke <ske@nvidia.com>
2026-01-20 00:33:51 -0800

    PR #34034: [XLA:GPU] Add conv fusion rewriter
    Imported from GitHub PR https://github.com/openxla/xla/pull/34034
    
    📝 Summary of Changes
    *	Added fusion model to grow fusion staring from operands and users. It enables epilogue activation fusion/integer conv fusion/ fp8 conv fusion/multi-output conv fusion for fp8 amax.
    *	Added float normalization support to skip adding type conversion for conv fusion.
    *	Added layout assignment for conv fusion to enforce NHWC layout.
    *	Added window(stride/padding/dilation) construction for backward conv because cuDNN requires backward conv to use same window as forward convolution.
    *	Added layout normalization to normalize layout to have descending layout.
    *	Reused cudnn_fused_conv_rewriter_test.cc in conv_fusion_rewriter_test.cc to test new conv fusion, need to rewrite fp8 patterns because new cudnn frontend doesn’t allow prologue fusion on weight.
    
    🎯 Justification
    XLA cuDNN conv refactoring to express conv as cuDNN fusion instead of custom call.
    
    🚀 Kind of Contribution
    ✨ New Feature
    
    📊 Benchmark (for Performance Improvements)
    No major speed up is expected as this is refactoring of cuDNN conv. It does enable more fusions with conv so there might be some speed up/regression.
    
    🧪 Unit Tests:
    reused unit tests from cudnn_fused_conv_rewriter_test.cc in conv_fusion_rewriter_test.cc
    
    🧪 Execution Tests:
    reused unit tests from cudnn_fused_conv_rewriter_test.cc in conv_fusion_rewriter_test.cc
    
    Copybara import of the project:
    
    --
    242500faaa8366fc94d3cd485b22349e7824244c by Cjkkkk <ske@nvidia.com>:
    
    add conv fusion rewriter
    
    --
    0e2acec5486379b9ff6f19ea62e2e7ca7ed33e08 by Cjkkkk <ske@nvidia.com>:
    
    address comments
    
    --
    8e389ade5984615a92945fce44568f417fb26f3f by Cjkkkk <ske@nvidia.com>:
    
    move conv fusion after layout assignment
    
    --
    1cc69650728b61423c0a69f7d5080ea61c1466c6 by Cjkkkk <ske@nvidia.com>:
    
    fix clang format&assign new field number for debug option
    
    --
    fa97b4217e5f33113034b6d6417bf91e174d5738 by Cjkkkk <ske@nvidia.com>:
    
    address comments
    
    --
    d7d6270d9889e42a76296560e3e5b31d2e914ff7 by Cjkkkk <ske@nvidia.com>:
    
    add unit tests
    
    --
    36e66bb2815e50a78ddd7ad2ceff4a810fd77bee by Cjkkkk <ske@nvidia.com>:
    
    undo unrelated changes
    
    Merging this change closes #34034
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/34034 from Cjkkkk:conv_fusion_rewriter 36e66bb2815e50a78ddd7ad2ceff4a810fd77bee
    PiperOrigin-RevId: 858455103

--- xla/hlo/ir/hlo_instructions.cc
+++ xla/hlo/ir/hlo_instructions.cc
@@ -3118,6 +3118,12 @@ void HloConvolutionInstruction::PrintExtraAttributesImpl(
       AppendCat(printer, "batch_group_count=", batch_group_count_);
     });
   }
+
+  if (conv_kind_ != ConvKind::UNSET) {
+    printer.Next([this](Printer* printer) {
+      AppendCat(printer, "conv_kind=", conv_kind_);
+    });
+  }
   PrintPrecisionConfig(printer, precision_config_);
 }
 
@@ -3147,10 +3153,13 @@ HloConvolutionInstruction::CloneWithNewOperandsImpl(
     const Shape& shape, absl::Span<HloInstruction* const> new_operands,
     HloCloneContext* context) const {
   CHECK_EQ(new_operands.size(), 2);
-  return std::make_unique<HloConvolutionInstruction>(
-      shape, new_operands[0], new_operands[1], feature_group_count_,
-      batch_group_count_, window(), convolution_dimension_numbers_,
-      precision_config_);
+  std::unique_ptr<HloConvolutionInstruction> clone =
+      std::make_unique<HloConvolutionInstruction>(
+          shape, new_operands[0], new_operands[1], feature_group_count_,
+          batch_group_count_, window(), convolution_dimension_numbers_,
+          precision_config_);
+  clone->set_conv_kind(conv_kind_);
+  return clone;
 }
 
 HloReduceWindowInstruction::HloReduceWindowInstruction(
