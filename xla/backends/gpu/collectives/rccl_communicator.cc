55c879338fb3b8311071e3dc8b411486278f6818
TJ Xu <tjx@nvidia.com>
2026-01-20 18:45:12 +0000

    integrate nccl put api to collective permute

--- xla/backends/gpu/collectives/rccl_communicator.cc
+++ xla/backends/gpu/collectives/rccl_communicator.cc
@@ -162,6 +162,8 @@ class RcclCommunicator::RcclRegisteredBufferHandle
         symmetric_handle_(symmetric_handle),
         device_ordinal_(device_ordinal) {}
 
+  void* GetHandle() const override { return handle_; }
+
   ~RcclRegisteredBufferHandle() override {
     if (auto status = Unregister(); !status.ok()) {
       LOG(ERROR) << status.message();
