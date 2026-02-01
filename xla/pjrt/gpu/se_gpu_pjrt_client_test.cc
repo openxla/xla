3480c44572cf652c4e724cba2d08324a2b0645c1
xla authors <google-ml-automation@google.com>
2026-02-01 13:28:23 -0800

    Automated Code Change
    PiperOrigin-RevId: 864037869

--- xla/pjrt/gpu/se_gpu_pjrt_client_test.cc
+++ xla/pjrt/gpu/se_gpu_pjrt_client_test.cc
@@ -2871,7 +2871,7 @@ TEST(StreamExecutorGpuClientTest, EventCaching) {
   TF_ASSERT_OK_AND_ASSIGN(auto client,
                           GetStreamExecutorGpuClient(DefaultOptions()));
   auto* async_work_runner =
-      tensorflow::down_cast<PjRtStreamExecutorClient*>(client.get())
+      absl::down_cast<PjRtStreamExecutorClient*>(client.get())
           ->async_work_runner();
   const auto& device = client->addressable_devices()[0];
   LocalDeviceState* local_device_state =
