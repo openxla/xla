1afc7f8efb31be36ead8248eff13f6c22d525e23
Niklas Vangerow <nikv@google.com>
2026-02-05 15:24:10 -0800

    Add HloLegacyGpuTestBase wrapper around HloTestBase.
    We want to avoid linking HloRunner in the same binary as HloRunnerPjRt.
    
    This CL keeps the "legacy" features of HloTestBase without impacting HloTestBase
    itself. When the time to delete HloTestBase comes we will hard fork HloTestBase
    into HloLegacyGpuTestBase.
    
    PiperOrigin-RevId: 866137973

--- xla/service/gpu/tests/hlo_pjrt_gpu_test_base.h
+++ xla/service/gpu/tests/hlo_pjrt_gpu_test_base.h
@@ -21,25 +21,28 @@ limitations under the License.
 #include "xla/backends/gpu/target_config/target_config.h"
 #include "xla/pjrt/pjrt_client.h"
 #include "xla/service/compiler.h"
+#include "xla/service/gpu/tests/hlo_gpu_test_base_interface.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/tests/hlo_pjrt_test_base.h"
 #include "xla/tests/hlo_runner_agnostic_test_base.h"
 
 namespace xla::gpu {
 
-class HloPjRtGpuTestBase : public HloRunnerAgnosticTestBase {
+class HloPjRtGpuTestBase : public HloRunnerAgnosticTestBase,
+                           public HloGpuTestBaseInterface {
  protected:
   explicit HloPjRtGpuTestBase(HloPjRtTestBaseOptions options = {});
 
-  const GpuTargetConfig& gpu_target_config() const {
+  const GpuTargetConfig& gpu_target_config() const override {
     return gpu_target_config_;
   }
 
-  const stream_executor::DeviceDescription& device_description() const {
+  const stream_executor::DeviceDescription& device_description()
+      const override {
     return gpu_target_config_.device_description;
   }
 
-  Compiler* compiler() const { return compiler_.get(); }
+  Compiler* compiler() const override { return compiler_.get(); }
 
  private:
   HloPjRtGpuTestBase(PjRtClient* client, HloPjRtTestBaseOptions options);
