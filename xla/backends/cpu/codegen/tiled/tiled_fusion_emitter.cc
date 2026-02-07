250d5568b0cb3be5696362f7d2aeb3432dc3bc4a
xla authors <google-ml-automation@google.com>
2026-02-06 23:01:31 -0800

    Automated Code Change
    PiperOrigin-RevId: 866767771

--- xla/backends/cpu/codegen/tiled/tiled_fusion_emitter.cc
+++ xla/backends/cpu/codegen/tiled/tiled_fusion_emitter.cc
@@ -23,6 +23,7 @@ limitations under the License.
 
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/check.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
