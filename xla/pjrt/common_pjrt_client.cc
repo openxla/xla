6526f945dab70b765060a5af739937b90ce14f84
Junwhan Ahn <junwhan@google.com>
2026-02-09 11:55:20 -0800

    Implement `strict_shape_checking` for `CommonPjRtClient`
    This CL also flips the default value of `strict_shape_checking` to false because there are too many existing tests that fail this check. We can selectively enable the check after the code is submitted.
    
    PiperOrigin-RevId: 867707421

--- xla/pjrt/common_pjrt_client.cc
+++ xla/pjrt/common_pjrt_client.cc
@@ -543,6 +543,24 @@ absl::Status CommonPjRtClient::PrepareArguments(
             i, replica, tfrt_buffer->device()->DebugString(),
             device->DebugString());
       }
+
+      const Shape& expected_shape = parameter_device_shapes[i];
+      const Shape& on_device_shape = tfrt_buffer->on_device_shape();
+
+      if (options.strict_shape_checking &&
+          // Skip shape check for non-array shapes (e.g. tuples).
+          expected_shape.IsArray() && on_device_shape.IsArray() &&
+          // Dynamic shapes cannot be compared directly.
+          expected_shape.is_static() && on_device_shape.is_static() &&
+          !xla::Shape::Equal().IgnoreMemorySpaceInLayout()(expected_shape,
+                                                           on_device_shape)) {
+        return InvalidArgument(
+            "Buffer passed to Execute() as argument %d to replica %d has "
+            "unexpected shape: %s (expected %s).",
+            i, replica, on_device_shape.ToString(/*print_layout=*/true),
+            expected_shape.ToString(/*print_layout=*/true));
+      }
+
       const bool donated_param =
           donate_it != donated_params.end() && *donate_it == i;
       if (donated_param) {
@@ -551,9 +569,8 @@ absl::Status CommonPjRtClient::PrepareArguments(
       const bool donation_denied_at_runtime =
           options.non_donatable_input_indices.contains(i);
       if (donated_param && donation_denied_at_runtime &&
-          tfrt_buffer->on_device_shape().has_layout() &&
-          tfrt_buffer->on_device_shape().layout().memory_space() ==
-              Layout::kHostMemorySpace) {
+          on_device_shape.has_layout() &&
+          on_device_shape.layout().memory_space() == Layout::kHostMemorySpace) {
         return absl::UnimplementedError(
             "pinned_host buffers do not support donation denial at runtime via "
             "`ExecuteOptions::non_donatable_input_indices`");
@@ -590,14 +607,10 @@ absl::Status CommonPjRtClient::PrepareArguments(
       }
       auto* device_buffer = hold.buffer();
 
-      const bool is_handle_dynamic_shape =
-          handle->on_device_shape().is_dynamic();
-
-      const Shape& expected_shape = parameter_device_shapes[i];
       if (device_buffer->raw_buffer()) {
         tsl::RCReference<CommonPjRtRawBuffer> actual_buffer =
             device_buffer->raw_buffer();
-        if (is_handle_dynamic_shape && !expected_shape.is_dynamic()) {
+        if (on_device_shape.is_dynamic() && !expected_shape.is_dynamic()) {
           TF_ASSIGN_OR_RETURN(auto handle_logical_device_shape,
                               handle->logical_on_device_shape());
           auto status_or_buffer =
