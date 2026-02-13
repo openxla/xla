3477f9a8d6a5970644b054ef54428c6e5a09b738
Bixia Zheng <bixia@google.com>
2026-02-13 07:21:25 -0800

    Move convolution operand swap to a stand-alone pass in conv_operand_swapper.cc. Also clone the tests for the transformations to conv_operand_swapper_test.cc.
    We will remove this functionality from AlgebraicSimplifier after we finish integrating the new pass into the XLA compiler pipelines.
    
    PiperOrigin-RevId: 869720736

--- xla/hlo/transforms/simplifiers/algebraic_simplifier.h
+++ xla/hlo/transforms/simplifiers/algebraic_simplifier.h
@@ -91,6 +91,10 @@ class AlgebraicSimplifierOptions {
     conv_is_lowerable_callback_ = std::move(conv_is_lowerable_callback);
   }
 
+  ConvIsLowerableCallback conv_is_lowerable_callback() const {
+    return conv_is_lowerable_callback_;
+  }
+
   // If is_layout_sensitive is true, then the simplifier preserves layout during
   // transformation. Otherwise, layout is ignored.
   void set_is_layout_sensitive(bool is_layout_sensitive) {
