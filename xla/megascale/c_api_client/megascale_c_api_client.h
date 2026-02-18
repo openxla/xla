51cd3675c13d737f5c37768b8ba3ecbd34ad03c0
Haibo Huang <hhb@google.com>
2026-02-18 11:45:08 -0800

    Move megascale c api types to a different file
    PiperOrigin-RevId: 871972630

--- xla/megascale/c_api_client/megascale_c_api_client.h
+++ xla/megascale/c_api_client/megascale_c_api_client.h
@@ -18,16 +18,12 @@ limitations under the License.
 
 #include <cstdint>
 #include <memory>
-#include <string>
 
-#include "absl/container/flat_hash_map.h"
 #include "absl/log/log.h"
 #include "absl/status/statusor.h"
-#include "absl/time/time.h"
 #include "xla/megascale/addresses.pb.h"
+#include "xla/megascale/c_api_client/megascale_types.h"
 #include "xla/megascale/dcn_topology.pb.h"
-#include "xla/pjrt/c/pjrt_c_api.h"
-#include "xla/pjrt/c/pjrt_c_api_megascale_extension.h"
 #include "xla/pjrt/pjrt_compiler.h"
 #include "xla/pjrt/pjrt_executable.h"
 #include "xla/tsl/platform/logging.h"
@@ -35,52 +31,6 @@ limitations under the License.
 namespace xla {
 namespace megascale {
 namespace c_api_client {
-
-class CApiPjRtClientContext {
- public:
-  CApiPjRtClientContext(PJRT_Megascale_ClientContext* client_context,
-                        const PJRT_Api* c_api,
-                        PJRT_Megascale_Extension* extension)
-      : client_context_(client_context),
-        c_api_(c_api),
-        extension_(CHECK_NOTNULL(extension)) {}
-
-  ~CApiPjRtClientContext();
-
-  PJRT_Megascale_ClientContext* get() const { return client_context_; }
-
-  absl::Status Initialize();
-
-  absl::Status UnblockPendingWork(int32_t launch_id,
-                                  absl::Duration expire_after);
-
-  absl::StatusOr<int> megascale_port();
-
- private:
-  PJRT_Megascale_ClientContext* client_context_;
-  const PJRT_Api* c_api_;
-  const PJRT_Megascale_Extension* extension_;
-};
-
-class PjRtCApiMultiSliceConfig : public xla::MultiSliceConfig {
- public:
-  PjRtCApiMultiSliceConfig(PJRT_Megascale_MultiSliceConfig* config,
-                           const PJRT_Api* c_api,
-                           PJRT_Megascale_Extension* extension)
-      : config_(config), c_api_(c_api), extension_(CHECK_NOTNULL(extension)) {}
-
-  ~PjRtCApiMultiSliceConfig() override;
-
-  int32_t NumSlices() const override;
-  int32_t SliceId() const override;
-  absl::flat_hash_map<int32_t, int32_t> NumDevicesPerSlice() const override;
-  std::string Serialize() const override;
-
- private:
-  PJRT_Megascale_MultiSliceConfig* config_;
-  const PJRT_Api* c_api_;
-  const PJRT_Megascale_Extension* extension_;
-};
 
 // Returns AoT config for megascale multi slice compilation.
 // REQUIRES: num_slices > 1.
