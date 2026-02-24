91a0b3544812a6d4ea1007c98d1bd4e5b2c3d81e
xla authors <google-ml-automation@google.com>
2026-02-24 02:43:46 -0800

    Reverts 33f947d264a4b471be4b3c1b130b6a92931bda84
    PiperOrigin-RevId: 874500422

--- xla/tsl/profiler/utils/xplane_schema.cc
+++ xla/tsl/profiler/utils/xplane_schema.cc
@@ -400,7 +400,12 @@ const StatTypeMap& GetStatTypeMap() {
        {"cuda_runtime_version", kMetadataCudaRuntimeVersion},
        {"cuda_driver_version", kMetadataCudaDriverVersion},
        // LLO Debug Dump.
-       {"llo_proto", kLloProto}});
+       {"llo_proto", kLloProto},
+       // Power-related stats
+       {"vdd_core_energy_nj", kVddCoreEnergy},
+       {"vdd_core_power_events", kVddCorePowerEvents},
+       {"hbm_energy_nj", kHbmEnergy},
+       {"hbm_power_events", kHbmPowerEvents}});
   DCHECK_EQ(stat_type_map->size(), kNumStatTypes);
   return *stat_type_map;
 }
