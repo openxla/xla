dd091c7296da60820aca094fb161eae0cde9daf8
leo-amd <drleonid@amd.com>
2026-02-26 17:35:22 +0100

    Import rocm_tag_filters from the old CI

--- build_tools/ci/build.py
+++ build_tools/ci/build.py
@@ -294,11 +294,27 @@ def _tag_filters_for_compute_capability(
   return tag_filters
 
 rocm_tag_filters = (
-    "-no_oss",
-    "-requires-gpu-nvidia",
+    "-no_gpu",
     "-requires-gpu-intel",
+    "-requires-gpu-nvidia",
     "-cuda-only",
     "-oneapi-only",
+    "-requires-gpu-sm60",
+    "-requires-gpu-sm60-only",
+    "-requires-gpu-sm70",
+    "-requires-gpu-sm70-only",
+    "-requires-gpu-sm80",
+    "-requires-gpu-sm80-only",
+    "-requires-gpu-sm86",
+    "-requires-gpu-sm86-only",
+    "-requires-gpu-sm89",
+    "-requires-gpu-sm89-only",
+    "-requires-gpu-sm90",
+    "-requires-gpu-sm90-only",
+    "-skip_rocprofiler_sdk",
+    "-no_oss",
+    "-oss_excluded",
+    "-oss_serial",
 )
 
 rocm_test_filters = ""
