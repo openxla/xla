2df8c4b67758212fcda097022659df59c2a32281
Haibo Huang <hhb@google.com>
2026-02-25 22:02:48 -0800

    Move MultiSliceConfig related functions to a new extension.
    PiperOrigin-RevId: 875512981

--- xla/pjrt/c/pjrt_c_api.h
+++ xla/pjrt/c/pjrt_c_api.h
@@ -75,6 +75,7 @@ typedef enum {
   PJRT_Extension_Type_Shardings,
   PJRT_Extension_Type_AbiVersion,
   PJRT_Extension_Type_Collectives,
+  PJRT_Extension_Type_MultiSlice,
 } PJRT_Extension_Type;
 
 // PJRT_Extension_Base contains a type and a pointer to next
