88dfac847f3087ac1c08463f52c60ec2f19b27c7
xla authors <google-ml-automation@google.com>
2026-03-08 05:51:31 -0700

    Remove legacy autotuning utilities.
    PiperOrigin-RevId: 880421152

--- xla/service/gpu/gpu_compiler.cc
+++ xla/service/gpu/gpu_compiler.cc
@@ -1761,11 +1761,6 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
           AlgebraicSimplifierMode::kPostLayoutAssignment,
           hlo_module->config().debug_options(),
           gpu_target_config.platform_name == "ROCM");
-  DeviceOrDevicelessConfig device_config =
-      GetDeviceConfig(stream_exec, options, gpu_target_config);
-  ASSIGN_OR_RETURN(
-      AutotuneConfig autotune_config,
-      AutotuneConfig::FromDebugOptions(device_config, debug_options));
   // Lambdas and related constants:
   const GpuFloatSupport bf16_support(gpu_version, BF16);
   const GpuFloatSupport f8e5m2_support(gpu_version, F8E5M2, F16);
@@ -1921,8 +1916,8 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
   add_float_normalization(pipeline);
 
   TF_RETURN_IF_ERROR(AddConvAndGemmAutotuningPass(
-      &pipeline, hlo_module, gpu_version, options, autotune_config, thread_pool,
-      stream_exec, &gpu_target_config, options.key_value_store,
+      &pipeline, hlo_module, gpu_version, options, thread_pool, stream_exec,
+      &gpu_target_config, options.key_value_store,
       gpu_target_config.device_description.runtime_version(), alias_info,
       debug_options, &mlir_context_, ShapeSizeBytesFunction()));
 
@@ -2114,10 +2109,6 @@ absl::StatusOr<std::unique_ptr<HloModule>> GpuCompiler::RunHloPasses(
   DumpHloModuleMetadataIfEnabled(module.get());
 
   AutotuneResults autotune_results;
-  DeviceOrDevicelessConfig device_config =
-      GetDeviceConfig(stream_exec, options, gpu_target_config);
-  ASSIGN_OR_RETURN(AutotuneConfig autotune_config,
-                   AutotuneConfig::FromDebugOptions(device_config, debug_opts));
   if (!is_deviceless) {
     RETURN_IF_ERROR(AutotunerUtil::SerializeAutotuneResults(&autotune_results));
     RETURN_IF_ERROR(SerializeAutotuneResultsToFile(debug_opts));
@@ -3239,8 +3230,7 @@ GpuCompiler::LoadExecutableFromAotResult(
 absl::Status GpuCompiler::AddConvAndGemmAutotuningPass(
     HloPassPipeline* pipeline, HloModule* hlo_module,
     const se::GpuComputeCapability& gpu_version, const CompileOptions& options,
-    AutotuneConfig& autotune_config, tsl::thread::ThreadPool* thread_pool,
-    se::StreamExecutor* stream_exec,
+    tsl::thread::ThreadPool* thread_pool, se::StreamExecutor* stream_exec,
     const Compiler::GpuTargetConfig* target_config,
     const MultiProcessKeyValueStore& key_value_store,
     const se::SemanticVersion& toolkit_version, const AliasInfo* alias_info,
