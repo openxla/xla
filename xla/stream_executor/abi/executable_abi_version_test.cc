openxla/xla

commit 5150bfd950537354a9517654df71efbf91fd876b

Add ExecutableAbiVersion to CompiledModule and Executable
This is making the GpuCompiler generate an instance of
ExecutableAbiVersion during compilation and stores it in the
compilation result (Either `CompiledModule` or `Executable`).

PiperOrigin-RevId: 879589363

xla/service/BUILD
@@ -1678,6 +1678,7 @@ cc_library(
         "//xla/stream_executor:kernel_stats",
         "//xla/stream_executor:platform",
         "//xla/stream_executor:stream",
+        "//xla/stream_executor/abi:executable_abi_version",
         "//xla/tsl/platform:errors",
         "//xla/tsl/platform:statusor",
         "@com_google_absl//absl/base:core_headers",
@@ -1719,6 +1720,7 @@ cc_library(
         ":buffer_assignment",
         "//xla/hlo/ir:hlo",
         "//xla/stream_executor:platform",
+        "//xla/stream_executor/abi:executable_abi_version",
         "@com_google_absl//absl/status",
         "@com_google_absl//absl/status:statusor",
     ],

xla/service/gpu/BUILD
@@ -755,6 +755,7 @@ cc_library(
         "//xla/stream_executor:scoped_module_handle",
         "//xla/stream_executor:stream",
         "//xla/stream_executor:stream_executor_h",
+        "//xla/stream_executor/abi:executable_abi_version",
         "//xla/stream_executor/cuda:cuda_compute_capability",
         "//xla/stream_executor/cuda:cuda_platform_id",
         "//xla/stream_executor/rocm:rocm_platform_id",
@@ -801,6 +802,7 @@ tf_proto_library(
         "//xla/backends/gpu/runtime:thunk_proto",
         "//xla/service:hlo_proto",
         "//xla/stream_executor:device_description_proto",
+        "//xla/stream_executor/abi:executable_abi_version_proto",
     ],
 )
 
@@ -834,6 +836,8 @@ xla_cc_test(
         "//xla/stream_executor:device_description",
         "//xla/stream_executor:semantic_version",
         "//xla/stream_executor:stream_executor_h",
+        "//xla/stream_executor/abi:executable_abi_version",
+        "//xla/stream_executor/abi:executable_abi_version_proto_cc",
         "//xla/stream_executor/cuda:cuda_compute_capability",
         "//xla/stream_executor/gpu:tma_metadata",
         "//xla/tsl/lib/core:status_test_util",
@@ -1887,6 +1891,7 @@ cc_library(
         "//xla/stream_executor:platform_manager",
         "//xla/stream_executor:semantic_version",
         "//xla/stream_executor:stream_executor_h",
+        "//xla/stream_executor/abi:executable_abi_version",
         "//xla/stream_executor/cuda:cuda_compute_capability",
         "//xla/stream_executor/integrations:device_mem_allocator",
         "//xla/stream_executor/platform:platform_object_registry",
@@ -2117,6 +2122,7 @@ cc_library(
         "//xla/stream_executor:device_description",
         "//xla/stream_executor:kernel_symbol_registry",
         "//xla/stream_executor:platform",
+        "//xla/stream_executor/abi:executable_abi_version",
         "//xla/tsl/platform:errors",
         "//xla/tsl/platform:statusor",
         "//xla/util/split_proto:split_gpu_executable_writer",
@@ -2157,8 +2163,10 @@ xla_cc_test(
         "//xla/stream_executor:mock_platform",
         "//xla/stream_executor:mock_stream_executor",
         "//xla/stream_executor:platform",
+        "//xla/stream_executor:platform_id",
         "//xla/stream_executor:semantic_version",
         "//xla/stream_executor:stream_executor_h",
+        "//xla/stream_executor/abi:executable_abi_version",
         "//xla/stream_executor/cuda:cuda_compute_capability",
         "//xla/stream_executor/gpu:tma_metadata",
         "//xla/tsl/lib/core:status_test_util",

xla/service/compiled_module.h
@@ -23,6 +23,7 @@ limitations under the License.
 #include "absl/status/statusor.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/service/buffer_assignment.h"
+#include "xla/stream_executor/abi/executable_abi_version.h"
 #include "xla/stream_executor/platform.h"
 
 namespace stream_executor {
@@ -56,6 +57,12 @@ class CompiledModule {
   // supports it.
   virtual const HloModule* optimized_module() const = 0;
   virtual std::shared_ptr<HloModule> shared_optimized_module() = 0;
+
+  virtual absl::StatusOr<stream_executor::ExecutableAbiVersion>
+  GetExecutableAbiVersion() const {
+    return absl::UnimplementedError(
+        "ExecutableAbiVersion is not supported by this executable.");
+  }
 };
 
 }  // namespace xla

xla/service/executable.h
@@ -43,6 +43,7 @@ limitations under the License.
 #include "xla/shape.h"
 #include "xla/shape_tree.h"
 #include "xla/shape_util.h"
+#include "xla/stream_executor/abi/executable_abi_version.h"
 #include "xla/stream_executor/device_address_allocator.h"
 #include "xla/stream_executor/kernel_stats.h"
 #include "xla/util.h"
@@ -330,7 +331,6 @@ class Executable {
       const ServiceExecutableRunOptions* run_options,
       std::vector<ExecutionInput> arguments);
 
-
   HloModule& module() const {
     CHECK(hlo_module_ != nullptr);
     return *hlo_module_;
@@ -432,6 +432,12 @@ class Executable {
       const ExecutableBuildOptions& options,
       const DebugOptions& debug_options) const {
     return absl::OkStatus();
+  }
+
+  virtual absl::StatusOr<stream_executor::ExecutableAbiVersion>
+  GetExecutableAbiVersion() const {
+    return absl::UnimplementedError(
+        "ExecutableAbiVersion is not supported by this executable.");
   }
 
  private:

xla/service/gpu/gpu_aot_compilation_result.h
@@ -30,6 +30,7 @@ limitations under the License.
 #include "xla/service/compiled_module.h"
 #include "xla/service/executable.h"
 #include "xla/service/gpu/gpu_executable.pb.h"
+#include "xla/stream_executor/abi/executable_abi_version.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/platform.h"
 
@@ -79,6 +80,12 @@ class GpuAotCompilationResult : public CompiledModule {
 
   std::shared_ptr<HloModule> shared_optimized_module() final {
     return hlo_module_;
+  }
+
+  absl::StatusOr<stream_executor::ExecutableAbiVersion>
+  GetExecutableAbiVersion() const final {
+    return stream_executor::ExecutableAbiVersion::FromProto(
+        GetExecutableProto().executable_abi_version());
   }
 
  private:

xla/service/gpu/gpu_aot_compilation_result_test.cc
@@ -43,6 +43,7 @@ limitations under the License.
 #include "xla/service/gpu/gpu_executable.h"
 #include "xla/service/gpu/launch_dimensions.h"
 #include "xla/service/hlo_module_config.h"
+#include "xla/stream_executor/abi/executable_abi_version.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/gpu/tma_metadata.h"
@@ -51,6 +52,7 @@ limitations under the License.
 #include "xla/stream_executor/mock_platform.h"
 #include "xla/stream_executor/mock_stream_executor.h"
 #include "xla/stream_executor/platform.h"
+#include "xla/stream_executor/platform_id.h"
 #include "xla/stream_executor/semantic_version.h"
 #include "xla/stream_executor/stream_executor.h"
 #include "xla/tsl/lib/core/status_test_util.h"
@@ -80,6 +82,7 @@ DeviceDescription GetDeviceDescription() {
       GpuComputeCapability{::stream_executor::CudaComputeCapability::Volta()});
   device_description.set_driver_version({12, 3, 0});
   device_description.set_runtime_version({12, 3, 0});
+  device_description.set_compile_time_toolkit_version({12, 3, 0});
   return device_description;
 }
 
@@ -139,8 +142,12 @@ class GpuAotCompilationResultTest : public ::testing::Test {
     params.module_name = "test_module";
     params.enable_debug_info_manager = false;
     params.mlir_allocations = {BufferAllocation(0, 1024, 0)};
-    TF_ASSIGN_OR_RETURN(std::unique_ptr<GpuExecutable> executable,
-                        GpuExecutable::Create(std::move(params)));
+    ASSIGN_OR_RETURN(
+        params.executable_abi_version,
+        stream_executor::ExecutableAbiVersion::FromDeviceDescription(
+            device_description_));
+    ASSIGN_OR_RETURN(std::unique_ptr<GpuExecutable> executable,
+                     GpuExecutable::Create(std::move(params)));
     return executable->ToProto();
   }
 
@@ -174,6 +181,7 @@ TEST_F(GpuAotCompilationResultTest, CreateAndSerialize) {
   TF_ASSERT_OK_AND_ASSIGN(
       std::unique_ptr<GpuAotCompilationResult> result,
       GpuAotCompilationResult::FromProto(reference_executable));
+
   TF_ASSERT_OK_AND_ASSIGN(std::string serialized_result,
                           result->SerializeAsString());
 
@@ -192,11 +200,33 @@ TEST_F(GpuAotCompilationResultTest, LoadExecutable) {
       std::unique_ptr<GpuAotCompilationResult> result,
       GpuAotCompilationResult::FromProto(reference_executable));
 
+  {
+    TF_ASSERT_OK_AND_ASSIGN(
+        stream_executor::ExecutableAbiVersion executable_abi_version,
+        result->GetExecutableAbiVersion());
+    EXPECT_EQ(executable_abi_version.platform_name(), "CUDA");
+    EXPECT_EQ(executable_abi_version.proto()
+                  .cuda_platform_version()
+                  .cuda_toolkit_version(),
+              "12.3.0");
+  }
+
   EnsureCudaSymbolIsRegistered();
 
   TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<Executable> executable,
                           std::move(*result).LoadExecutable(
                               platform_.id(), GetDeviceDescription()));
+
+  {
+    TF_ASSERT_OK_AND_ASSIGN(
+        stream_executor::ExecutableAbiVersion executable_abi_version,
+        executable->GetExecutableAbiVersion());
+    EXPECT_EQ(executable_abi_version.platform_name(), "CUDA");
+    EXPECT_EQ(executable_abi_version.proto()
+                  .cuda_platform_version()
+                  .cuda_toolkit_version(),
+              "12.3.0");
+  }
 
   auto* gpu_executable = dynamic_cast<GpuExecutable*>(executable.get());
   ASSERT_NE(gpu_executable, nullptr) << "Executable is not a GpuExecutable.";

xla/service/gpu/gpu_compiler.cc
@@ -44,7 +44,6 @@ limitations under the License.
 #include "llvm/AsmParser/Parser.h"
 #include "llvm/Bitcode/BitcodeReader.h"
 #include "llvm/Bitcode/BitcodeWriter.h"
-#include "llvm/IR/Constants.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/DiagnosticInfo.h"
 #include "llvm/IR/DiagnosticPrinter.h"
@@ -52,7 +51,6 @@ limitations under the License.
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Verifier.h"
-#include "llvm/Support/Casting.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/TargetParser/Triple.h"
@@ -313,6 +311,7 @@ limitations under the License.
 #include "xla/shape.h"
 #include "xla/shape_util.h"
 #include "xla/status_macros.h"
+#include "xla/stream_executor/abi/executable_abi_version.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/device_description.pb.h"
@@ -2725,6 +2724,11 @@ absl::StatusOr<std::unique_ptr<Executable>> GpuCompiler::RunBackend(
   });
 
   std::unique_ptr<GpuAliasInfo> alias_info = GetAliasInfo(gpu_device_info);
+
+  ASSIGN_OR_RETURN(stream_executor::ExecutableAbiVersion executable_abi_version,
+                   stream_executor::ExecutableAbiVersion::FromDeviceDescription(
+                       gpu_device_info));
+
   ASSIGN_OR_RETURN(
       std::unique_ptr<GpuExecutable> gpu_executable,
       GpuExecutable::Create(GpuExecutable::Params{
@@ -2752,7 +2756,8 @@ absl::StatusOr<std::unique_ptr<Executable>> GpuCompiler::RunBackend(
               ? std::move(module)
               : std::unique_ptr<HloModule>(),
           /*enable_debug_info_manager=*/embed_debug_info,
-          /*module_stats=*/std::move(module_stats)}));
+          /*module_stats=*/std::move(module_stats),
+          /*executable_abi_version=*/executable_abi_version}));
 
   if (embed_ir_in_executable) {
     std::string ir_module_string_before_opt =
@@ -3201,6 +3206,10 @@ GpuCompiler::LoadExecutableFromAotResult(
   DebugOptions debug_options = hlo_module->config().debug_options();
   std::string hlo_module_name = hlo_module->name();
 
+  ASSIGN_OR_RETURN(auto executable_abi_version,
+                   stream_executor::ExecutableAbiVersion::FromDeviceDescription(
+                       device_description));
+
   {
     tsl::profiler::TraceMe traceme("CreateGpuExecutable");
     std::unique_ptr<GpuAliasInfo> alias_info = GetAliasInfo(device_description);
@@ -3221,7 +3230,9 @@ GpuCompiler::LoadExecutableFromAotResult(
         /*debug_options=*/std::move(debug_options),
         /*device_description=*/device_description,
         /*debug_module=*/std::move(hlo_module),
-        /*enable_debug_info_manager=*/true});
+        /*enable_debug_info_manager=*/true,
+        /*module_stats=*/{},
+        /*executable_abi_version=*/executable_abi_version});
   }
 }
 

xla/service/gpu/gpu_executable.cc
@@ -100,6 +100,7 @@ limitations under the License.
 #include "xla/shape_tree.h"
 #include "xla/shape_util.h"
 #include "xla/status_macros.h"
+#include "xla/stream_executor/abi/executable_abi_version.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/cuda/cuda_platform_id.h"
 #include "xla/stream_executor/device_address.h"
@@ -296,7 +297,8 @@ absl::StatusOr<std::unique_ptr<GpuExecutable>> GpuExecutable::Create(
       std::move(allocator.MutableAllocations()), std::move(params.alias_info),
       std::move(params.debug_options), std::move(params.constants),
       std::move(params.output_info), params.enable_debug_info_manager,
-      std::move(params.module_stats), std::move(thunk_sequence_proto)));
+      std::move(params.module_stats), std::move(thunk_sequence_proto),
+      std::move(params.executable_abi_version)));
 }
 
 // Implementation note: HLO profiling is always enabled for GPU executables,
@@ -314,7 +316,8 @@ GpuExecutable::GpuExecutable(
     std::vector<ConstantInfo> constants,
     absl::flat_hash_map<ShapeIndex, OutputInfo> output_info,
     bool enable_debug_info_manager, ModuleStats module_stats,
-    absl::StatusOr<std::vector<ThunkProto>> thunk_sequence_proto)
+    absl::StatusOr<std::vector<ThunkProto>> thunk_sequence_proto,
+    stream_executor::ExecutableAbiVersion executable_abi_version)
     : Executable(std::move(debug_module)),
       text_(std::move(asm_text)),
       binary_(std::move(binary)),
@@ -335,7 +338,8 @@ GpuExecutable::GpuExecutable(
       constants_(std::move(constants)),
       output_info_(std::move(output_info)),
       enable_debug_info_manager_(enable_debug_info_manager),
-      thunk_sequence_proto_(std::move(thunk_sequence_proto)) {
+      thunk_sequence_proto_(std::move(thunk_sequence_proto)),
+      executable_abi_version_(std::move(executable_abi_version)) {
   if (gpu_version_.IsRocm()) {
     // ROCm uses hsaco hashes to distinguish between modules.
     // Bad things happen if multiple modules with identical code are loaded.
@@ -1484,6 +1488,8 @@ absl::StatusOr<GpuExecutableProto> GpuExecutable::ToProto() const {
     *proto.add_constants() = constant.ToProto();
   }
 
+  *proto.mutable_executable_abi_version() = executable_abi_version_.proto();
+
   return proto;
 }
 
@@ -1563,6 +1569,10 @@ absl::StatusOr<std::unique_ptr<GpuExecutable>> GpuExecutable::FromProto(
   params.module_name = proto.module_name();
   ASSIGN_OR_RETURN(params.program_shape,
                    ProgramShape::FromProto(proto.program_shape()));
+
+  ASSIGN_OR_RETURN(params.executable_abi_version,
+                   stream_executor::ExecutableAbiVersion::FromProto(
+                       proto.executable_abi_version()));
 
   return Create(std::move(params));
 }

commit b8c135866b6aa901bb22a85823f96dbeeaf18ad0

PR #38836: [ROCm] Add minimal ROCm support to ExecutableAbiVersion
Imported from GitHub PR https://github.com/openxla/xla/pull/38836

Hot-Fix for breaking changes added in 5150bfd950

📝 Summary of Changes
The ExecutableAbiVersion::FromDeviceDescription was only implemented for CUDA, returning UNIMPLEMENTED for all other platforms. This broke every ROCm compilation path after 5150bfd950 introduced mandatory ABI version derivation in GpuCompiler::RunBackend.

Add a CreateForRocm handler that returns a minimal ABI version with
only platform_name set to "ROCm" and no platform-specific version
info. This preserves pre-existing ROCm behavior (no compatibility
checks) until proper ABI versioning is designed for ROCm.

🎯 Justification
without this change JAX tests on ROCm platform fail with error:
```bash
jax.errors.JaxRuntimeError: UNIMPLEMENTED: Deriving the executable ABI version from the device description is not implemented for the target platform.
```
🚀 Kind of Contribution
🐛 Bug Fix

🧪 Unit Tests:
added rocm test to test //xla/stream_executor/abi:executable_abi_version_test

Copybara import of the project:

--
a24b40af2f37fc67a1014572e7c7a2672996053e by Manjunath Gaonkar <magaonka@amd.com>:

[ROCm] Add minimal ROCm support to ExecutableAbiVersion

The ExecutableAbiVersion::FromDeviceDescription was only implemented
for CUDA, returning UNIMPLEMENTED for all other platforms. This broke
every ROCm compilation path after 5150bfd introduced mandatory ABI
version derivation in GpuCompiler::RunBackend.

Add a CreateForRocm handler that returns a minimal ABI version with
only platform_name set to "ROCm" and no platform-specific version
info. This preserves pre-existing ROCm behavior (no compatibility
checks) until proper ABI versioning is designed for ROCm.

Add a unit test for the ROCm path.

Merging this change closes #38836

xla/stream_executor/abi/BUILD
@@ -41,6 +41,7 @@ xla_cc_test(
         "//xla/stream_executor:device_description",
         "//xla/stream_executor:semantic_version",
         "//xla/stream_executor/cuda:cuda_compute_capability",
+        "//xla/stream_executor/rocm:rocm_compute_capability",
         "//xla/tsl/util/proto:proto_matchers",
         "@com_google_googletest//:gtest_main",
     ],

xla/stream_executor/abi/executable_abi_version.cc
@@ -39,6 +39,16 @@ static absl::StatusOr<ExecutableAbiVersion> CreateForCuda(
   return ExecutableAbiVersion::FromProto(std::move(proto));
 }
 
+// Returns a minimal ABI version for ROCm with no platform-specific version
+// info. Compatibility checks will treat this as always-compatible, preserving
+// pre-existing ROCm behavior until proper ABI versioning is designed.
+static absl::StatusOr<ExecutableAbiVersion> CreateForRocm(
+    const DeviceDescription& /*device_description*/) {
+  ExecutableAbiVersionProto proto;
+  proto.set_platform_name("ROCm");
+  return ExecutableAbiVersion::FromProto(std::move(proto));
+}
+
 absl::StatusOr<ExecutableAbiVersion> ExecutableAbiVersion::FromProto(
     const ExecutableAbiVersionProto& proto) {
   return ExecutableAbiVersion(proto);
@@ -48,6 +58,9 @@ ExecutableAbiVersion::FromDeviceDescription(
     const DeviceDescription& device_description) {
   if (device_description.gpu_compute_capability().IsCuda()) {
     return CreateForCuda(device_description);
+  }
+  if (device_description.gpu_compute_capability().IsRocm()) {
+    return CreateForRocm(device_description);
   }
 
   return absl::UnimplementedError(

xla/stream_executor/abi/executable_abi_version_test.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include <gtest/gtest.h>
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.h"
+#include "xla/stream_executor/rocm/rocm_compute_capability.h"
 #include "xla/stream_executor/semantic_version.h"
 #include "xla/tsl/util/proto/proto_matchers.h"
 
@@ -57,6 +58,19 @@ TEST(ExecutableAbiVersionTest, FromDeviceDescription) {
                   cub_version: "2.0.0"
                 }
               )pb"));
+}
+
+TEST(ExecutableAbiVersionTest, FromDeviceDescriptionRocm) {
+  DeviceDescription device_description;
+  device_description.set_gpu_compute_capability(
+      GpuComputeCapability(RocmComputeCapability("gfx942")));
+
+  ASSERT_OK_AND_ASSIGN(
+      ExecutableAbiVersion executable_abi_version,
+      ExecutableAbiVersion::FromDeviceDescription(device_description));
+  EXPECT_THAT(executable_abi_version.platform_name(), "ROCm");
+  EXPECT_THAT(executable_abi_version.proto(),
+              EqualsProto(R"pb(platform_name: "ROCm")pb"));
 }
 
 }  // namespace