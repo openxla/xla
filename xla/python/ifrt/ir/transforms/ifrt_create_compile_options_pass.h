f9d9a3e0276d066714bd869751371f7e431fdb9c
Ionel Gog <icgog@google.com>
2026-04-03 10:40:58 -0700

    [IFRT IR] Clean-up of how compile options creation
    PiperOrigin-RevId: 894149651

---
+++ xla/python/ifrt/ir/transforms/ifrt_create_compile_options_pass.h
@@ -0,0 +1,55 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_PYTHON_IFRT_IR_TRANSFORMS_IFRT_CREATE_COMPILE_OPTIONS_PASS_H_
+#define XLA_PYTHON_IFRT_IR_TRANSFORMS_IFRT_CREATE_COMPILE_OPTIONS_PASS_H_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/status/statusor.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "xla/pjrt/pjrt_executable.h"
+
+namespace xla {
+namespace ifrt {
+
+// Compile environment options override is a vector of pairs of compile option
+// flag and value strings.
+using EnvOptionsOverride =
+    std::vector<std::pair<std::string, xla::CompileOptions::OptionOverride>>;
+
+// Mapping between a string identifier (e.g., mesh name) and XLA compile
+// options.
+using CompileOptionsMap = absl::flat_hash_map<std::string, xla::CompileOptions>;
+
+// Gets the per atom program compile options from the IFRT IR module.
+//
+// `compile_options_overrides` is a mapping from mesh name to compile option
+// overrides. The overrides apply to all the computations assigned to a mesh.
+//
+// `threshold_for_parameter_tupling` is the threshold for parameter tupling.
+absl::StatusOr<CompileOptionsMap> GetCompileOptions(
+    mlir::ModuleOp module,
+    const absl::flat_hash_map<std::string, const EnvOptionsOverride>&
+        compile_options_overrides,
+    int threshold_for_parameter_tupling = 2000);
+
+}  // namespace ifrt
+}  // namespace xla
+
+#endif  // XLA_PYTHON_IFRT_IR_TRANSFORMS_IFRT_CREATE_COMPILE_OPTIONS_PASS_H_
--- xla/python/ifrt/ir/transforms/utils.cc
+++ xla/python/ifrt/ir/transforms/utils.cc
@@ -31,6 +31,7 @@ limitations under the License.
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "llvm/ADT/Hashing.h"
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
@@ -45,6 +46,7 @@ limitations under the License.
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/IR/OwningOpRef.h"
+#include "mlir/IR/SymbolTable.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Support/DebugStringHelper.h"
 #include "mlir/Support/LLVM.h"
@@ -68,6 +70,8 @@ limitations under the License.
 #include "xla/python/pjrt_ifrt/pjrt_dtype.h"
 #include "xla/python/pjrt_ifrt/xla_compiler.h"
 #include "xla/python/pjrt_ifrt/xla_sharding.h"
+#include "xla/service/computation_placer.h"
+#include "xla/service/spmd/shardy/utils.h"
 #include "xla/status_macros.h"
 #include "xla/tsl/platform/statusor.h"
 #include "xla/xla_data.pb.h"
@@ -164,6 +168,43 @@ void GetPrettyLocation(mlir::Location loc,
   } else if (auto file_loc = mlir::dyn_cast<mlir::FileLineColLoc>(loc)) {
     PrintFileLoc(file_loc, loc_stream);
   }
+}
+
+// Construct a bool vector with a True entry for each input sharding that must
+// be inferred.
+llvm::SmallVector<bool> GetInputShardingPropagation(
+    mlir::func::FuncOp func_op) {
+  llvm::SmallVector<bool> sharding_propagation_to_input;
+  sharding_propagation_to_input.reserve(func_op.getNumArguments());
+  for (int idx = 0; idx < func_op.getNumArguments(); ++idx) {
+    const auto hlo_sharding_attr =
+        func_op.getArgAttrOfType<mlir::StringAttr>(idx, kHloShardingAttrName);
+    if (hlo_sharding_attr == nullptr) {
+      sharding_propagation_to_input.push_back(true);
+    } else {
+      sharding_propagation_to_input.push_back(false);
+    }
+  }
+  return sharding_propagation_to_input;
+}
+
+// Construct a bool vector with a True entry for each output sharding that must
+// be inferred.
+llvm::SmallVector<bool> GetOutputShardingPropagation(
+    mlir::func::FuncOp func_op) {
+  llvm::SmallVector<bool> sharding_propagation_to_output;
+  sharding_propagation_to_output.reserve(func_op.getNumResults());
+  for (int idx = 0; idx < func_op.getNumResults(); ++idx) {
+    const auto hlo_sharding_attr =
+        func_op.getResultAttrOfType<mlir::StringAttr>(idx,
+                                                      kHloShardingAttrName);
+    if (hlo_sharding_attr == nullptr) {
+      sharding_propagation_to_output.push_back(true);
+    } else {
+      sharding_propagation_to_output.push_back(false);
+    }
+  }
+  return sharding_propagation_to_output;
 }
 
 }  // namespace
@@ -373,6 +414,53 @@ absl::StatusOr<ArraySpec> ArraySpecFromMlirType(
       /*shape=*/Shape(ifrt_array_type.getShape().getShape()),
       /*sharding=*/std::move(sharding),
   };
+}
+
+xla::CompileOptions GetDefaultCompileOptions(CallOp call_op,
+                                             bool enable_sharding_propagation,
+                                             bool enable_parameter_tupling) {
+  mlir::SymbolTableCollection symbol_table;
+
+  xla::CompileOptions compile_options;
+  auto& exec_build_options = compile_options.executable_build_options;
+  // Executable build options are constructed using logical ids, which are
+  // later converted into real Device ids by using the logical ids as
+  // indices into the device list given at compilation invocation time.
+  llvm::ArrayRef<int> logical_device_ids = call_op.getDevices();
+  mlir::func::FuncOp callee = call_op.getCalleeOp(symbol_table);
+  if (call_op->hasAttrOfType<mlir::UnitAttr>(kIfrtLocalViewAttrName)) {
+    exec_build_options.set_num_replicas(logical_device_ids.size());
+    exec_build_options.set_num_partitions(1);
+    xla::DeviceAssignment device_assignment(logical_device_ids.size(), 1);
+    for (const auto [i, device_id] : llvm::enumerate(logical_device_ids)) {
+      device_assignment(i, 0) = device_id;
+    }
+    exec_build_options.set_device_assignment(device_assignment);
+    exec_build_options.set_use_spmd_partitioning(false);
+  } else {
+    exec_build_options.set_num_replicas(1);
+    exec_build_options.set_num_partitions(logical_device_ids.size());
+    xla::DeviceAssignment device_assignment(1, logical_device_ids.size());
+    for (const auto [i, device_id] : llvm::enumerate(logical_device_ids)) {
+      device_assignment(0, i) = device_id;
+    }
+    exec_build_options.set_device_assignment(device_assignment);
+    exec_build_options.set_use_spmd_partitioning(true);
+    mlir::ModuleOp callee_module = callee->getParentOfType<mlir::ModuleOp>();
+    if (xla::sdy::hasShardyMesh(callee_module)) {
+      exec_build_options.set_use_shardy_partitioner(true);
+    }
+    if (enable_sharding_propagation) {
+      exec_build_options.set_allow_spmd_sharding_propagation_to_parameters(
+          GetInputShardingPropagation(callee));
+      exec_build_options.set_allow_spmd_sharding_propagation_to_output(
+          GetOutputShardingPropagation(callee));
+    }
+  }
+
+  compile_options.parameter_is_tupled_arguments = enable_parameter_tupling;
+
+  return compile_options;
 }
 
 }  // namespace ifrt
