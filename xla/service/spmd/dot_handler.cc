e3f6d8baa70f08d69c4c364f209c5aee310c9e0b
xla authors <google-ml-automation@google.com>
2026-04-30 09:04:20 -0700

    Add sharding conversions in `DotHandler` for operands used for functions expecting v2 sharding specifically `ShardingPropagation::GetShardingFromUser`
    Reverts ad9965a3e2a51e2e807b814ef7d765226a387955
    
    PiperOrigin-RevId: 908207907

--- xla/service/spmd/dot_handler.cc
+++ xla/service/spmd/dot_handler.cc
@@ -437,13 +437,9 @@ std::optional<WindowedEinsumConfig> GetWindowedEinsumConfiguration(
     if (!original_ideal_sharding) {
       return true;
     }
-    for (HloInstruction* user : to_loop_over->users()) {
+    for (const HloInstruction* user : to_loop_over->users()) {
       if (user == original_hlo) {
         continue;
-      }
-      if (user->has_sharding() && user->sharding().UseNamedShardingLeaf()) {
-        user->set_sharding(
-            HloSharding::V3ToV2Sharding(user->sharding().named_sharding()));
       }
       std::optional<HloSharding> from_user =
           ShardingPropagation::GetShardingFromUser(
@@ -4302,19 +4298,6 @@ absl::Status SpmdPartitioningVisitor::HandleDotHelper(
   if (hlo->sharding().IsSingleDevice()) {
     return DefaultAction(hlo);
   }
-
-  if (hlo->has_sharding() && hlo->sharding().UseNamedShardingLeaf()) {
-    hlo->set_sharding(
-        HloSharding::V3ToV2Sharding(hlo->sharding().named_sharding()));
-  }
-  for (int i = 0; i < hlo->operand_count(); ++i) {
-    if (hlo->operand(i)->has_sharding() &&
-        hlo->operand(i)->sharding().UseNamedShardingLeaf()) {
-      hlo->mutable_operand(i)->set_sharding(HloSharding::V3ToV2Sharding(
-          hlo->operand(i)->sharding().named_sharding()));
-    }
-  }
-
   HloInstruction* partitioned_dot;
   Window conv_window;
   if constexpr (std::is_same_v<CreateShardedFunctor,
