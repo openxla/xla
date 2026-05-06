a1806d661e3ad60724a5ddc7054f81c23bf04a77
Nhat Le <nhat.le@intel.com>
2026-05-06 08:06:58 -0700

    Add required header

--- xla/backends/gpu/runtime/send_recv_thunk_test.cc
+++ xla/backends/gpu/runtime/send_recv_thunk_test.cc
@@ -47,6 +47,7 @@ limitations under the License.
 #include "xla/stream_executor/platform.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/stream_executor/stream_executor.h"
+#include "xla/stream_executor/sycl/sycl_platform_id.h"
 #include "xla/tests/hlo_test_base_legacy.h"
 #include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/platform/test.h"
