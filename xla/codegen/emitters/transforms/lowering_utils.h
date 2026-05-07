96325845405baca72b36479134675e1cfcd92378
Akhil Goel <akhil.goel@intel.com>
2026-05-07 10:07:01 -0700

    PR #42129: [XLA:GPU][codegen] Use driver intrinsics for natural log/exp and (A)Trig(H) math lowerings in SPIRV backend
    Imported from GitHub PR https://github.com/openxla/xla/pull/42129
    
    This PR uses driver intrinsics to lower natural log/exp, regular, hyperbolic and inverse trigonometric functions for single and double precision types in SPIR-V backend.
    
    Copybara import of the project:
    
    --
    ad2df7d6592c55b44a2662ce478680515098f39d by Akhil Goel <akhil.goel@intel.com>:
    
    Use ocl intrinsics for SPIRV math lowering
    
    Merging this change closes #42129
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/42129 from Intel-tensorflow:akhil/oneapi_math ad2df7d6592c55b44a2662ce478680515098f39d
    PiperOrigin-RevId: 912022350

--- xla/codegen/emitters/transforms/lowering_utils.h
+++ xla/codegen/emitters/transforms/lowering_utils.h
@@ -16,7 +16,12 @@ limitations under the License.
 #ifndef XLA_CODEGEN_EMITTERS_TRANSFORMS_LOWERING_UTILS_H_
 #define XLA_CODEGEN_EMITTERS_TRANSFORMS_LOWERING_UTILS_H_
 
+#include "absl/strings/str_format.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Dialect/Math/IR/Math.h"
 #include "mlir/IR/Operation.h"
+#include "mlir/lib/Conversion/GPUCommon/GPUOpsLowering.h"
+#include "mlir/lib/Conversion/GPUCommon/OpToFuncCallLowering.h"
 
 namespace xla {
 namespace emitters {
@@ -25,6 +30,43 @@ namespace emitters {
 // AMDGPU requires allocas in AS5, but MLIR lowering creates them in AS0.
 void EnsureAMDGPUAllocasUseAS5(mlir::Operation* operation);
 
+namespace spirv {
+namespace mm = ::mlir::math;
+template <typename... Ops>
+struct SPIRVMathOps {};
+
+inline auto getSPIRVMathOps() {
+  return SPIRVMathOps<
+      mm::AcosOp, mm::AcoshOp, mm::AsinOp, mm::AsinhOp, mm::Atan2Op, mm::AtanOp,
+      mm::AtanhOp, mm::CosOp, mm::CoshOp, mm::ExpM1Op, mm::ExpOp, mm::Log1pOp,
+      mm::LogOp, mm::SinOp, mm::SinhOp, mm::TanOp, mm::TanhOp>{};
+}
+
+// Lowers math ops to SPIR-V OCL driver intrinsics to preserve accuracy,
+// particularly for small-magnitude inputs where generic MLIR lowering may
+// lose precision.
+template <typename Op>
+inline void populateLLVMSPVMathOpPatterns(mlir::LLVMTypeConverter& converter,
+                                          mlir::RewritePatternSet& patterns) {
+  auto op_name =
+      mlir::OperationName(Op::getOperationName(), &converter.getContext())
+          .stripDialect();
+  auto base =
+      absl::StrFormat("_Z%u__spirv_ocl_%s", op_name.size() + 12, op_name.str());
+
+  patterns.add<mlir::ScalarizeVectorOpLowering<Op>>(converter, 1);
+  patterns.add<mlir::OpToFuncCallLowering<Op>>(converter, base + "f",
+                                               base + "d", "", "", "", 1);
+}
+
+template <typename... Ops>
+void populateMathToLLVMSPVConversionPatterns(
+    spirv::SPIRVMathOps<Ops...>, mlir::LLVMTypeConverter& converter,
+    mlir::RewritePatternSet& patterns) {
+  (spirv::populateLLVMSPVMathOpPatterns<Ops>(converter, patterns), ...);
+}
+
+}  // namespace spirv
 }  // namespace emitters
 }  // namespace xla
 
