f7005b01af125e2864a8f09a052e0b6836dd34bc
Shawn Wang <shawnw@nvidia.com>
2026-06-11 02:59:13 +0000

    Fix dynamic slice async wrapper formatting

--- xla/backends/gpu/transforms/dynamic_slice_copy_fusion_async_wrapper.cc
+++ xla/backends/gpu/transforms/dynamic_slice_copy_fusion_async_wrapper.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include "absl/container/flat_hash_set.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
+#include "xla/tsl/platform/status_macros.h"
 #include "xla/backends/gpu/transforms/dynamic_slice_fusion.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
@@ -29,7 +30,6 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/shape_util.h"
-#include "xla/tsl/platform/status_macros.h"
 #include "xla/xla_data.pb.h"
 
 namespace xla::gpu {
