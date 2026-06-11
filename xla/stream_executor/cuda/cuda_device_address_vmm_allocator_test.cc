613c866d9b808d5be43f742305708dc613cec569
Shawn Wang <shawnw@nvidia.com>
2026-06-11 14:40:06 +0000

    Rename VMM device address allocator files

--- xla/stream_executor/cuda/cuda_device_address_vmm_allocator_test.cc
+++ xla/stream_executor/cuda/cuda_device_address_vmm_allocator_test.cc
@@ -32,7 +32,7 @@ limitations under the License.
 #include "xla/stream_executor/platform_manager.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/stream_executor/stream_executor.h"
-#include "xla/stream_executor/vmm_device_address_allocator.h"
+#include "xla/stream_executor/device_address_vmm_allocator.h"
 #include "xla/tsl/platform/statusor.h"
 
 namespace stream_executor {
