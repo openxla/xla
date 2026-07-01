de01ff2147302abcbf5e3f4c9e6153b9ac4518e6
Aditya Sharma <adisharma@google.com>
2026-07-01 01:01:14 -0700

    Use TPU Profiler C API in XLA plugin_tracer_impl and ProfilerSession
    PiperOrigin-RevId: 940915721

--- third_party/tsl/tsl/profiler/lib/profiler_session.h
+++ third_party/tsl/tsl/profiler/lib/profiler_session.h
@@ -67,6 +67,13 @@ class ProfilerSession {
   absl::Status CollectData(tensorflow::profiler::XSpace* space)
       TF_LOCKS_EXCLUDED(mutex_);
 
+  // Consumes collected profile data and returns a container of consumed data.
+  absl::StatusOr<profiler::ConsumeResult> Consume() TF_LOCKS_EXCLUDED(mutex_);
+
+  // Serializes consumed data into XSpace.
+  absl::Status Serialize(std::any data, tensorflow::profiler::XSpace* space)
+      TF_LOCKS_EXCLUDED(mutex_);
+
  private:
   // Constructs an instance of the class and starts profiling
   explicit ProfilerSession(const tensorflow::ProfileOptions& options);
