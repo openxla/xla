3e8197d41928fbdbe78dcadcb9c1a34ff771b92a
LiaMath <lialytics@gmail.com>
2026-07-26 15:58:48 +0900

    Fix status_matchers include for cpu_instruction_fusion_test

--- xla/service/cpu/cpu_instruction_fusion_test.cc
+++ xla/service/cpu/cpu_instruction_fusion_test.cc
@@ -23,6 +23,7 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/algorithm/container.h"
+#include "absl/status/status_matchers.h"  // IWYU pragma: keep
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
