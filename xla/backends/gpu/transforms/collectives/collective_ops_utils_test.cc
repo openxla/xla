a1aee5b83dc8c488d724c6dcab44debc18e90ee3
scxfjiang <xuefei.jiang@amd.com>
2026-08-04 00:04:35 +0800

    clean up code

--- xla/backends/gpu/transforms/collectives/collective_ops_utils_test.cc
+++ xla/backends/gpu/transforms/collectives/collective_ops_utils_test.cc
@@ -17,7 +17,6 @@ limitations under the License.
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include "absl/status/status.h"
 #include "absl/status/status_matchers.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
@@ -35,7 +34,6 @@ namespace {
 
 using ::absl_testing::IsOk;
 using ::absl_testing::IsOkAndHolds;
-using ::absl_testing::StatusIs;
 using ::testing::Test;
 
 class CommunicationTypeTest : public Test {
