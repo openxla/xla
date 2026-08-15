75216e702779ff05c2665819cd88a215e4ce7624
Alexander Belyaev <pifon@google.com>
2026-08-15 15:08:52 -0700

    [XLA:CPU] Unroll vectors to remove the stack allocations.
    PiperOrigin-RevId: 965305095

--- xla/backends/cpu/codegen/fusion_compiler.cc
+++ xla/backends/cpu/codegen/fusion_compiler.cc
@@ -454,7 +454,7 @@ void AddNewVectorToLLVMPasses(mlir::OpPassManager& pm, bool fast_min_max) {
   pm.addPass(cpu::createMemrefCopyToLoopsPass());
   pm.addPass(cpu::createLowerToLLVMPass());
   pm.addPass(mlir::createConvertVectorToSCFPass(
-      mlir::VectorTransferToSCFOptions().enableFullUnroll(false)));
+      mlir::VectorTransferToSCFOptions().enableFullUnroll(true)));
   pm.addNestedPass<mlir::func::FuncOp>(cpu::createHoistAllocaPass());
   pm.addPass(cpu::createUnpackSubByteVectorWritePass());
 
