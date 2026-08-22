ryan-g-benney/tensorflow

Use two-pass variance calculation in BatchNormExpander to prevent negative variance with large input offsets.
Fixes #118701

Reverts 8537a319505e185323ecb0b761941faab81ecb98

FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/47544 from ezhulenev:future-perfect-forwarding 20f9edc1304a7d176f4fd5a6f19fb9f180617983
PiperOrigin-RevId: 967235342

third_party/xla/xla/tests/batch_norm_training_test.cc
@@ -13,14 +13,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cmath>
 #include <utility>
 
 #include "xla/tests/xla_test_backend_predicates.h"
 #include "absl/status/status.h"
 #include "xla/hlo/testlib/test.h"
 #include "xla/literal_util.h"
-#include "xla/tests/hlo_pjrt_test_base.h"
-#include "xla/tsl/platform/statusor.h"
+#include "xla/tests/hlo_test_base.h"
 
 namespace xla {
 namespace {
@@ -40,15 +40,14 @@ ENTRY entry {
 class BatchNormTrainingTest : public HloTestBase {};
 
 TEST_F(BatchNormTrainingTest, CorrectComputation) {
-  TF_ASSERT_OK_AND_ASSIGN(auto module,
-                          ParseAndReturnVerifiedModule(kModuleStr));
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(kModuleStr));
 
   auto input = LiteralUtil::CreateR2<float>({{1.0}, {2.0}});
   auto scale = LiteralUtil::CreateR1<float>({0.5});
   auto offset = LiteralUtil::CreateR1<float>({0.1});
 
-  TF_ASSERT_OK_AND_ASSIGN(
-      auto result, Execute(std::move(module), {&input, &scale, &offset}));
+  ASSERT_OK_AND_ASSIGN(auto result,
+                       Execute(std::move(module), {&input, &scale, &offset}));
 
   // Decompose result tuple
   auto result_tuple = result.DecomposeTuple();
@@ -78,12 +77,50 @@ TEST_F(BatchNormTrainingTest, CorrectComputation) {
   }
 }
 
+TEST_F(BatchNormTrainingTest, LargeOffset) {
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(kModuleStr));
+
+  auto input =
+      LiteralUtil::CreateR2<float>({{10000.0f + 1.0f}, {10000.0f + 2.0f}});
+  auto scale = LiteralUtil::CreateR1<float>({0.5f});
+  auto offset = LiteralUtil::CreateR1<float>({0.1f});
+
+  ASSERT_OK_AND_ASSIGN(auto result,
+                       Execute(std::move(module), {&input, &scale, &offset}));
+
+  auto result_tuple = result.DecomposeTuple();
+
+  auto expected_output =
+      LiteralUtil::CreateR2<float>({{-0.399003029f}, {0.599003f}});
+  auto expected_scale = LiteralUtil::CreateR1<float>({10000.0f + 1.5f});
+  auto expected_mean = LiteralUtil::CreateR1<float>({0.25f});
+
+  const float tolerance = 1e-4f;
+
+  for (int i = 0; i < expected_output.element_count(); ++i) {
+    EXPECT_FALSE(std::isnan(result_tuple[0].data<float>()[i]));
+    EXPECT_NEAR(result_tuple[0].data<float>()[i],
+                expected_output.data<float>()[i], tolerance);
+  }
+
+  for (int i = 0; i < expected_scale.element_count(); ++i) {
+    EXPECT_NEAR(result_tuple[1].data<float>()[i],
+                expected_scale.data<float>()[i], tolerance);
+  }
+
+  for (int i = 0; i < expected_mean.element_count(); ++i) {
+    EXPECT_FALSE(std::isnan(result_tuple[2].data<float>()[i]));
+    EXPECT_GE(result_tuple[2].data<float>()[i], 0.0f);
+    EXPECT_NEAR(result_tuple[2].data<float>()[i],
+                expected_mean.data<float>()[i], tolerance);
+  }
+}
+
 TEST_F(BatchNormTrainingTest, ReturnsErrorWhenHloPassesDisabled) {
   if (test::DeviceTypeIsOneOf({test::kGpu, test::kInterpreter, test::kTpu})) {
     GTEST_SKIP();
   }
-  TF_ASSERT_OK_AND_ASSIGN(auto module,
-                          ParseAndReturnVerifiedModule(kModuleStr));
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(kModuleStr));
 
   auto status_or_result =
       Execute(std::move(module), {}, /*run_hlo_passes=*/false);

openxla/xla

xla/tests/batch_norm_training_test.cc
@@ -13,14 +13,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cmath>
 #include <utility>
 
 #include "xla/tests/xla_test_backend_predicates.h"
+#include <gmock/gmock.h>
 #include "absl/status/status.h"
 #include "xla/hlo/testlib/test.h"
 #include "xla/literal_util.h"
-#include "xla/tests/hlo_pjrt_test_base.h"
-#include "xla/tsl/platform/statusor.h"
+#include "xla/tests/hlo_test_base.h"
 
 namespace xla {
 namespace {
@@ -40,15 +41,14 @@ ENTRY entry {
 class BatchNormTrainingTest : public HloTestBase {};
 
 TEST_F(BatchNormTrainingTest, CorrectComputation) {
-  TF_ASSERT_OK_AND_ASSIGN(auto module,
-                          ParseAndReturnVerifiedModule(kModuleStr));
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(kModuleStr));
 
   auto input = LiteralUtil::CreateR2<float>({{1.0}, {2.0}});
   auto scale = LiteralUtil::CreateR1<float>({0.5});
   auto offset = LiteralUtil::CreateR1<float>({0.1});
 
-  TF_ASSERT_OK_AND_ASSIGN(
-      auto result, Execute(std::move(module), {&input, &scale, &offset}));
+  ASSERT_OK_AND_ASSIGN(auto result,
+                       Execute(std::move(module), {&input, &scale, &offset}));
 
   // Decompose result tuple
   auto result_tuple = result.DecomposeTuple();
@@ -78,12 +78,50 @@ TEST_F(BatchNormTrainingTest, CorrectComputation) {
   }
 }
 
+TEST_F(BatchNormTrainingTest, LargeOffset) {
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(kModuleStr));
+
+  auto input =
+      LiteralUtil::CreateR2<float>({{10000.0f + 1.0f}, {10000.0f + 2.0f}});
+  auto scale = LiteralUtil::CreateR1<float>({0.5f});
+  auto offset = LiteralUtil::CreateR1<float>({0.1f});
+
+  ASSERT_OK_AND_ASSIGN(auto result,
+                       Execute(std::move(module), {&input, &scale, &offset}));
+
+  auto result_tuple = result.DecomposeTuple();
+
+  auto expected_output =
+      LiteralUtil::CreateR2<float>({{-0.399003029f}, {0.599003f}});
+  auto expected_batch_mean = LiteralUtil::CreateR1<float>({10000.0f + 1.5f});
+  auto expected_batch_var = LiteralUtil::CreateR1<float>({0.25f});
+
+  const float tolerance = 1e-4f;
+
+  for (int i = 0; i < expected_output.element_count(); ++i) {
+    EXPECT_FALSE(std::isnan(result_tuple[0].data<float>()[i]));
+    EXPECT_NEAR(result_tuple[0].data<float>()[i],
+                expected_output.data<float>()[i], tolerance);
+  }
+
+  for (int i = 0; i < expected_batch_mean.element_count(); ++i) {
+    EXPECT_NEAR(result_tuple[1].data<float>()[i],
+                expected_batch_mean.data<float>()[i], tolerance);
+  }
+
+  for (int i = 0; i < expected_batch_var.element_count(); ++i) {
+    EXPECT_FALSE(std::isnan(result_tuple[2].data<float>()[i]));
+    EXPECT_GE(result_tuple[2].data<float>()[i], 0.0f);
+    EXPECT_NEAR(result_tuple[2].data<float>()[i],
+                expected_batch_var.data<float>()[i], tolerance);
+  }
+}
+
 TEST_F(BatchNormTrainingTest, ReturnsErrorWhenHloPassesDisabled) {
   if (test::DeviceTypeIsOneOf({test::kGpu, test::kInterpreter, test::kTpu})) {
     GTEST_SKIP();
   }
-  TF_ASSERT_OK_AND_ASSIGN(auto module,
-                          ParseAndReturnVerifiedModule(kModuleStr));
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(kModuleStr));
 
   auto status_or_result =
       Execute(std::move(module), {}, /*run_hlo_passes=*/false);