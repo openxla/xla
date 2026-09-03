2418d926c11181bece8a245a33f0bdad377e7c82
David Pizzuto <pizzud@google.com>
2026-09-03 14:58:34 -0700

    pjrt_c_api_client: Remove "PJRT C API" from the platform version string.
    A new PJRT method is introduced to allow C API detection specifically rather
    than inferring it.
    
    This allows easier alignment with internal codepaths that don't go through this
    particular case.
    
    PiperOrigin-RevId: 975941153

--- xla/pjrt/pjrt_client.cc
+++ xla/pjrt/pjrt_client.cc
@@ -119,6 +119,8 @@ PjRtMemorySpaceCApiDelegator::PjRtMemorySpaceCApiDelegator(
 
 PjRtBuffer::ExternalReference::~ExternalReference() = default;
 
+bool PjRtClient::is_c_api() const { return false; }
+
 absl::StatusOr<std::uintptr_t> PjRtClient::UnsafeBufferPointer(
     PjRtBuffer* buffer) {
   if (buffer->on_device_shape().IsTuple()) {
