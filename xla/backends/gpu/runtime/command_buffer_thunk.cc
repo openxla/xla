e7647b7ed4994ce14ffab3cf804951ef4cdeb732
Eugene Zhulenev <ezhulenev@openxla.org>
2026-09-14 23:57:17 +0000

    [xla] Add a mechanism to forward custom options from IFRT down to XLA:GPU and CPU runtime

--- xla/backends/gpu/runtime/command_buffer_thunk.cc
+++ xla/backends/gpu/runtime/command_buffer_thunk.cc
@@ -217,7 +217,8 @@ absl::Status CommandBufferThunk::Initialize(const InitializeParams& params) {
       /*device_to_host_stream=*/nullptr,
       /*host_to_device_stream=*/nullptr,
       /*send_device_memory_function=*/nullptr,
-      /*recv_device_memory_function=*/nullptr, params.ffi_execution_context,
+      /*recv_device_memory_function=*/nullptr, params.custom_options,
+      params.ffi_execution_context,
       /*additional_compute_streams=*/{}, params.execution_scoped_state,
       /*mock_collectives=*/false, /*execution_id=*/0,
       /*rng_seed=*/0, params.persistent_alloc_indices);
