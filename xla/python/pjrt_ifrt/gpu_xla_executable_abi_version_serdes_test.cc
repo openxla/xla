9eff72326404e7e4dc1b3a568c394dcc4f0642b8
xla authors <google-ml-automation@google.com>
2026-09-16 19:16:25 -0700

    Automated Code Change
    PiperOrigin-RevId: 982865523

--- xla/python/pjrt_ifrt/gpu_xla_executable_abi_version_serdes_test.cc
+++ xla/python/pjrt_ifrt/gpu_xla_executable_abi_version_serdes_test.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include <memory>
 #include <utility>
 
+#include "absl/status/status.h"
 #include "absl/status/status_macros.h"
 #include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
