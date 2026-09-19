370f2d56c68868efb0def496aa9848994750e400
xla authors <google-ml-automation@google.com>
2026-09-18 18:35:07 -0700

    Automated Code Change
    PiperOrigin-RevId: 984175698

--- xla/backends/gpu/collectives/nccl_collectives.cc
+++ xla/backends/gpu/collectives/nccl_collectives.cc
@@ -70,6 +70,7 @@ limitations under the License.
 #include "xla/tsl/platform/logging.h"
 #include "xla/tsl/platform/threadpool.h"
 #include "xla/util.h"
+#include "xla/xla.pb.h"
 
 namespace xla::gpu {
 
