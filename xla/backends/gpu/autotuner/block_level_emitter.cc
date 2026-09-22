20c74be557c4a1652a0740f40ecc7576c80c1ae2
Mohammed Anany <manany@google.com>
2026-09-22 13:12:41 -0700

    [XLA:GPU] Evaluate tiling candidates in parallel in SoftmaxRewriterTriton
    PiperOrigin-RevId: 986187112

--- xla/backends/gpu/autotuner/block_level_emitter.cc
+++ xla/backends/gpu/autotuner/block_level_emitter.cc
@@ -129,8 +129,9 @@ BlockLevelEmitterBackend::GetSupportedConfigs(const HloInstruction& instr) {
                             .xla_gpu_fusion_autotune_top_k_configs();
   ABSL_ASSIGN_OR_RETURN(
       TopKTiledRunTimeDataOrError tiled_runtime_data,
-      indexing_performance_model_.TryFindTopKBestTilingsForFusion(
-          *fusion_adaptor, num_configs));
+      indexing_performance_model_
+          .TryFindTopKBestTilingsForFusionAsync(*fusion_adaptor, num_configs)
+          .Await());
 
   if (std::holds_alternative<FusionDecision>(tiled_runtime_data)) {
     return std::vector<std::unique_ptr<BackendConfig>>();
@@ -157,9 +158,10 @@ BlockLevelEmitterBackend::GetCostModelConfig(const HloInstruction& instr) {
   auto fusion_adaptor =
       HloFusionAdaptor::ForInstruction(Cast<HloFusionInstruction>(&instr));
 
-  ABSL_ASSIGN_OR_RETURN(
-      TiledRunTimeDataOrError tiled_runtime_data_or_error,
-      indexing_performance_model_.TryFindBestTilingForFusion(*fusion_adaptor));
+  ABSL_ASSIGN_OR_RETURN(TiledRunTimeDataOrError tiled_runtime_data_or_error,
+                        indexing_performance_model_
+                            .TryFindBestTilingForFusionAsync(*fusion_adaptor)
+                            .Await());
 
   if (const auto* fusion_decision =
           std::get_if<FusionDecision>(&tiled_runtime_data_or_error)) {
