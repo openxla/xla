1798a1e9951e6a96a6af7802d54d9f212a71b142
Henning Becker <hebecker@google.com>
2026-09-24 00:09:47 -0700

    [XLA:Codegen] Remove unused GPU `backend_configs_cc` dependencies from `codegen/tiling` and `codegen/xtile`
    Remove unused `#include "xla/service/gpu/backend_configs.proto.h"` includes and `//xla/service/gpu:backend_configs_cc` build dependencies from `//xla/codegen/tiling:symbolic_tile_analysis`, `//xla/codegen/tiling/experimental:tiled_hlo`, `//xla/codegen/xtile:block_level_parameters`, and `//xla/codegen/xtile:tiling_from_block_parameters` (left over after `BlockLevelFusionConfig` and `Tile` were migrated to `xtile_config.proto`).
    
    PiperOrigin-RevId: 987284532

--- xla/codegen/xtile/block_level_parameters_test.cc
+++ xla/codegen/xtile/block_level_parameters_test.cc
@@ -19,7 +19,6 @@ limitations under the License.
 #include <gtest/gtest.h>
 
 #include "xla/codegen/xtile/xtile_config.pb.h"
-#include "xla/service/gpu/backend_configs.pb.h"
 
 namespace xla::xtile {
 namespace {
