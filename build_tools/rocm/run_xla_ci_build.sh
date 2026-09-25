ddb76fc240b56be7672d430e70c0bd6eaa1662f2
Alexandros Theodoridis <atheodor@amd.com>
2026-09-25 11:51:15 +0000

    Move shim script logic upstream

--- build_tools/rocm/run_xla_ci_build.sh
+++ build_tools/rocm/run_xla_ci_build.sh
@@ -35,16 +35,27 @@ for arg in "$@"; do
     fi
 done
 
+EXCLUDED_TESTS=(
+    "ConvolutionTest.Convolve3D_1x4x2x3x3_2x2x2x3x3_Valid*"
+    "ConvolutionTest.Convolve_1x1x4x4_1x1x2x2_Valid*"
+    "ConvolutionTest.Convolve_1x1x4x4_1x1x2x2_Same*"
+    "ConvolutionTest.Convolve_1x1x4x4_1x1x3x3_Same*"
+    "StreamExecutorGpuClientTest.NumaNode"
+)
+
 SCRIPT_DIR=$(dirname $0)
 bazel --bazelrc="$SCRIPT_DIR/rocm_xla_ci.bazelrc" test \
     --build_tag_filters=$TAG_FILTERS \
     --test_tag_filters=$TAG_FILTERS \
-    --test_timeout=920,2400,7200,9600 \
-    --profile=/tf/pkg/profile.json.gz \
-    --test_sharding_strategy=disabled \
-    --flaky_test_attempts=3 \
     --nokeep_going \
     --test_env=TF_TESTS_PER_GPU=1 \
     --action_env=XLA_FLAGS="--xla_gpu_force_compilation_parallelism=16 --xla_gpu_blas_max_algorithms=8" \
     --test_output=errors \
+    --sandbox_add_mount_pair=/dev/null:/etc/ld.so.cache \
+    --curses=no \
+    --color=yes \
+    --test_filter=-$(
+        IFS=:
+        echo "${EXCLUDED_TESTS[*]}"
+    ) \
     "$@"
